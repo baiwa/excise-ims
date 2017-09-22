8a8fc6de827d7c27e6b846838a4882be553004bc
taechapon <partner_taechapon.hi@saicmotor-cp.com>
2017-09-22 17:20:41 +0700

    Initial Login Rest Service

--- workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/common/config/SecurityConfig.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/common/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +14,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.savedrequest.NullRequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import th.co.baiwa.buckwaframework.security.rest.entrypoint.RestAuthenticationEntryPoint;
+import th.co.baiwa.buckwaframework.security.rest.handler.RestAuthenticationSuccessHandler;
+import th.co.baiwa.buckwaframework.security.rest.handler.RestLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final String LOGIN_PATH = "/api/security/login";
 	
 //	@Autowired
 //	@Qualifier("authenticationProvider")
@@ -36,35 +46,71 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		);
 //	}
 	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers(
+//				"/", "/index.htm", "/index.html",
+//				"/login.htm",
+//				"/resources/**",
+//				"/app/**",
+//				"/assets/**"
+//			).permitAll()
+//			//.antMatchers("/admin/**").hasRole("ADMIN")
+//			.anyRequest().hasRole("USER")
+//		.and()
+//		.formLogin()
+//			.loginPage("/login.htm").permitAll()
+//			.defaultSuccessUrl("/welcome.htm")
+//			.failureUrl("/login.htm?error").permitAll()
+//			.usernameParameter("username")
+//			.passwordParameter("password")
+//		.and()
+//		.logout()
+//			.logoutRequestMatcher(new AntPathRequestMatcher("/logout.htm"))
+//			//.logoutSuccessHandler(CustomLogoutSuccessHandler.getInstance("/login.htm"))
+//			.invalidateHttpSession(true)
+//		.and()
+//		.exceptionHandling().accessDeniedPage("/403.htm");
+//		
+//		http.csrf().disable();
+//	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers(
-				"/", "/index.htm", "/index.html",
-				"/login.htm",
-				"/resources/**",
-				"/app/**",
-				"/assets/**"
-			).permitAll()
-			//.antMatchers("/admin/**").hasRole("ADMIN")
-			.anyRequest().hasRole("USER")
+			.antMatchers("/api/**")
+			.authenticated()
 		.and()
 		.formLogin()
-			.loginPage("/login.htm").permitAll()
-			.defaultSuccessUrl("/welcome.htm")
-			.failureUrl("/login.htm?error").permitAll()
-			.usernameParameter("username")
-			.passwordParameter("password")
+			.loginProcessingUrl(LOGIN_PATH)
+			.successHandler(restAuthenticationSuccessHandler())
+			.failureHandler(restAuthenticationFailureHandler())
 		.and()
 		.logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout.htm"))
-			//.logoutSuccessHandler(CustomLogoutSuccessHandler.getInstance("/login.htm"))
-			.invalidateHttpSession(true)
+			.permitAll()
+			.logoutRequestMatcher(new AntPathRequestMatcher(LOGIN_PATH, HttpMethod.DELETE.toString()))
+			.logoutSuccessHandler(restLogoutSuccessHandler())
 		.and()
-		.exceptionHandling().accessDeniedPage("/403.htm");
+		.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint())
+		.and()
+		.requestCache().requestCache(new NullRequestCache());
 		
 		http.csrf().disable();
 	}
+	
+//	@Configuration
+//	@Order(1)
+//	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http
+//				.antMatcher("/api/**")
+//				.authorizeRequests()
+//					.anyRequest().hasRole("ADMIN")
+//				.and()
+//				.httpBasic();
+//		}
+//	}
 	
 //	@Bean(name = "passwordEncoder")
 //	public PasswordEncoder passwordEncoder() {
@@ -75,9 +121,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("jim").password("demo").roles("ADMIN").and()
-			.withUser("bob").password("demo").roles("USER").and()
+			.withUser("jim").password("demo").roles("ADMIN")
+			.and()
+			.withUser("bob").password("demo").roles("USER")
+			.and()
 			.withUser("ted").password("demo").roles("USER", "ADMIN");
+	}
+	
+	@Bean
+	public RestAuthenticationSuccessHandler restAuthenticationSuccessHandler() {
+		return new RestAuthenticationSuccessHandler();
+	}
+	
+	@Bean
+	public SimpleUrlAuthenticationFailureHandler restAuthenticationFailureHandler() {
+		return new SimpleUrlAuthenticationFailureHandler();
+	}
+	
+	@Bean
+	public RestLogoutSuccessHandler restLogoutSuccessHandler() {
+		return new RestLogoutSuccessHandler();
+	}
+	
+	@Bean
+	public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
+		return new RestAuthenticationEntryPoint();
 	}
 	
 }
