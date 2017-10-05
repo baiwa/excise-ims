d90ebbfce02833ad320d743879964e5cf985f73f
Wongwithit Rcl <Wongwithit.rcl@gmail.com>
2017-10-05 13:26:12 +0700

    By Yung

---
+++ workspace/ims-client/src/app/buckwaframework/common/models/parameterInfo.ts
@@ -0,0 +1,16 @@
+export class ParameterInfo {
+
+        paramInfoId : number;           
+        paramGroupId  : number;        
+        paramCode : string;              
+        value1 : string;                 
+        value2 : string;                 
+        value3 : string;                
+        value4 : string;                 
+        value5 : string;                
+        value6 : string;                 
+        sortingOrder: number;          
+        isDefault : string;    
+    
+}
+      
