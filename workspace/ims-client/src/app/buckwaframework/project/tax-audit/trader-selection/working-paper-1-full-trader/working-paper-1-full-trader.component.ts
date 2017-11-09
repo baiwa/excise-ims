f0269e785475ccd66afad046e958d0f1fe02718c
HeroBoy <teeratadpi@gmail.com>
2017-11-09 13:28:54 +0700

    update content pages

--- workspace/ims-client/src/app/buckwaframework/project/tax-audit/trader-selection/working-paper-1-full-trader/working-paper-1-full-trader.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/tax-audit/trader-selection/working-paper-1-full-trader/working-paper-1-full-trader.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkingPaper1FullTraderComponent implements OnInit {
 
+  private listItem: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.listItem = ["น้ำมัน"
+      , "เครื่องดื่ม"
+      , "ยาสูบ"
+      , "ไพ่"
+      , "แก้วและเครื่องแก้ว"
+      , "รถยนต์"
+      , 'พรมและสิ่งทอปูพื้น'
+      , "แบตเตอรี่"
+      , "ไนท์คลับและดิสโกเธค"
+      , "สถานอาบน้ำหรืออบตัวและนวด"
+      , "สนามแข่งม้า"
+      , 'สนามกอล์ฟ'
+      , "รวม"];
   }
 
 }
