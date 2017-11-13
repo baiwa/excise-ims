3890e3c20766d7cef84a75c32fa58800b4336952
HeroBoy <teeratadpi@gmail.com>
2017-11-13 13:29:15 +0700

    update content

--- workspace/ims-client/src/app/buckwaframework/project/pages/check-stamp-branch/check-stamp-branch.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/check-stamp-branch/check-stamp-branch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'check-stamp-branch',
   templateUrl: './check-stamp-branch.component.html',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckStampBranchComponent implements OnInit {
 
+  private zoneList: any[];
+  private areaList: any[];
+  private branchList: any[];
+
+  private showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.zoneList = [
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 1' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 2' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 3' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 4' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 5' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 6' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 7' }
+    ];
+
+    this.areaList = [
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรลำปาง' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรสาคร' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรปราการ' }
+    ];
+
+    this.branchList = [
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่ลำปางสาขาเมืองลำปาง' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรสาครสาขาเมืองสมุทรสาคร' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรปราการสาขาเมืองสมุทรปราการ' }
+    ];
   }
 
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
+
+
+  editData() {
+    $('#modalCheckStampBranch').modal('show');
+  }
+
+  closeModal() {
+    $('#modalCheckStampBranch').modal('hide');
+  }
 }
--- workspace/ims-client/src/app/buckwaframework/project/pages/check-stamp-yearly/check-stamp-yearly.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/check-stamp-yearly/check-stamp-yearly.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckStampYearlyComponent implements OnInit {
 
+  private zoneList: any[];
+  private areaList: any[];
+
+  private showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.zoneList = [
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 1' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 2' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 3' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 4' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 5' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 6' },
+      { 'value': 'สำนักงานสรรพสามิตภาคที่ 7' }
+    ];
+
+    this.areaList = [
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรลำปาง' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรสาคร' },
+      { 'value': 'สำนักงานสรรพสามิตพื้นที่สมุทรปราการ' }
+    ];
   }
 
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
 }
