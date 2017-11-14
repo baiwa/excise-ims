6e31dc4fad0e0b8f079062eb8c287eabe28867ad
HeroBoy <teeratadpi@gmail.com>
2017-11-14 09:59:11 +0700

    update content check-expense-withdrawal.component page

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/check-withdrawal/check-expense-withdrawal/check-expense-withdrawal.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/check-withdrawal/check-expense-withdrawal/check-expense-withdrawal.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckExpenseWithdrawalComponent implements OnInit {
 
+  private showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
   }
 
 }
