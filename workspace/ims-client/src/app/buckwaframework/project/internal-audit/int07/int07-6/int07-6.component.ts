310123dd784901866610d1091fa451101baca74c
HeroBoy <teeratadpi@gmail.com>
2017-11-17 13:18:23 +0700

    update content int07-6

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int07/int07-6/int07-6.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int07/int07-6/int07-6.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Int076Component implements OnInit {
 
+  private showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
 }
