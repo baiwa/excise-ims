2ad4f78807f366ef0c58f6bbae168ef39cd7d0a6
HeroBoy <teeratadpi@gmail.com>
2017-11-20 16:28:00 +0700

    update content int08-1-1, int08-1-2

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-1/int08-1-1.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-1/int08-1-1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'int08-1-1',
   templateUrl: './int08-1-1.component.html',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Int0811Component implements OnInit {
 
+  private showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  ngAfterViewInit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
+
+  popupEditData() {
+    $('#modalInt0811').modal('show');
+    $('#select1').show();
+    $('#select2').show();
+    $('#select3').show();
+  }
+
+  closePopupEdit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+    $('#modalInt0811').modal('hide');
+  }
 }
--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-2/int08-1-2.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-2/int08-1-2.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'int08-1-2',
   templateUrl: './int08-1-2.component.html',
   styleUrls: ['./int08-1-2.component.css']
 })
 export class Int0812Component implements OnInit {
+  private showData: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
+
+  popupEditData() {
+    $('#modalInt0812').modal('show');
+    $('#select1').show();
+    $('#select2').show();
+    $('#select3').show();
+  }
+
+  closePopupEdit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+    $('#modalInt0812').modal('hide');
   }
 
 }
