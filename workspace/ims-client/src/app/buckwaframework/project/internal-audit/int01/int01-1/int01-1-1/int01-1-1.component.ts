1271e713b21a5d865514b046ee070a87c698b9f0
Nuttawut Singhabut <bootnuttawig@gmail.com>
2017-11-21 10:33:17 +0700

    init int01-1-1

---
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-1/int01-1-1/int01-1-1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'int01-1-1',
+  templateUrl: './int01-1-1.component.html'
+})
+export class Int0111Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
