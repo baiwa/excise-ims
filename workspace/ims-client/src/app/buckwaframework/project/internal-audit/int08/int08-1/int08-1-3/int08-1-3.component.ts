832b10232e86e8913bb2feb96421edb34e071dd7
HeroBoy <teeratadpi@gmail.com>
2017-11-22 13:25:00 +0700

    update content

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-3/int08-1-3.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-1/int08-1-3/int08-1-3.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-int08-1-3',
   templateUrl: './int08-1-3.component.html',
@@ -10,6 +11,35 @@ export class Int0813Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+  }
+
+  popupEditData() {
+    $('#modalInt0813').modal('show');
+    $('#select1').show();
+    $('#select2').show();
+    $('#select3').show();
+  }
+
+  closePopupEdit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+    $('#modalInt0813').modal('hide');
+  }
+
+  popupWeightData() {
+    $('#modalInt0813-weight-data').modal('show');
+  }
+
+  clearPopupWeightData() {
+    $('#modalInt0813-weight-data').modal('hide');
   }
 
 }
