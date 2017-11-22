856a224ed917be549125fd0ec0e64d59fd60e5a0
HeroBoy <teeratadpi@gmail.com>
2017-11-22 10:15:15 +0700

    update content

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-1/int01-1-3/int01-1-3.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-1/int01-1-3/int01-1-3.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-int01-1-3',
-  templateUrl: './int01-1-3.component.html'
+  templateUrl: './int01-1-3.component.html',
+  styleUrls: ['./int01-1-3.component.css']
 })
 export class Int0113Component implements OnInit {
 
