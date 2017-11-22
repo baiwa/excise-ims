7c65005002a180c7c42a4252dc002d97833dbdcb
HeroBoy <teeratadpi@gmail.com>
2017-11-22 14:02:07 +0700

    update content

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-2/int08-2.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-2/int08-2.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'int08-2',
   templateUrl: './int08-2.component.html',
   styleUrls: ['./int08-2.component.css']
 })
 export class Int082Component implements OnInit {
+  private showData: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
+  }
+
+  popupEditData() {
+    $('#modalInt082').modal('show');
+    $('#select1').show();
+    $('#select2').show();
+    $('#select3').show();
+  }
+
+  closePopupEdit() {
+    $('#select1').hide();
+    $('#select2').hide();
+    $('#select3').hide();
+    $('#modalInt082').modal('hide');
   }
 
 }
