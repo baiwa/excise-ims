b95d3aff36c9eb62d2f5175ea8b6162e7639fcd8
HeroBoy <teeratadpi@gmail.com>
2017-12-01 12:22:03 +0700

    update content

--- workspace/ims-client/src/app/buckwaframework/project/pages/create-paper-quantity-product/create-paper-quantity-product.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/create-paper-quantity-product/create-paper-quantity-product.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePaperQuantityProductComponent implements OnInit {
 
+  public showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
   }
 
 }
