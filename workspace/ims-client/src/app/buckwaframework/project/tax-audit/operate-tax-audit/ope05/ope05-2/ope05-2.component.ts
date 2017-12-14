b265dc5be8226db622b47d1d8634b24a6a08fc80
HeroBoy <teeratadpi@gmail.com>
2017-12-14 13:32:43 +0700

    update content ope05-2

--- workspace/ims-client/src/app/buckwaframework/project/tax-audit/operate-tax-audit/ope05/ope05-2/ope05-2.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/tax-audit/operate-tax-audit/ope05/ope05-2/ope05-2.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ope052Component implements OnInit {
 
+  public showData: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  uploadData() {
+    this.showData = true;
+  }
+
+  clearData() {
+    this.showData = false;
   }
 
 }
