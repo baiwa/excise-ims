1227032c246303cabef34ad08c4d8572b9f97ad1
ZenNight <artistkanjai1@gmail.com>
2018-05-21 14:32:05 +0700

    fix checked-box, change styles

--- workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-02/ts01-02.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-02/ts01-02.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-ts01-02',
   templateUrl: './ts01-02.component.html',
+  styleUrls: ['./ts01-02.component.css']
 })
 export class Ts0102Component implements OnInit {
 
