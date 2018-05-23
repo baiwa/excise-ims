5938b812090f0b5420f6b8dc3da977a2c87db2c0
FlukeTP <31270690+3laclz@users.noreply.github.com>
2018-05-23 14:32:55 +0700

    -add mock up ts01-14

---
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-14/ts01-14.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { TextDateTH, formatter } from '../../../../../common/helper/datepicker';
+declare var jQuery: any;
+declare var $: any;
+
+@Component({
+  selector: 'app-ts01-14',
+  templateUrl: './ts01-14.component.html',
+})
+export class Ts0114Component implements OnInit {
+
+  @Output() discard = new EventEmitter<any>();
+
+  numbers:number[];
+
+  constructor() {
+    this.numbers = [1,2,3];
+  }
+
+  ngOnInit() {
+    $('#begin_date').calendar({
+      type: 'date',
+      text: TextDateTH,
+      formatter: formatter
+    });
+    $('#end_date').calendar({
+      type: 'date',
+      text: TextDateTH,
+      formatter: formatter
+    });
+    $('#nut_date').calendar({
+      type: 'date',
+      text: TextDateTH,
+      formatter: formatter
+    });
+    $('#nut1_date').calendar({
+      type: 'date',
+      text: TextDateTH,
+      formatter: formatter
+    });
+  }
+
+  onDiscard = () => {
+    // on click this view hide them
+    // ... Don't change or delete this
+    this.discard.emit(false);
+  };
+
+  onAddField = () => {
+    let num = this.numbers[this.numbers.length-1];
+    this.numbers.push(num+1);
+    this.numbers.sort();
+  };
+  
+  onDelField = index => {
+    this.numbers.splice(index, 1);
+    this.numbers.sort();
+  };
+  
+}
