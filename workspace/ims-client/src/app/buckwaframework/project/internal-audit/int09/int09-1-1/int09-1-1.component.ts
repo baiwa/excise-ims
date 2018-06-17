ed798e33cbb2dc921127346103c423017d6e795a
KimJaeHa <artistkanjai1@gmail.com>
2018-06-17 22:24:45 +0700

    add travelcosts in int09-1-1

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int09/int09-1-1/int09-1-1.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int09/int09-1-1/int09-1-1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TravelCosts } from '../../../../common/models';
 
 @Component({
   selector: 'app-int09-1-1',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Int0911Component implements OnInit {
 
-  constructor() { }
+  public allData: TravelCosts[];
+  public data: TravelCosts;
+
+  constructor() {
+    this.data = new TravelCosts();
+    this.allData = new Array<TravelCosts>();
+  }
 
   ngOnInit() {
+  }
+
+  onSubmitt() {
+    alert("submit...~");
+  }
+
+  total(index) {
+    const { allowance, accommodation, passage, outgoings } = this.allData[index];
+    return allowance + accommodation + passage + outgoings;
+  }
+
+  selectedBox(e, index) {
+    this.allData[index].checked = e.target.checked;
+  }
+
+  addData() {
+    this.allData.push(this.data);
+    this.clearData();
+  }
+
+  deleteData() {
+    this.allData = this.allData.filter( obj => {
+      return !obj.checked;
+    });
+  }
+
+  editData(index: number) {
+    this.data = this.allData[index];
+  }
+
+  clearData() {
+    this.data = new TravelCosts();
   }
 
 }
