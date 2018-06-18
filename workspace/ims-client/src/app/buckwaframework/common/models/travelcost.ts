d8981674a82bd4a9d2afdaad9f13fa91790d38b1
flame123flame <flame.tana@gmail.com>
2018-06-18 13:45:27 +0700

    commot mock up

---
+++ workspace/ims-client/src/app/buckwaframework/common/models/travelcost.ts
@@ -0,0 +1,12 @@
+export class TravelCost {
+    workSheetHeaderId: string = '';
+    workSheetHeaderName: string = '';
+    departmentName: string = '';
+    startDate: any = null;
+    endDate: any = null;
+    description: string = '';
+    createdBy: string = '';
+    createdDatetime: any = null;
+    updateBy: string = '';
+    updateDatetime: any = null;
+}
