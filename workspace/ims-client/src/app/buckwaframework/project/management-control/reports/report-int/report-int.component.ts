0c9a2d3c7cd03c358fccccca869133ca36ea2d9f
KimJaeHa <artistkanjai1@gmail.com>
2018-06-20 16:38:29 +0700

    report-int and report-tax edited variable

--- workspace/ims-client/src/app/buckwaframework/project/management-control/reports/report-int/report-int.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/management-control/reports/report-int/report-int.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TextDateTH, formatter } from '../../../../common/helper/datepicker';
 
+declare var $: any;
 @Component({
   selector: 'app-report-int',
   templateUrl: './report-int.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportIntComponent implements OnInit {
 
+  year: any;
+  toggled: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    // calendar
+    $('#year').calendar({
+      maxDate: new Date(),
+      type: 'year',
+      text: TextDateTH,
+      formatter: formatter('year')
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.year = e.target.year.value;
+    e.target.year.value = '';
+    this.toggled = true;
+  }
+
+  onCancel() {
+    this.toggled = false;
   }
 
 }
