7f0aef9e189513118d60b4ee36b8f90506591c21
flame123flame <flame.tana@gmail.com>
2018-06-20 16:27:45 +0700

    commit report-opt and out

--- workspace/ims-client/src/app/buckwaframework/project/management-control/reports/report-opt/report-opt.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/management-control/reports/report-opt/report-opt.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TextDateTH, formatter } from '../../../../common/helper/datepicker';
 
+declare var $: any;
 @Component({
   selector: 'app-report-opt',
   templateUrl: './report-opt.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportOptComponent implements OnInit {
 
+  target : any;
+  year: any;
+  toggled: boolean;
   constructor() { }
-
   ngOnInit() {
+    // calendar
+    $('#year').calendar({
+      maxDate: new Date(),
+      type: 'year',
+      text: TextDateTH,
+      formatter: formatter('year')
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.year = e.target.year.value;
+    e.target.year.value = '';
+    this.toggled = true;
+    this.target = e.target.target.value;
+    e.target.target.value = '';
+  }
+
+  onCancel() {
+    this.toggled = false;
   }
 
 }
