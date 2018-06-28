8a8d0566883b6c711414d0c3f4794c0e29d4a6f1
KimJaeHa <artistkanjai1@gmail.com>
2018-06-28 16:13:12 +0700

    fix error routes not fond 'int02s' [Frontend]

--- workspace/ims-client/src/app/buckwaframework/common/configs/app-routing.module.ts
+++ workspace/ims-client/src/app/buckwaframework/common/configs/app-routing.module.ts
@@ -4,12 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 // service
 import { AuthGuard } from '../services/auth-guard.service';
 
-// pages
-import { AddExternalDataComponent } from '../../project/tax-audit/trader-selection/add-external-data/add-external-data.component';
-import { SendLineUserComponent } from '../../project/tax-audit/trader-selection/add-external-data/send-line-user/send-line-user.component';
-import { Int021Component } from '../../project/internal-audit/int02/int02-1/int02-1.component';
-import { Int022Component } from '../../project/internal-audit/int02/int02-2/int02-2.component';
-
 const routes: Routes = [
 
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -47,7 +41,7 @@ const routes: Routes = [
 
     { path: 'epa01', loadChildren: '../../project/export-audit/epa01/epa01.module#Epa01Module' },
     { path: 'epa02', loadChildren: '../../project/export-audit/epa02/epa02.module#Epa02Module' },
-    
+
     { path: 'mgcontrol', loadChildren: '../../project/management-control/mgcontrol/mgcontrol.module#MgcontrolComponentModule' },
     { path: 'mgReportResult', loadChildren: '../../project/management-control/mgc02/mgReportResult.module#MgReportResultComponentModule' },
 
@@ -56,18 +50,15 @@ const routes: Routes = [
     { path: 'cop03', loadChildren: '../../project/check-operation/cop03/cop03.module#Cop03Module' },
     { path: 'cop04', loadChildren: '../../project/check-operation/cop04/cop04.module#Cop04Module' },
     { path: 'cop05', loadChildren: '../../project/check-operation/cop05/cop05.module#Cop05Module' },
-    
-    { path: 'int01', loadChildren: '../../project/internal-audit/int01/int01.module#Int01Module' },
 
-    { path: 'int02-1', component: Int021Component, canActivate: [AuthGuard] },
-    { path: 'int02-2', component: Int022Component, canActivate: [AuthGuard] },
-    
+    { path: 'int01', loadChildren: '../../project/internal-audit/int01/int01.module#Int01Module' },
+    { path: 'int02', loadChildren: '../../project/internal-audit/int02/int02.module#Int02Module' },
     { path: 'int05', loadChildren: '../../project/internal-audit/int05/int05.module#Int05Module' },
     { path: 'int06', loadChildren: '../../project/internal-audit/int06/int06.module#Int06Module' },
     { path: 'int07', loadChildren: '../../project/internal-audit/int07/int07.module#Int07Module' },
     { path: 'int08', loadChildren: '../../project/internal-audit/int08/int08.module#Int08Module' },
     { path: 'int09', loadChildren: '../../project/internal-audit/int09/int09.module#Int09Module' },
-    
+
     { path: 'ope04', loadChildren: '../../project/tax-audit/operate-tax-audit/ope04/ope04.module#Ope04Module' },
     { path: 'ope05', loadChildren: '../../project/tax-audit/operate-tax-audit/ope05/ope05.module#Ope05Module' },
 
---
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+
+import { Routes, RouterModule } from '@angular/router';
+import { Int021Component } from './int02-1/int02-1.component';
+import { Int022Component } from './int02-2/int02-2.component';
+
+const routes: Routes = [
+    { path: '1', component: Int021Component },
+    { path: '2', component: Int022Component }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+        CommonModule
+    ],
+    declarations: [
+        Int021Component,
+        Int022Component
+    ],
+    exports: [RouterModule]
+})
+export class Int02Module { }
