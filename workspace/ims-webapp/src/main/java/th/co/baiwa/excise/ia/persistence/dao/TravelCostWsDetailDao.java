bc178ba1be8022de228360deb73dcdd60abfa296
flukeTP <fook_1589@hotmail.com>
2018-07-03 17:23:01 +0700

    insert TravelCost [Backend]

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/persistence/dao/TravelCostWsDetailDao.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/persistence/dao/TravelCostWsDetailDao.java
@@ -13,17 +13,31 @@ import th.co.baiwa.excise.ia.persistence.entity.TravelCostWsDetail;
 public class TravelCostWsDetailDao {
 	
 	 @Autowired
-	    private JdbcTemplate jdbcTemplate;
-	 public void insertTravelCostWsDetail(TravelCostWsDetail value) {
+	 private JdbcTemplate jdbcTemplate;
+	 
+	 public int insertTravelCostWsDetail(TravelCostWsDetail value) {
 			//inti SQL for insert to database
 			StringBuilder sql = new StringBuilder();
-			sql.append(" insert into insert into IA_TRAVEL_COST_WS_DETAIL (WORK_SHEET_DETAIL_ID,HEADER_ID,NAME,LAST_NAME,POSITION,CATEGORY,DEGREE,ALLOWANCE_DATE,ALLOWANCE_COST,RENT_DATE,RENT_COST,TRAVEL_COST,OTHER_COST,SUM_COST,NOTE,CREATED_BY,CREATED_DATETIME,UPDATE_BY,UPDATE_DATETIME) ");
-			sql.append(" values(WORK_SHEET_DETAIL_ID.nextval ,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ");
+			sql.append(" insert into IA_TRAVEL_COST_WS_DETAIL (WORK_SHEET_DETAIL_ID,HEADER_ID,NAME,LAST_NAME,POSITION,CATEGORY,DEGREE,ALLOWANCE_DATE,ALLOWANCE_COST,RENT_DATE,RENT_COST,TRAVEL_COST,OTHER_COST,SUM_COST,NOTE,CREATED_BY,CREATED_DATETIME,UPDATE_BY,UPDATE_DATETIME) ");
+			sql.append(" values(WORK_SHEET_DETAIL_SEQ.nextval ,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ");
 			//for to set Object
-			jdbcTemplate.update(sql.toString() ,TravelCostWsDetailToArrayObject(value) );
-		}
+			return jdbcTemplate.update(sql.toString() ,TravelCostWsDetailToArrayObject(value) );
+	 }
+	 
+	 /*public int insertTravelCostWsDetails(TravelCostWsDetail[] value) {
+			//inti SQL for insert to database
+			StringBuilder sql = new StringBuilder();
+			sql.append(" INSERT ALL ");
+			for(int i=0; i<value.length; i++) {
+				sql.append(" INTO IA_TRAVEL_COST_WS_DETAIL (WORK_SHEET_DETAIL_ID,HEADER_ID,NAME,LAST_NAME,POSITION,CATEGORY,DEGREE,ALLOWANCE_DATE,ALLOWANCE_COST,RENT_DATE,RENT_COST,TRAVEL_COST,OTHER_COST,SUM_COST,NOTE,CREATED_BY,CREATED_DATETIME,UPDATE_BY,UPDATE_DATETIME) ");
+				sql.append(" VALUES (WORK_SHEET_DETAIL_SEQ.nextval ,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ");
+			}
+			sql.append(" SELECT * FROM dual ");
+			//for to set Object
+			return jdbcTemplate.update(sql.toString() , TravelCostWsDetailToArrayObjects(value) );
+	}*/
+	 
 	 private Object[] TravelCostWsDetailToArrayObject(TravelCostWsDetail value) {
-
 			List<Object> valueList = new ArrayList<Object>();
 			if (value != null) {
 				valueList.add(value.getHeaderId());
@@ -44,10 +58,36 @@ public class TravelCostWsDetailDao {
 				valueList.add(value.getCreatedDatetime());				
 				valueList.add(value.getUpdateBy());				
 				valueList.add(value.getUpdateDatetime());
-				
 			}
 			return valueList.toArray();
 		}
+	 
+	 /*private Object[] TravelCostWsDetailToArrayObjects(TravelCostWsDetail[] values) {
+			List<Object> valueList = new ArrayList<Object>();
+			for(TravelCostWsDetail value: values) {
+				if (value != null) {
+					valueList.add(value.getHeaderId());
+					valueList.add(value.getName());
+					valueList.add(value.getLastName());
+					valueList.add(value.getPosition());
+					valueList.add(value.getCategory());
+					valueList.add(value.getDegree());				
+					valueList.add(value.getAllowanceDate());
+					valueList.add(value.getAllowanceCost());
+					valueList.add(value.getRentDate());
+					valueList.add(value.getRentCost());				
+					valueList.add(value.getTravelCost());				
+					valueList.add(value.getOtherCost());				
+					valueList.add(value.getSumCost());				
+					valueList.add(value.getNote());				
+					valueList.add(value.getCreatedBy());				
+					valueList.add(value.getCreatedDatetime());				
+					valueList.add(value.getUpdateBy());				
+					valueList.add(value.getUpdateDatetime());
+				}
+			}
+			return valueList.toArray();
+		}*/
 	 
 	
 	
