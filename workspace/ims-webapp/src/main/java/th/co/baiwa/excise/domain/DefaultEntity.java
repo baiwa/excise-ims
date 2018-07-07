36333cf70b2c27de889fe42519fe683d9d84f2ea
baiwaDev <pavit@baiwa.co.th>
2018-07-07 13:59:06 +0700

    Add class DefaultEntity

---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/domain/DefaultEntity.java
@@ -0,0 +1,53 @@
+package th.co.baiwa.excise.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class DefaultEntity {
+	
+	private String isDeleted;
+	private BigDecimal version;
+	private String createdBy;
+	private Date createdDate;
+	private String updatedBy;
+	private Date updatedDate;
+	
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public BigDecimal getVersion() {
+		return version;
+	}
+	public void setVersion(BigDecimal version) {
+		this.version = version;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	
+	
+}
