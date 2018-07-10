5b34dc4c8a75ccb7a9b39af1b64fd4b85a16b3e1
KimJaeHa <artistkanjai1@gmail.com>
2018-07-10 14:30:46 +0700

    fixed conflict 'Int09Service, Controller' [Backend]

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/service/Int09Service.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/service/Int09Service.java
@@ -1,5 +1,6 @@
 package th.co.baiwa.excise.ia.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,11 +8,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import th.co.baiwa.excise.ia.persistence.entity.TravelCostWorkSheetHeader;
-import th.co.baiwa.excise.ia.persistence.entity.TravelCostWsDetail;
 import th.co.baiwa.buckwaframework.security.util.UserLoginUtils;
 import th.co.baiwa.excise.ia.persistence.dao.TravelCostWorkSheetHeaderDao;
 import th.co.baiwa.excise.ia.persistence.dao.TravelCostWsDetailDao;
+import th.co.baiwa.excise.ia.persistence.entity.TravelCostWorkSheetHeader;
+import th.co.baiwa.excise.ia.persistence.entity.TravelCostWsDetail;
 import th.co.baiwa.excise.ia.persistence.entity.TravelCostWsIntegrate;
 
 @Service
@@ -22,28 +23,35 @@ public class Int09Service {
 
 	@Autowired
 	private TravelCostWsDetailDao travelCostWsDetailDao;
-	
-	
+
+	public Integer deleteTravelCostHeader(String id) {
+		if (travelCostWorkSheetHeaderDao.deleteTravelCostWorksheetHeader(new BigDecimal(id)) > 0) {
+			return travelCostWsDetailDao.deleteTravelCostWorksheetDetail(new BigDecimal(id));
+		} else {
+			return 0;
+		}
+	}
+
 	public List<TravelCostWorkSheetHeader> listTravelCostHeaderService(TravelCostWorkSheetHeader travel) {
 
 		// Query Data
 		List<TravelCostWorkSheetHeader> result = travelCostWorkSheetHeaderDao.queryTravelCostWorksheetHeader(travel);
-		
+
 		// Return Result
 		return result;
 	}
-	
+
 	public List<TravelCostWsIntegrate> listTravelCostService(TravelCostWorkSheetHeader travel) {
 		// Initial Objects
 		List<TravelCostWsIntegrate> result = new ArrayList<TravelCostWsIntegrate>();
 		TravelCostWsDetail detail = new TravelCostWsDetail();
 		detail.setHeaderId(travel.getWorkSheetHeaderId().toString());
 		TravelCostWsIntegrate ti = new TravelCostWsIntegrate();
-		
+
 		// Query Data
 		List<TravelCostWorkSheetHeader> header = travelCostWorkSheetHeaderDao.queryTravelCostWorksheetHeader(travel);
 		List<TravelCostWsDetail> details = travelCostWsDetailDao.queryTravelCostWsDetail(detail);
-		
+
 		// Mapping Data
 		ti.setWorkSheetHeaderId(header.get(0).getWorkSheetHeaderId());
 		ti.setWorkSheetHeaderName(header.get(0).getWorkSheetHeaderName());
@@ -56,10 +64,10 @@ public class Int09Service {
 		ti.setUpdatedBy(header.get(0).getUpdatedBy());
 		ti.setUpdatedDate(header.get(0).getUpdatedDate());
 		ti.setDetail(details);
-		
+
 		// Add to ArrayList
 		result.add(ti);
-		
+
 		// Return Result
 		return result;
 	}
@@ -80,7 +88,8 @@ public class Int09Service {
 		// Insert data to HeaderCost
 		if (travelCostWorkSheetHeaderDao.insertTravelCostWorksheetHeader(travelCostWsIntegrate) > 0) {
 			// Get ID HeaderCost
-			List<TravelCostWorkSheetHeader> header = travelCostWorkSheetHeaderDao.queryTravelCostWorksheetHeader("WORK_SHEET_HEADER_ID DESC");
+			List<TravelCostWorkSheetHeader> header = travelCostWorkSheetHeaderDao
+					.queryTravelCostWorksheetHeader("WORK_SHEET_HEADER_ID DESC");
 
 			if (header.toArray().length > 0) {
 				// New ArrayList
@@ -88,7 +97,7 @@ public class Int09Service {
 
 				for (TravelCostWsDetail t : travelCostWsIntegrate.getDetail()) {
 					// Set data to DetailCost
-					t.setHeaderId(header.get (0).getWorkSheetHeaderId().toString());
+					t.setHeaderId(header.get(0).getWorkSheetHeaderId().toString());
 					t.setCreatedBy(creator);
 					t.setUpdatedBy(creator);
 					t.setCreatedDate(date);
