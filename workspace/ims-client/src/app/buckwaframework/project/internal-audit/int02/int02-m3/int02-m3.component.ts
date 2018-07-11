10b6a8cadb616b780ea61a5a83ba3f924cf5208c
flukeTP <fook_1589@hotmail.com>
2018-07-11 17:01:13 +0700

    add page menu "สรุปผลแบบสอบทานระบบการควบคุมภายใน"

---
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02-m3/int02-m3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-int02-m3',
+  templateUrl: './int02-m3.component.html',
+  styleUrls: ['./int02-m3.component.css']
+})
+export class Int02M3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02.module.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02.module.ts
@@ -7,12 +7,16 @@ import { Int022Component } from "./int02-2/int02-2.component";
 import { FormsModule } from "@angular/forms";
 import { Int023Component } from "./int02-3/int02-3.component";
 import { Int02M2Component } from "./int02-m2/int02-m2.component";
+import { Int02M3Component } from './int02-m3/int02-m3.component';
+import { Int02M4Component } from './int02-m4/int02-m4.component';
 
 const routes: Routes = [
   { path: "1", component: Int021Component },
   { path: "2", component: Int022Component },
   { path: "3", component: Int023Component },
-  { path: "m2", component: Int02M2Component }
+  { path: "m2", component: Int02M2Component },
+  { path: "m3", component: Int02M3Component },
+  { path: "m4", component: Int02M4Component }
 ];
 
 @NgModule({
@@ -21,7 +25,9 @@ const routes: Routes = [
     Int021Component,
     Int022Component,
     Int023Component,
-    Int02M2Component
+    Int02M2Component,
+    Int02M3Component,
+    Int02M4Component
   ],
   exports: [RouterModule]
 })
