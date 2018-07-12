c7aed12c87865e1e291ff9cd5888f4d6a1eb2abd
flukeTP <fook_1589@hotmail.com>
2018-07-12 19:34:22 +0700

    wording title

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02-m3/int02-m3.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int02/int02-m3/int02-m3.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+var jQuery: any;
+declare var $: any;
 
 @Component({
-  selector: 'app-int02-m3',
-  templateUrl: './int02-m3.component.html',
-  styleUrls: ['./int02-m3.component.css']
+  selector: "app-int02-m3",
+  templateUrl: "./int02-m3.component.html",
+  styleUrls: ["./int02-m3.component.css"]
 })
 export class Int02M3Component implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    $("#showList").click(function() {
+      $(".ui.modal.show").modal("show");
+    });
 
+    $("#closeList").click(function() {
+      $(".ui.modal.show").modal("hide");
+    });
+  }
 }
