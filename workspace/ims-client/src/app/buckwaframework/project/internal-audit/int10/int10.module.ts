b720810f1715c09fe7801b1037164c7724c24836
flame123flame <flame.tana@gmail.com>
2018-07-18 13:36:48 +0700

    commit module

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int10/int10.module.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int10/int10.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 
 import { Routes, RouterModule } from "@angular/router";
 import { Int101Component } from "./int10-1/int10-1.component";
+import { FormsModule } from "@angular/forms";
 
 
 const routes: Routes = [
@@ -11,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), CommonModule],
+  imports: [RouterModule.forChild(routes), CommonModule,FormsModule],
   declarations: [Int101Component],
   exports: [RouterModule]
 })
