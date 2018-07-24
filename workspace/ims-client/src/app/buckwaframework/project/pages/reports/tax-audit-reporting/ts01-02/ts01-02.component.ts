ec5fce66a49e32afe7cdee859bc01d0e7b5aa773
toffeeDev <saknarin.top21@gmail.com>
2018-07-24 10:30:21 +0700

    mis_t_s_01_02 addVariable

--- workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-02/ts01-02.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-02/ts01-02.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AjaxService } from "../../../../../common/services";
 
 @Component({
   selector: 'app-ts01-02',
@@ -11,8 +12,13 @@ export class Ts0102Component implements OnInit {
 
   add: number;
 
-  constructor() {
+  obj: Ts0102;
+
+  constructor(
+    private ajax: AjaxService
+  ) {
     this.add = 0;
+    this.obj = new Ts0102();
   }
 
   ngOnInit() {
@@ -27,4 +33,47 @@ export class Ts0102Component implements OnInit {
   onAddField = () => {
     this.add++;
   };
+  onSubmit = e => {
+    e.preventDefault();
+    const url = "report/pdf/ts/mis_t_s_01_02";
+    if (this.obj.agreeBox == "factory") {
+      this.obj.factory = true;
+      this.obj.service = false;
+      this.obj.company = false;
+    } else if (this.obj.agreeBox == "service") {
+      this.obj.factory = false;
+      this.obj.service = true;
+      this.obj.company = false;
+    } else if (this.obj.agreeBox == "company") {
+      this.obj.factory = false;
+      this.obj.service = false;
+      this.obj.company = true;
+    }
+    this.ajax.post(url, `'${JSON.stringify(this.obj).toString()}'`, res => {
+      if (res.status == 200 && res.statusText == "OK") {
+        window.open("/ims-webapp/api/report/pdf/mis_t_s_01_02/file");
+      }
+    });
+  };
+
+}
+
+class Ts0102 {
+  [x: string]: any;
+  sendTo: string;
+  department: string;
+  item: string;
+  agreeBox: string;
+  factory: boolean;
+  service: boolean;
+  company: boolean;
+  name: string;
+  exciseId: string;
+  address: string;
+  workType: string;
+  date: string;
+  case: string;
+  signature: string;
+  position:string;
+
 }
