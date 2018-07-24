57417bd2b66e3ccff978d09a39971a73da11b5b2
flame123flame <flame.tana@gmail.com>
2018-07-24 10:52:10 -0700

    commit jasper

--- workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-04/ts01-04.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-04/ts01-04.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AjaxService, MessageBarService } from '../../../../../common/services';
 
 @Component({
   selector: 'app-ts01-04',
@@ -8,11 +9,15 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class Ts0104Component implements OnInit {
 
   @Output() discard = new EventEmitter<any>();
-
+  numbers: number[];
+  obj: Ts0104;
   add: number;
 
-  constructor() {
+  constructor(   private messageBarService: MessageBarService,
+    private ajax: AjaxService) {
     this.add = 0;
+    this.numbers = [1, 2, 3];
+    this.obj = new Ts0104();
   }
 
   ngOnInit() {
@@ -25,6 +30,63 @@ export class Ts0104Component implements OnInit {
   };
 
   onAddField = () => {
-    this.add++;
+    let num = this.numbers.length;
+    if (num < 5) {
+      this.numbers.push(num + 1);
+    } else {
+      this.messageBarService.errorModal(
+        "ไม่สามารถทำรายการได้",
+        "เกิดข้อผิดพลาด"
+      );
+    }
   };
+
+  onDelField = index => {
+    this.obj["analys" + (index + 1)] = "";
+    this.numbers.splice(index, 1);
+  };
+
+  
+  onSubmit = e => {
+    e.preventDefault();
+    const url = "report/pdf/ts/mis_t_s_01_04";
+  
+    this.ajax.post(url, `'${JSON.stringify(this.obj).toString()}'`, res => {
+      if (res.status == 200 && res.statusText == "OK") {
+        // var byteArray = new Uint8Array(res.json());
+        // var blob = new Blob([byteArray], { type: "application/pdf" });
+        // if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        //   window.navigator.msSaveOrOpenBlob(blob);
+        // } else {
+        //   var objectUrl = URL.createObjectURL(blob);
+        //   window.open(objectUrl);
+        // }
+        window.open("/ims-webapp/api/report/pdf/mis_t_s_01_04/file");
+      }
+    });
+  };
+}
+
+class Ts0104 {
+  
+  onNumber: string;
+  onNumber1: string;
+  date1: string;
+  date2: string;
+  address: string;
+  month1: string;
+  month2: string;
+  buddhist1: string;
+  subject: string;
+  study: string;
+  referto: string;
+  want: string;
+  appointment: string;
+  analysControl: string;
+  pleasebring: string;
+  intpector: string;
+  poslion: string;
+
+
+ 
 }
