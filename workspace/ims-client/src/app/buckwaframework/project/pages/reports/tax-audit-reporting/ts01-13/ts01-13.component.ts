c6dd393583bebe52fb037dfbc8d923ba1139577a
flame123flame <flame.tana@gmail.com>
2018-07-26 11:47:13 -0700

    commit jasper 01_13

--- workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-13/ts01-13.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-13/ts01-13.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
-import { TextDateTH, formatter } from '../../../../../common/helper/datepicker';
+import { AjaxService, MessageBarService } from '../../../../../common/services';
 declare var jQuery: any;
 declare var $: any;
 
@@ -11,35 +10,20 @@ declare var $: any;
 })
 export class Ts0113Component implements OnInit {
 
+  
   @Output() discard = new EventEmitter<any>();
+  numbers: number[];
+  obj: Ts0113;
+  add: number;
 
-  numbers:number[];
-
-  constructor() {
-    this.numbers = [1,2,3];
+  constructor(   private messageBarService: MessageBarService,
+    private ajax: AjaxService) {
+    this.add = 0;
+    this.numbers = [1, 2, 3];
+    this.obj = new Ts0113();
   }
 
   ngOnInit() {
-    $('#begin_date').calendar({
-      type: 'date',
-      text: TextDateTH,
-      formatter: formatter()
-    });
-    $('#end_date').calendar({
-      type: 'date',
-      text: TextDateTH,
-      formatter: formatter()
-    });
-    $('#nut_date').calendar({
-      type: 'date',
-      text: TextDateTH,
-      formatter: formatter()
-    });
-    $('#nut1_date').calendar({
-      type: 'date',
-      text: TextDateTH,
-      formatter: formatter()
-    });
   }
 
   onDiscard = () => {
@@ -47,5 +31,63 @@ export class Ts0113Component implements OnInit {
     // ... Don't change or delete this
     this.discard.emit(false);
   };
+
+  onAddField = () => {
+    let num = this.numbers.length;
+    if (num < 5) {
+      this.numbers.push(num + 1);
+    } else {
+      this.messageBarService.errorModal(
+        "ไม่สามารถทำรายการได้",
+        "เกิดข้อผิดพลาด"
+      );
+    }
+  };
+
+  onDelField = index => {
+    this.obj["analys" + (index + 1)] = "";
+    this.numbers.splice(index, 1);
+  };
+
   
+  onSubmit = e => {
+    e.preventDefault();
+    const url = "report/pdf/ts/mis_t_s_01_13";
+  
+    this.ajax.post(url, `'${JSON.stringify(this.obj).toString()}'`, res => {
+      if (res.status == 200 && res.statusText == "OK") {
+        // var byteArray = new Uint8Array(res.json());
+        // var blob = new Blob([byteArray], { type: "application/pdf" });
+        // if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        //   window.navigator.msSaveOrOpenBlob(blob);
+        // } else {
+        //   var objectUrl = URL.createObjectURL(blob);
+        //   window.open(objectUrl);
+        // }
+        window.open("/ims-webapp/api/report/pdf/mis_t_s_01_13/file");
+      }
+    });
+  };
+}
+
+class Ts0113 {
+  
+  factory1: boolean = false;
+  brothel1: boolean = false;
+  importer1: boolean = false;
+  myName1: string;
+  date1: string;
+  monht1: string; 
+  buddhist1: string;
+  postion1: string;
+  Nameplace1: string;
+  bookNumber1: string;
+  exciseRegister1: string;
+  checkInOn1: string;
+  checktime1: string;
+  dateCheck1: string;
+  acditor1: string;
+ 
+
+
 }
