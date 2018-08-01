c7ad33755d019c514142994135e7aa365d6f82b3
flukeTP <fook_1589@hotmail.com>
2018-08-01 09:46:59 +0700

    uploadfile [backend]

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/upload/controller/UploadFileExciseController.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/upload/controller/UploadFileExciseController.java
@@ -1,6 +1,8 @@
 package th.co.baiwa.excise.upload.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.EncryptedDocumentException;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import th.co.baiwa.buckwaframework.preferences.persistence.entity.Lov;
+import th.co.baiwa.buckwaframework.support.ApplicationCache;
 import th.co.baiwa.excise.domain.form.FormUpload;
 import th.co.baiwa.excise.upload.service.UploadFileExciseService;
 
@@ -27,9 +31,38 @@ public class UploadFileExciseController {
 
 	@PostMapping("excel")
 	@ResponseBody
-	public List<Object> excel(@ModelAttribute FormUpload mainForm) throws EncryptedDocumentException, InvalidFormatException, IOException{
+	public List<FormUpload> excel(@ModelAttribute FormUpload mainForm) throws EncryptedDocumentException, InvalidFormatException, IOException{
 		logger.debug("mainForm : " + mainForm);
-		return uploadFileExciseService.readFileExcel(mainForm);
+		List<String> headerList = new ArrayList<>();
+		List<Lov> lovHeaderColumnList = ApplicationCache.getListOfValueByValueType("COLUMN_NAME", "OPE04-1");
+		List<FormUpload> fuList = new ArrayList<>();
+		FormUpload fu = new FormUpload();
+		for (Lov lov : lovHeaderColumnList) {
+			headerList.add(lov.getValue1());
+		}
+		List<String[]> xxx = uploadFileExciseService.readFileExcel(mainForm, headerList);
+		for (String[] stringArr : xxx) {
+			fu = new FormUpload();
+			for(int i = 0 ; i < stringArr.length ; i++) {
+				if(i == 0) {
+					fu.setColumn1(stringArr[i]);
+				}else if(i == 1) {
+					fu.setColumn2(stringArr[i]);
+				}else if(i == 2) {
+					fu.setColumn3(stringArr[i]);
+				}else if(i == 3) {
+					fu.setColumn4(stringArr[i]);
+				}else if(i == 4) {
+					fu.setColumn5(stringArr[i]);
+				}else if(i == 5) {
+					fu.setColumn6(stringArr[i]);
+				}
+			}
+			fuList.add(fu);
+		}
+		
+		
+		return fuList;
 	}
 
 }
--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/upload/service/UploadFileExciseService.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/upload/service/UploadFileExciseService.java
@@ -1,9 +1,11 @@
 package th.co.baiwa.excise.upload.service;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -20,41 +22,100 @@ import th.co.baiwa.excise.domain.form.FormUpload;
 import th.co.baiwa.excise.ta.service.ExciseDetailService;
 
 @Service
+//public class UploadFileExciseService {
+//
+//	private Logger logger = LoggerFactory.getLogger(ExciseDetailService.class);
+//
+//	public List<Object> readFileExcel(FormUpload multipartFile)
+//			throws IOException, EncryptedDocumentException, InvalidFormatException {
+//		logger.info("UploadFileExciseService.readFileExcel");
+//
+//		if ( multipartFile.getFileExel() != null) {
+//			byte[] byt =  multipartFile.getFileExel().getBytes();
+//			Workbook workbook = WorkbookFactory.create(new ByteArrayInputStream(byt));
+//			// Get Sheet at index 0
+//			Sheet sheet = workbook.getSheetAt(0);
+//
+//			List<Object> res = new ArrayList<Object>();
+//			List<Object> li = new ArrayList<Object>(); 
+//			Row row;
+//			Cell cell;
+//
+//			for (int i = 0; i <= 6; i++) {
+//				row = sheet.getRow(i);
+//				li = new ArrayList<Object>();
+//				for (int j = 2; j <= 5; j++) {
+//					if(j != 4) {
+//						cell = row.getCell(j);
+//						li.add(cell.toString());
+//					}
+//				}
+//				res.add(li);
+//			}
+//			
+//			return res;
+//		}
+//
+//		return null;
+//	}
+//
+//}
 public class UploadFileExciseService {
-
+	
 	private Logger logger = LoggerFactory.getLogger(ExciseDetailService.class);
-
-	public List<Object> readFileExcel(FormUpload multipartFile)
+	
+	public List<String[]> readFileExcel(FormUpload multipartFile , List<String> headerNameList)
 			throws IOException, EncryptedDocumentException, InvalidFormatException {
 		logger.info("UploadFileExciseService.readFileExcel");
+		Workbook workbook = WorkbookFactory.create(new FileInputStream("C:\\file.xlsx"));
+		Sheet sheet = workbook.getSheetAt(0);
+		int totalRows = sheet.getPhysicalNumberOfRows();
 
-		if ( multipartFile.getFileExel() != null) {
-			byte[] byt =  multipartFile.getFileExel().getBytes();
-			Workbook workbook = WorkbookFactory.create(new ByteArrayInputStream(byt));
-			// Get Sheet at index 0
-			Sheet sheet = workbook.getSheetAt(0);
-
-			List<Object> res = new ArrayList<Object>();
-			List<Object> li = new ArrayList<Object>();
-			Row row;
-			Cell cell;
-
-			for (int i = 0; i <= 6; i++) {
-				row = sheet.getRow(i);
-				li = new ArrayList<Object>();
-				for (int j = 2; j <= 5; j++) {
-					if(j != 4) {
-						cell = row.getCell(j);
-						li.add(cell.toString());
-					}
-				}
-				res.add(li);
-			}
-			
-			return res;
+		Map<String, Integer> map = new HashMap<String,Integer>(); //Create map
+		Row row = sheet.getRow(0); //Get first row
+		//following is boilerplate from the java doc
+		short minColIx = row.getFirstCellNum(); //get the first column index for a row
+		short maxColIx = row.getLastCellNum(); //get the last column index for a row
+		for(short colIx=minColIx; colIx<maxColIx; colIx++) { //loop from first to last index
+		Cell cell = row.getCell(colIx); //get the cell
+ 		map.put(cell.getStringCellValue(),cell.getColumnIndex()); //add the cell contents (name of column) and cell index to the map
 		}
-
-		return null;
+		
+		List<String[]> listOfDataFromReport = new ArrayList<String[]>();
+		String[] rowData  = null;
+		for(int x = 1; x<=totalRows; x++){
+			FormUpload rr = new FormUpload(); //Data structure to hold the data from the xls file.
+		 Row dataRow = sheet.getRow(x); //get row 1 to row n (rows containing data)
+		 rowData = new String[headerNameList.size()];
+		 if(dataRow != null) {
+			 for(String data : headerNameList) {
+				 int idn = map.get(data);
+				 Cell cell = dataRow.getCell(idn); 
+				 rowData[idn] = getCellValue(cell);
+			 }
+		 }
+		 listOfDataFromReport.add(rowData);  
+		}
+		
+		//Now you have a list of report rows
+		for(int j = 0; j< listOfDataFromReport.size();j++){
+		   System.out.println("column "+j+1+" Value: " +   listOfDataFromReport.get(j));
+		//etc...    
+		}
+		return listOfDataFromReport;
 	}
-
+	
+	private String getCellValue(Cell cell) {
+		try {
+			return cell.getStringCellValue();
+		} catch (Exception e) {
+			try {
+				return cell.getNumericCellValue()+"";
+			} catch (Exception ex) {
+				
+			}
+		}
+		return "";
+	}
+	
 }
