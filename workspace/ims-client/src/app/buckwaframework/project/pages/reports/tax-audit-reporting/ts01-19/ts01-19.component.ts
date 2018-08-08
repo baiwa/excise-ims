309bab488f3f8a0d680e163ac9620d02a194f96c
flame123flame <flame.tana@gmail.com>
2018-08-08 18:27:47 +0700

    jasper 16,19,17

--- workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-19/ts01-19.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/pages/reports/tax-audit-reporting/ts01-19/ts01-19.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { TextDateTH, formatter } from '../../../../../common/helper/datepicker';
+import { AjaxService } from '../../../../../common/services';
 declare var jQuery: any;
 declare var $: any;
 
@@ -9,9 +10,10 @@ declare var $: any;
   templateUrl: './ts01-19.component.html',
 })
 export class Ts0119Component implements OnInit {
-
+  obj: Ts0119;
   @Output() discard = new EventEmitter<any>();
-  constructor() {
+  constructor(private ajax: AjaxService) {
+    this.obj = new Ts0119();
   }
 
   ngOnInit() {
@@ -22,5 +24,29 @@ export class Ts0119Component implements OnInit {
     // ... Don't change or delete this
     this.discard.emit(false);
   };
-  
+
+  optionAddress = () => {
+    const optionURL = "excise/detail/objectAddressByExciseId";
+    this.ajax.post(optionURL, {
+      exciseId: this.obj.exciseId
+    }, res => {
+      console.log(res.json());
+      var dat = res.json();
+   
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const url = "report/pdf/ts/mis_t_s_01_19";
+    this.ajax.post(url, `'${JSON.stringify(this.obj).toString()}'`, res => {
+      if (res.status == 200 && res.statusText == "OK") {
+        window.open("/ims-webapp/api/report/pdf/mis_t_s_01_19/file");
+      }
+    });
+  };
+}
+class Ts0119 {
+  logo: string = "logo.jpg";
+  [x: string]: any;
+
 }
