d9046b59cfa90028c0d6cfa075b4d574230beb5a
teerawut kulrittichai <teerawutkt3@gmail.com>
2018-08-08 18:23:43 +0700

    Label value bean

---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/domain/LabelValueBean.java
@@ -0,0 +1,38 @@
+package th.co.baiwa.excise.domain;
+
+import java.io.Serializable;
+
+public class LabelValueBean implements Serializable{
+	
+	private static final long serialVersionUID = 2767358184153440939L;
+	private String label;
+	private String value;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public LabelValueBean(String label, String value) {
+		super();
+		this.label = label;
+		this.value = value;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "LabelValueBean [label=" + label + ", value=" + value + "]";
+	}
+	
+}
