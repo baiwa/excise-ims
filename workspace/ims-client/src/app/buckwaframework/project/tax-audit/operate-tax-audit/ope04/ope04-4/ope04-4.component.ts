5362c21e7c9d1b265e841f626c1931758ee1310c
flame123flame <flame.tana@gmail.com>
2018-08-20 16:51:11 +0700

    edit table

--- workspace/ims-client/src/app/buckwaframework/project/tax-audit/operate-tax-audit/ope04/ope04-4/ope04-4.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/tax-audit/operate-tax-audit/ope04/ope04-4/ope04-4.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TextDateTH } from '../../../../../common/helper';
+declare var $: any;
 @Component({
   selector: 'ope04-4',
   templateUrl: './ope04-4.component.html',
@@ -12,6 +13,26 @@ export class Ope044Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(".ui.dropdown").dropdown();
+    $(".ui.dropdown.ope04-1").css("width", "100%");
+
+    $("#calendarFront").calendar({
+      endCalendar: $("#calendarLast"),
+      maxDate: new Date(),
+      type: "month",
+      text: TextDateTH,
+      
+      
+    });
+
+    $("#calendarLast").calendar({
+      startCalendar: $("#calendarFront"),
+      maxDate: new Date(),
+      type: "month",
+      text: TextDateTH,
+    
+    });
   }
 
   uploadData() {
