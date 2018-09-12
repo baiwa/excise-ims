0e3a1474ae2e887251f49619d3ee85aa924623fd
flmae123flame <flame.tana@gmail.com>
2018-09-12 14:13:08 +0700

    commit ยาสูบ

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-2/int01-2-1/int01-2-1.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-2/int01-2-1/int01-2-1.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { AjaxService } from "../../../../../common/services";
+import { TextDateTH, formatter } from "../../../../../common/helper";
 declare var $: any;
+
+const URL = {
+  DROPDOWN : "combobox/controller/getDropByTypeAndParentId"
+};
 @Component({
   selector: "int01-2-1",
   templateUrl: "./int01-2-1.component.html",
@@ -8,11 +14,34 @@ declare var $: any;
 export class Int0121Component implements OnInit {
   private selectedProduct: string = "สำนักงานสรรพสามิตพื้นที่เมืองพิษณุโลก ";
   private productList: any[];
-  constructor() {}
+
+  travelTo1List: any;
+  travelTo2List: any;
+  travelTo3List: any;
+
+  constructor(private ajax: AjaxService) { }
 
   ngOnInit() {
+
+    
+    $("#calendar1").calendar({
+      maxDate: new Date(),
+      type: "date",
+      text: TextDateTH,
+      formatter: formatter()
+
+    });
+
+    $("#calendar2").calendar({
+      maxDate: new Date(),
+      type: "date",
+      text: TextDateTH,
+      formatter: formatter()
+
+    });
+    
     $(".ui.dropdown").dropdown();
-    $(".ui.dropdown.ai").css("width", "100%");
+    $(".ui.dropdown.search").css("width", "100%");
     this.productList = [
       { value: "สำนักงานสรรพสามิตพื้นที่เมืองพิษณุโลก " },
       { value: "สำนักงานสรรพสามิตพื้นที่เมืองพิษณุโลก " },
@@ -21,16 +50,31 @@ export class Int0121Component implements OnInit {
       { value: "สำนักงานสรรพสามิตพื้นที่เมืองพิษณุโลก " },
       { value: "สำนักงานสรรพสามิตพื้นที่เมืองพิษณุโลก " }
     ];
-  }
-  popupEditData() {
-    $("#modalEditData").modal("show");
-    $("#modalInt062").modal("show");
-    $("#selectTrading").show();
+    this.travelTo1Dropdown();
   }
 
-  closePopupEdit() {
-    $("#selectTrading").show();
-    $("#modalEditData").modal("hide");
-    $("#modalInt062").modal("hide");
+  travelTo1Dropdown = () => {
+    this.ajax.post(URL.DROPDOWN, { type: "SECTOR_VALUE" }, res => {
+      this.travelTo1List = res.json();
+    });
   }
+
+  travelTo2Dropdown = e => {
+    var id = e.target.value;
+    if (id != "") {
+      this.ajax.post(URL.DROPDOWN, { type: "SECTOR_VALUE", lovIdMaster: id }, res => {
+        this.travelTo2List = res.json();
+      });
+    }
+  }
+
+  travelTo3Dropdown = e => {
+    var id = e.target.value;
+    if (id != "") {
+      this.ajax.post(URL.DROPDOWN, { type: "SECTOR_VALUE", lovIdMaster: id }, res => {
+        this.travelTo3List = res.json();
+      });
+    }
+  }
+
 }
