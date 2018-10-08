0d3ee560fa10c398f4676c1134c1fd09601c7e7c
flmae123flame <flame.tana@gmail.com>
2018-10-08 15:04:29 +0700

    edit mockup

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-9/int01-9-2/int01-9-2.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int01/int01-9/int01-9-2/int01-9-2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-int01-9-2',
   templateUrl: './int01-9-2.component.html',
@@ -10,6 +11,22 @@ export class Int0192Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    
   }
 
+  addEditData() {
+    $('#modal').modal('show');
+  }
+
+  closePopupaddData() {
+    $('#modal').modal('hide');
+  }
+  popupEditData() {
+    $('#modal1').modal('show');
+  }
+
+  closePopupEdit() {
+    $('#modal1').modal('hide');
+  }
 }
