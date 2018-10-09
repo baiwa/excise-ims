adb2be0c24a938f4ca5f18650ef6c9e3cb87f8ca
flukeTP <fook_1589@hotmail.com>
2018-10-09 11:39:03 +0700

    fix bug Int07-1 and Int07-2 to success

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int07/int07-2/int07-2.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int07/int07-2/int07-2.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Int072Service } from "./int07-2.service";
 import { ActivatedRoute } from "@angular/router";
 import { BreadCrumb } from "models/breadcrumb";
+import { DecimalFormat } from "helpers/decimalformat";
 
 declare var $: any;
 @Component({
@@ -14,6 +15,8 @@ export class Int072Component implements OnInit {
   breadcrumb: BreadCrumb[] = [];
   verifyAccountHeaderId: any;
   loadingTable: boolean;
+  footer: any;
+  testData: void;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,28 +25,61 @@ export class Int072Component implements OnInit {
     this.breadcrumb = [
       { label: "ตรวจสอบภายใน", route: "#" },
       { label: "ตรวจสอบบัญชี", route: "#" },
-      { label: "ตรวจสอบข้อมูลบัญชี", route: "/int07/1" }
+      { label: "ตรวจสอบข้อมูลบัญชี", route: "#" }
     ];
-  }
 
-  ngAfterViewInit() {
-    $("#export .dropdown").dropdown({
-      transition: "drop"
-    });
-  }
-
-  ngOnInit() {
+    this.footer = {
+      ledgerAccountNumber: "",
+      ledgerAccountName: "",
+      bringForward: 0.0,
+      debit: "",
+      credit: "",
+      carryForward: 0.0,
+      loadingTable: ""
+    };
     this.loadingTable = true; //Loading dataTable
+  }
+
+  ngAfterViewInit() {}
+
+  async ngOnInit() {
     this.verifyAccountHeaderId = this.route.snapshot.queryParams[
       "verifyAccountHeaderId"
     ];
-    this.selfService.onInitDataTable(
-      this.verifyAccountHeaderId,
-      this.loadTable
-    );
+    await this.selfService
+      .onInitDataTable(this.verifyAccountHeaderId)
+      .then(val => {
+        if (val) {
+          this.footer = val;
+          this.loadingTable = false;
+        }
+      });
   }
 
-  loadTable = arg => {
-    this.loadingTable = arg;
+  onExport = () => {
+    console.log("this.testData: ", this.testData);
   };
+
+  DF(what) {
+    const df = new DecimalFormat("###,###.00");
+    return df.format(what);
+  }
+
+  toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split("e-")[1]);
+      if (e) {
+        x *= Math.pow(10, e - 1);
+        x = "0." + new Array(e).join("0") + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split("+")[1]);
+      if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10, e);
+        x += new Array(e + 1).join("0");
+      }
+    }
+    return x;
+  }
 }
