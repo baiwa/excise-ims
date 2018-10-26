176749bb0a19c2926f563c05ef52a581edacc326
toffeeDev <saknarin.top21@gmail.com>
2018-10-26 16:42:48 +0700

    updateCode int076

---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/persistence/vo/Int076FormVo.java
@@ -0,0 +1,19 @@
+package th.co.baiwa.excise.ia.persistence.vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Int076FormVo {
+	private MultipartFile fileExel;
+
+	public MultipartFile getFileExel() {
+		return fileExel;
+	}
+
+	public void setFileExel(MultipartFile fileExel) {
+		this.fileExel = fileExel;
+	}
+
+
+	
+	
+}
