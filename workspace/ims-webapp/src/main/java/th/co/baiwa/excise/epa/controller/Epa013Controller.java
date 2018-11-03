1d66184bd4686b41a22bf3b61d6d0fbabdb69f97
teerawukt(ท๊อปสุดหลุด) <teerawutkt3@gmail.com>
2018-11-04 03:10:42 +0700

    ope01-3

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/epa/controller/Epa013Controller.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/epa/controller/Epa013Controller.java
@@ -6,23 +6,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import th.co.baiwa.excise.domain.datatable.DataTableAjax;
-import th.co.baiwa.excise.epa.persistence.vo.Epa013FormVo;
-import th.co.baiwa.excise.epa.persistence.vo.Epa013Vo;
-import th.co.baiwa.excise.epa.service.Epa013Service;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011DtlVo;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011FormVo;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011Vo;
+import th.co.baiwa.excise.epa.persistence.vo.InvhdrFormVo;
+import th.co.baiwa.excise.epa.service.Epa011Service;
 
 @Controller
 @RequestMapping("api/epa/epa013")
 public class Epa013Controller {
-	
-	@Autowired
-	private Epa013Service epa013Service;
 
+	@Autowired
+	private Epa011Service epa011Service;
+	
 	@PostMapping("/search")
 	@ResponseBody
-	public DataTableAjax<Epa013Vo> search(@RequestBody Epa013FormVo epa013FormVo) {
-		return epa013Service.search(epa013FormVo);
+	public DataTableAjax<Epa011Vo> search(@RequestBody Epa011FormVo epa011FormVo) {
+		return epa011Service.search(epa011FormVo);
+	}
+
+	@PostMapping("/searchDetail")
+	@ResponseBody
+	public DataTableAjax<Epa011DtlVo> searchDetail(@RequestBody Epa011FormVo epa011FormVo) {
+		return epa011Service.searchDetail(epa011FormVo);
+	}
+	
+	
+	@PostMapping("/getDetail")
+	@ResponseBody
+	public Epa011Vo getDetail(@RequestBody Epa011FormVo epa011FormVo) {
+		return epa011Service.getDetail(epa011FormVo);
+	}
+	
+	@PostMapping("/getInvDetail")
+	@ResponseBody
+	public InvhdrFormVo getInvDetail(@RequestBody InvhdrFormVo InvhdrFormVo) {
+		 return epa011Service.getInvDetail(InvhdrFormVo);
+	}
+	
+	@PostMapping("/saveInv")
+	@ResponseBody
+	public InvhdrFormVo saveInv(@RequestBody InvhdrFormVo invhdrFormVo) {
+		epa011Service.saveInv(invhdrFormVo);
+		return invhdrFormVo;
 	}
 	
 }
--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/epa/service/Epa013Service.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/epa/service/Epa013Service.java
@@ -1,36 +1,73 @@
 package th.co.baiwa.excise.epa.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import th.co.baiwa.excise.constant.ExciseConstants;
+import th.co.baiwa.excise.constant.ExciseConstants.SEARCH_FLAG;
 import th.co.baiwa.excise.domain.datatable.DataTableAjax;
 import th.co.baiwa.excise.epa.persistence.dao.ExportCheckingDao;
-import th.co.baiwa.excise.epa.persistence.vo.Epa013FormVo;
-import th.co.baiwa.excise.epa.persistence.vo.Epa013Vo;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011DtlVo;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011FormVo;
+import th.co.baiwa.excise.epa.persistence.vo.Epa011Vo;
+import th.co.baiwa.excise.epa.persistence.vo.InvhdrFormVo;
+
+import java.util.List;
 
 @Service
 public class Epa013Service {
 
 	@Autowired
 	private ExportCheckingDao exportCheckingDao;
-	
-	public DataTableAjax<Epa013Vo> search(Epa013FormVo epa013FormVo) {
-		DataTableAjax<Epa013Vo> dataTableAjax = new DataTableAjax<>();
-		
-		if (ExciseConstants.SEARCH_FLAG.TRUE.equalsIgnoreCase(epa013FormVo.getSearchFlag())) {
-			List<Epa013Vo> list = exportCheckingDao.searchForReport(epa013FormVo);
-			long count = exportCheckingDao.countForReport(epa013FormVo);
-			
-			dataTableAjax.setDraw(epa013FormVo.getDraw() + 1);
+
+	public DataTableAjax<Epa011Vo> search(Epa011FormVo epa011FormVo) {
+		DataTableAjax<Epa011Vo> dataTableAjax = new DataTableAjax<Epa011Vo>();
+
+		if (SEARCH_FLAG.TRUE.equalsIgnoreCase(epa011FormVo.getSearchFlag())) {
+			List<Epa011Vo> list = exportCheckingDao.listExportCheckingData(epa011FormVo);
+			long count = exportCheckingDao.countExportCheckingData(epa011FormVo);
+
+			dataTableAjax.setDraw(epa011FormVo.getDraw() + 1);
 			dataTableAjax.setRecordsTotal(count);
 			dataTableAjax.setRecordsFiltered(count);
 			dataTableAjax.setData(list);
 		}
-		
+
 		return dataTableAjax;
+	}
+
+	public Epa011Vo getDetail(Epa011FormVo epa011FormVo) {
+		return exportCheckingDao.getDetail(epa011FormVo.getViewId());
+	}
+
+	public DataTableAjax<Epa011DtlVo> searchDetail(Epa011FormVo epa011FormVo) {
+		DataTableAjax<Epa011DtlVo> dataTableAjax = new DataTableAjax<Epa011DtlVo>();
+
+		if (epa011FormVo.getViewId() != null ) {
+			List<Epa011DtlVo> list = exportCheckingDao.listDetailData(epa011FormVo);
+			long count = exportCheckingDao.countDetail(epa011FormVo);
+
+			dataTableAjax.setDraw(epa011FormVo.getDraw() + 1);
+			dataTableAjax.setRecordsTotal(count);
+			dataTableAjax.setRecordsFiltered(count);
+			dataTableAjax.setData(list);
+		}
+
+		return dataTableAjax;
+	}
+
+	public InvhdrFormVo getInvDetail(InvhdrFormVo invhdrFormVo) {
+
+		Epa011Vo taxhdr = exportCheckingDao.getDetail(invhdrFormVo.getHdrId());
+		invhdrFormVo.setHdrVo(taxhdr);
+		
+		Epa011DtlVo taxdtl = exportCheckingDao.getInvHdr(invhdrFormVo.getDtlId());
+		invhdrFormVo.setDtlVo(taxdtl);
+		
+		return invhdrFormVo;
+	}
+
+	public void saveInv(InvhdrFormVo invhdrFormVo) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
