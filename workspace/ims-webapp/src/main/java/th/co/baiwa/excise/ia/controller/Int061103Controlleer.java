4b86916a2307ae6c1be01bd56781821ba2cf563b
teerawukt(ท๊อปสุดหลุด) <teerawutkt3@gmail.com>
2018-11-05 16:41:14 +0700

    int06-11-3

---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/controller/Int061103Controlleer.java
@@ -0,0 +1,15 @@
+package th.co.baiwa.excise.ia.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import th.co.baiwa.excise.ia.service.Int061103Service;
+
+@Controller
+@RequestMapping("api/ia/int061103")
+public class Int061103Controlleer {
+
+	 @Autowired
+	 private Int061103Service int061103Service;
+}
---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/ia/service/Int061103Service.java
@@ -0,0 +1,9 @@
+package th.co.baiwa.excise.ia.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Int061103Service {
+
+}
+
