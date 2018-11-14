eea957dfdd442fbe1d8719658e9c1066de809e05
unknown <amtheflash1234@gmail.com>
2018-11-14 15:55:15 +0700

    add menu

---
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int06/int06-14/int06-14.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'services/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbModule } from 'components/breadcrumb/breadcrumb.module';
+import { Int0614Component } from './int06-14.component';
+
+
+const routes: Routes = [
+  { path: "", component: Int0614Component, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BreadcrumbModule
+  ],
+  declarations: [Int0614Component],
+  providers: [Int0614Component]
+})
+export class Int0614Module { }
