e0cc1a14c1daba0d91a9ef733aba1c719275ecf3
toffeeDev <saknarin.top21@gmail.com>
2018-11-14 07:46:34 +0700

    report int08-7

--- workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-7/int08-7.component.ts
+++ workspace/ims-client/src/app/buckwaframework/project/internal-audit/int08/int08-7/int08-7.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { BreadCrumb } from 'models/breadcrumb';
+import { ActivatedRoute } from '@angular/router';
+import { AjaxService } from 'services/ajax.service';
+import { TextDateTH, formatter } from 'helpers/datepicker';
+import { AuthService } from 'services/auth.service';
 
+
+declare var $: any;
 
 @Component({
   selector: 'app-int08-7',
@@ -8,16 +14,104 @@ import { BreadCrumb } from 'models/breadcrumb';
   styleUrls: ['./int08-7.component.css']
 })
 export class Int087Component implements OnInit {
+  @Output() discard = new EventEmitter<any>();
+ 
   breadcrumb: BreadCrumb[];
-  constructor() {
+  obj: data;
+  box1: Boolean;
+  box2: Boolean;
+ 
+  constructor( 
+    private route: ActivatedRoute,
+    private ajax: AjaxService,
+    private authService: AuthService,
+    ) {
     this.breadcrumb = [
       { label: "ตรวจสอบภายใน", route: "#" },
       { label: "ตรวจสอบรายได้", route: "#" },
       { label: "รายงานผลการตรวจสอบรายได้", route: "#" },
     ];
+
+    this.obj = new data();
    }
 
   ngOnInit() {
+    this.authService.reRenderVersionProgram('INT-087').then(user=>{
+    //this.user= user;
+   this.obj.officer= user.fullName;
+   this.obj.position=user.title;
+    });
+    this.calenda();
   }
 
+  calenda = () => {
+    $("#date1").calendar({
+      endCalendar: $("#date1"),
+      type: "date",
+      text: TextDateTH,
+      formatter: formatter('วดป'),
+      onChanges: (date , text)=>{
+       $("#date").val(text);
+      }
+    });
+
+  }
+
+  onSubmit = e => {
+    if(this.box1 && !this.box2){
+      console.log("1,!2")
+      this.obj.group1Flag="Y"
+      this.obj.group2Flag="N"
+    }else if(this.box2 && !this.box1){
+      console.log("!1,2")
+      this.obj.group1Flag="N"
+      this.obj.group2Flag="Y"
+    }else if(this.box1 && this.box2){
+      console.log("1,2")
+      this.obj.group1Flag="Y"
+      this.obj.group2Flag="Y"
+    }else{
+      console.log("!1,!2")
+      this.obj.group1Flag="N"
+      this.obj.group2Flag="N"
+    }
+
+
+    console.log(this.obj);
+    this.obj.date =   $("#date").val();
+    var form = document.createElement("form");
+		form.method = "POST";
+		form.action = AjaxService.CONTEXT_PATH + "internalAudit/report/pdf/int/reportCheckIncome";
+    form.style.display = "none";    
+    var jsonInput = document.createElement("input");
+    jsonInput.name = "json";
+    jsonInput.value = JSON.stringify(this.obj);
+    form.appendChild(jsonInput);
+
+    document.body.appendChild(form);
+	  form.submit();
+
+
+  }
+
+  onDiscard = () => {
+    // on click this view hide them
+    // ... Don't change or delete this
+    this.discard.emit(false);
+  };
+
 }
+
+class data {
+  logo: string = "logo1.jpg";
+  government : string;
+  date : string;   
+  study : string;
+  billLost: string;
+  sendMoney: string;
+  group1Flag: string;
+  group2Flag: string;
+  officer:string;
+  position:string;
+} 
+
---
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/controller/InternalAuditReportController.java
@@ -0,0 +1,43 @@
+package th.co.baiwa.excise.report.controller;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.sf.jasperreports.engine.JRException;
+import th.co.baiwa.excise.report.bean.ReportJsonBean;
+import th.co.baiwa.excise.report.service.InternalAuditReportService;
+
+@Controller
+@RequestMapping("api/internalAudit/report")
+public class InternalAuditReportController {
+
+	@Autowired
+	private InternalAuditReportService internalAuditReportService;
+	
+	
+	@PostMapping("/pdf/int/reportCheckIncome")
+	public void pdfTs(@ModelAttribute ReportJsonBean reportJsonBean, HttpServletResponse response) throws IOException, JRException {
+		
+		String fileName = URLEncoder.encode("รายงานผลการตรวจสอบรายได้","UTF-8") ;
+		
+		byte[] reportFile = internalAuditReportService.reportCheckIncomeToPDF(reportJsonBean);
+		
+		response.setContentType("application/pdf");
+		response.addHeader("Content-Disposition", "inline;filename="+fileName+".pdf");
+		
+		IOUtils.write(reportFile, response.getOutputStream());
+	}
+
+
+	
+	
+}
