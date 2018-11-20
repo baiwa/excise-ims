05f6b4a10c1bdae0a6180fdfe8ffec6d9203a95a
toffeeDev <saknarin.top21@gmail.com>
2018-11-20 22:49:41 +0700

    แก้คำผิด

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/controller/ExciseTaxReportController.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/controller/ExciseTaxReportController.java
@@ -19,6 +19,7 @@ import net.sf.jasperreports.engine.JRException;
 import th.co.baiwa.buckwaframework.preferences.persistence.entity.Message;
 import th.co.baiwa.excise.report.bean.ReportJsonBean;
 import th.co.baiwa.excise.report.service.TaxAuditReportService;
+import th.co.baiwa.excise.ta.persistence.entity.TaYearPlanReport;
 
 @Controller
 @RequestMapping("api/exciseTax/report")
@@ -26,27 +27,36 @@ public class ExciseTaxReportController {
 
 	@Autowired
 	private TaxAuditReportService taxAuditReportService;
-	
-	
+
 	@PostMapping("/updateFlag")
 	@ResponseBody
 	public Message updateRiskAssInfHdr(@RequestBody BigDecimal id) {
 		return taxAuditReportService.updateFlag(id);
 	}
-	
-	
+
 	@PostMapping("/pdf/tax/checkExciseTax")
-	public void pdfTs(@ModelAttribute ReportJsonBean reportJsonBean, HttpServletResponse response) throws IOException, JRException {
+	public void pdfTs(@ModelAttribute ReportJsonBean reportJsonBean, HttpServletResponse response)
+			throws IOException, JRException {
 		byte[] reportFile = taxAuditReportService.exciseTaxToPDF(reportJsonBean);
-		
-		String fileName = URLEncoder.encode("รายงานผลการวิเคราะห์ข้อมูล","UTF-8") ;
-		
+
+		String fileName = URLEncoder.encode("รายงานผลการวิเคราะห์ข้อมูล", "UTF-8");
+
 		response.setContentType("application/pdf");
-		response.addHeader("Content-Disposition", "inline;filename="+fileName+".pdf");
-		
+		response.addHeader("Content-Disposition", "inline;filename=" + fileName + ".pdf");
+
 		IOUtils.write(reportFile, response.getOutputStream());
 	}
-	
 
-	
+	@PostMapping("/saveReport")
+	@ResponseBody
+	public void saveReport(@RequestBody TaYearPlanReport taYearPlanReport) {
+		taxAuditReportService.saveTaYearPlanReport(taYearPlanReport);
+	}
+
+	@PostMapping("/dataReport")
+	@ResponseBody
+	public TaYearPlanReport dataReport(@RequestBody Long id) {
+		return taxAuditReportService.dataReport(id);
+	}
+
 }
