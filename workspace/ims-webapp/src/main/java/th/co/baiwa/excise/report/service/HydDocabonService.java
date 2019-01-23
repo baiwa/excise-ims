459665f8bb18085b227127a8d98e85e04b5e56a5
flame123flame <flame.tana@gmail.com>
2019-01-23 13:47:02 +0700

    add jasper

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/service/HydDocabonService.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/service/HydDocabonService.java
@@ -30,6 +30,7 @@ public class HydDocabonService {
 		JasperPrint jasperPrint1 = ReportUtils.exportReport("Report_01", params, new JREmptyDataSource());
 		JasperPrint jasperPrint2 = ReportUtils.exportReport("Report_05", params, new JREmptyDataSource());
 		JasperPrint jasperPrint3 = ReportUtils.exportReport("Report_03", params, new JREmptyDataSource());
+		JasperPrint jasperPrint8 = ReportUtils.exportReport("Report_07", params, new JREmptyDataSource());
 		JasperPrint jasperPrint4 = ReportUtils.exportReport("Report_04", params, new JREmptyDataSource());
 		JasperPrint jasperPrint5 = ReportUtils.exportReport("Report_02", params, new JREmptyDataSource());
 		JasperPrint jasperPrint6 = ReportUtils.exportReport("Solvent-01", params, new JREmptyDataSource());
@@ -44,7 +45,7 @@ public class HydDocabonService {
 		items.add(new SimpleExporterInputItem(jasperPrint5));
 		items.add(new SimpleExporterInputItem(jasperPrint6));
 		items.add(new SimpleExporterInputItem(jasperPrint7));		
-		
+		items.add(new SimpleExporterInputItem(jasperPrint8));		
 		
 		JRPdfExporter exporter = new JRPdfExporter();
 		exporter.setExporterInput(new SimpleExporterInput(items));
