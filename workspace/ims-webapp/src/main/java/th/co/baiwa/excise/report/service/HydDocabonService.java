d4ff53a844150de27bbfa688ef49ecb660e34609
flame123flame <flame.tana@gmail.com>
2019-01-23 18:55:50 +0700

    add jasper

--- workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/service/HydDocabonService.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/excise/report/service/HydDocabonService.java
@@ -26,27 +26,26 @@ public class HydDocabonService {
 	public byte[] objectToPDF(ReportJsonBean reportJsonBean) throws IOException, JRException {
 
 		Map<String, Object> params = new HashMap<String, Object>();
-		
 		JasperPrint jasperPrint1 = ReportUtils.exportReport("Report_01", params, new JREmptyDataSource());
 		JasperPrint jasperPrint2 = ReportUtils.exportReport("Report_05", params, new JREmptyDataSource());
-		JasperPrint jasperPrint3 = ReportUtils.exportReport("Report_03", params, new JREmptyDataSource());
-		JasperPrint jasperPrint8 = ReportUtils.exportReport("Report_07", params, new JREmptyDataSource());
-		JasperPrint jasperPrint4 = ReportUtils.exportReport("Report_04", params, new JREmptyDataSource());
-		JasperPrint jasperPrint5 = ReportUtils.exportReport("Report_02", params, new JREmptyDataSource());
-		JasperPrint jasperPrint6 = ReportUtils.exportReport("Solvent-01", params, new JREmptyDataSource());
-		JasperPrint jasperPrint7 = ReportUtils.exportReport("Solvent-02", params, new JREmptyDataSource());
-		
-		
+		JasperPrint jasperPrint3 = ReportUtils.exportReport("Report_07", params, new JREmptyDataSource());
+		JasperPrint jasperPrint4 = ReportUtils.exportReport("Report_02", params, new JREmptyDataSource());
+		JasperPrint jasperPrint5 = ReportUtils.exportReport("Report_03", params, new JREmptyDataSource());
+		JasperPrint jasperPrint6 = ReportUtils.exportReport("Report_04", params, new JREmptyDataSource());
+		JasperPrint jasperPrint7 = ReportUtils.exportReport("Solvent-01", params, new JREmptyDataSource());
+		JasperPrint jasperPrint8 = ReportUtils.exportReport("Solvent-02", params, new JREmptyDataSource());
+
 		List<ExporterInputItem> items = new ArrayList<ExporterInputItem>();
+
 		items.add(new SimpleExporterInputItem(jasperPrint1));
 		items.add(new SimpleExporterInputItem(jasperPrint2));
 		items.add(new SimpleExporterInputItem(jasperPrint3));
 		items.add(new SimpleExporterInputItem(jasperPrint4));
 		items.add(new SimpleExporterInputItem(jasperPrint5));
 		items.add(new SimpleExporterInputItem(jasperPrint6));
-		items.add(new SimpleExporterInputItem(jasperPrint7));		
-		items.add(new SimpleExporterInputItem(jasperPrint8));		
-		
+		items.add(new SimpleExporterInputItem(jasperPrint7));
+		items.add(new SimpleExporterInputItem(jasperPrint8));
+
 		JRPdfExporter exporter = new JRPdfExporter();
 		exporter.setExporterInput(new SimpleExporterInput(items));
 
