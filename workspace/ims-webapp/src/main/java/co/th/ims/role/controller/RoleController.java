1e86557cebbf4546bfe4d68151259c96ec4be855
teerawut(ท๊อปสุดหล่อ) <teerawutkt3@gmail.com>
2019-01-30 11:27:06 +0700

    add message contant

--- workspace/ims-webapp/src/main/java/co/th/ims/role/controller/RoleController.java
+++ workspace/ims-webapp/src/main/java/co/th/ims/role/controller/RoleController.java
@@ -1,16 +1,17 @@
 package co.th.ims.role.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import co.th.baiwa.buckwaframework.common.constant.MessageContants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import co.th.baiwa.buckwaframework.common.bean.ResponseData;
 import co.th.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_STATUS;
 import co.th.ims.role.domain.Role;
 import co.th.ims.role.service.RoleService;
+import springfox.documentation.service.ResponseMessage;
 
 @RequestMapping("/api/role")
 @RestController
@@ -26,13 +27,44 @@ public class RoleController {
 
 		try {
 			response.setData(roleService.findAll());
-			response.setMessage("SUCCESS");
+			response.setMessage(MessageContants.STATUS.SUCCESS);
 			response.setStatus(RESPONSE_STATUS.SUCCESS);
 		} catch (Exception e) {
-			response.setMessage("FAIL");
+			response.setMessage(MessageContants.STATUS.FAILED);
 			response.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return response;
+	}
+
+	@PutMapping("/update")
+	public ResponseData<Role> update(@RequestBody Role role){
+		ResponseData<Role> response = new ResponseData<Role>();
+		try {
+			roleService.update(role);
+			response.setStatus(RESPONSE_STATUS.SUCCESS);
+			response.setMessage(MessageContants.STATUS.SUCCESS);
+		}catch (Exception e){
+			response.setStatus(RESPONSE_STATUS.FAILED);
+			response.setMessage(MessageContants.STATUS.FAILED);
 		}
 
 		return response;
+	}
+
+	@PostMapping("/create")
+	public ResponseData<Role> create(@RequestBody Role role){
+		ResponseData<Role> responseData = new ResponseData<>();
+
+		try {
+			roleService.create(role);
+			responseData.setMessage(MessageContants.STATUS.SUCCESS);
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+		}catch (Exception e){
+			e.printStackTrace();
+			responseData.setMessage(MessageContants.STATUS.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+
+		return responseData;
 	}
 }
--- workspace/ims-webapp/src/main/java/co/th/ims/role/service/RoleService.java
+++ workspace/ims-webapp/src/main/java/co/th/ims/role/service/RoleService.java
@@ -3,6 +3,7 @@ package co.th.ims.role.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import co.th.baiwa.buckwaframework.security.util.UserLoginUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,16 @@ public class RoleService {
 	private RoleDao roleDao;
 	
 	public List<Role> findAll() {
-		List<Role> role = new ArrayList<Role>();
-		role = roleDao.findAll();
+		List<Role> role = roleDao.findAll();
 		return role;
+	}
+
+	public void update(Role role){
+		roleDao.update(role);
+	}
+
+	public void create(Role role){
+		String createBy = UserLoginUtils.getCurrentUsername();
+		roleDao.create(role, createBy);
 	}
 }
