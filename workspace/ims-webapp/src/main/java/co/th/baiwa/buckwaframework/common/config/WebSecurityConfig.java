2e8e4e14bac7a5282613ba6a5aee16a8e3abd8da
ryangek <artistkanjai1@gmail.com>
2019-01-30 16:05:40 +0700

    web security config

--- workspace/ims-webapp/src/main/java/co/th/baiwa/buckwaframework/common/config/WebSecurityConfig.java
+++ workspace/ims-webapp/src/main/java/co/th/baiwa/buckwaframework/common/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/","/onlogout","/app/**").permitAll()
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.anyRequest().authenticated().and().formLogin().permitAll()
 				.successForwardUrl("/onloginsuccess")
 				.failureHandler(customfailHandler()).and().logout().permitAll().and().csrf().disable();
 		
