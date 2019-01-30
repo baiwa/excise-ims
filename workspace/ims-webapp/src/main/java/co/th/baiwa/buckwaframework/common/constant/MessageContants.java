3079759c3d24b1ab11b6d483d573706b8ab5d6e9
teerawut(ท๊อปสุดหล่อ) <teerawutkt3@gmail.com>
2019-01-30 10:19:15 +0700

    add message contant

--- workspace/ims-webapp/src/main/java/co/th/baiwa/buckwaframework/common/constant/MessageContants.java
+++ workspace/ims-webapp/src/main/java/co/th/baiwa/buckwaframework/common/constant/MessageContants.java
@@ -2,8 +2,8 @@ package co.th.baiwa.buckwaframework.common.constant;
 
 public class MessageContants {
 
-    public static final class STATUS {
-        public static final String SUCCEED = "SUCCEED";
-        public static final String FAILED = "FAILED";
-    }
+public static final class STATUS {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILED = "FAILED";
+}
 }
