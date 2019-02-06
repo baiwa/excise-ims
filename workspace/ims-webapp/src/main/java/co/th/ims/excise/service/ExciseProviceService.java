9b7db74bb50dcdbf68835ab58eb22ba27b82ab43
BaiwaDev <pavit@baiwa.co.th>
2019-02-06 14:59:13 +0700

    commit geo and provice

---
+++ workspace/ims-webapp/src/main/java/co/th/ims/excise/service/ExciseProviceService.java
@@ -0,0 +1,20 @@
+package co.th.ims.excise.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.th.ims.excise.dao.ExciseProviceDao;
+import co.th.ims.excise.domain.ExciseProvice;
+
+@Service
+public class ExciseProviceService {
+
+	@Autowired
+	private ExciseProviceDao exciseProviceDao;
+	
+	public List<ExciseProvice> findProviceByCriteria(ExciseProvice exciseProvice){
+		return exciseProviceDao.findbyCriteria(exciseProvice);
+	}
+}
