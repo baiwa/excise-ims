75b09bf6df0949fed6afa7d5a6aee0d6742626d2
phattarapong58021725 <phattarapong_i@hotmail.com>
2019-02-14 17:45:03 +0700

    delete

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/entity/IaRiskFactors.java
@@ -0,0 +1,124 @@
+
+package th.go.excise.ims.ia.persistence.entity;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import th.co.baiwa.buckwaframework.common.persistence.entity.BaseEntity;
+
+@Entity
+@Table(name = "IA_RISK_FACTORS")
+public class IaRiskFactors
+    extends BaseEntity
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "IA_RISK_FACTORS_GEN")
+    @SequenceGenerator(name = "IA_RISK_FACTORS_GEN", sequenceName = "IA_RISK_FACTORS_SEQ", allocationSize = 1)
+    @Column(name = "ID")
+    private BigDecimal id;
+    @Column(name = "RISK_FACTORS")
+    private String riskFactors;
+    @Column(name = "BUDGET_YEAR")
+    private String budgetYear;
+    @Column(name = "SIDE")
+    private String side;
+    @Column(name = "FACTORS_LOW")
+    private String factorsLow;
+    @Column(name = "FACTORS_MEDIUM")
+    private String factorsMedium;
+    @Column(name = "FACTORS_HIGH")
+    private String factorsHigh;
+    @Column(name = "STATUS_SCREEN")
+    private String statusScreen;
+    @Column(name = "DATE_CRITERIA")
+    private String dateCriteria;
+    @Column(name = "STATUS_CHICKING")
+    private BigDecimal statusChicking;
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getRiskFactors() {
+        return riskFactors;
+    }
+
+    public void setRiskFactors(String riskFactors) {
+        this.riskFactors = riskFactors;
+    }
+
+    public String getBudgetYear() {
+        return budgetYear;
+    }
+
+    public void setBudgetYear(String budgetYear) {
+        this.budgetYear = budgetYear;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public String getFactorsLow() {
+        return factorsLow;
+    }
+
+    public void setFactorsLow(String factorsLow) {
+        this.factorsLow = factorsLow;
+    }
+
+    public String getFactorsMedium() {
+        return factorsMedium;
+    }
+
+    public void setFactorsMedium(String factorsMedium) {
+        this.factorsMedium = factorsMedium;
+    }
+
+    public String getFactorsHigh() {
+        return factorsHigh;
+    }
+
+    public void setFactorsHigh(String factorsHigh) {
+        this.factorsHigh = factorsHigh;
+    }
+
+    public String getStatusScreen() {
+        return statusScreen;
+    }
+
+    public void setStatusScreen(String statusScreen) {
+        this.statusScreen = statusScreen;
+    }
+
+    public String getDateCriteria() {
+        return dateCriteria;
+    }
+
+    public void setDateCriteria(String dateCriteria) {
+        this.dateCriteria = dateCriteria;
+    }
+
+    public BigDecimal getStatusChicking() {
+        return statusChicking;
+    }
+
+    public void setStatusChicking(BigDecimal statusChicking) {
+        this.statusChicking = statusChicking;
+    }
+
+}
