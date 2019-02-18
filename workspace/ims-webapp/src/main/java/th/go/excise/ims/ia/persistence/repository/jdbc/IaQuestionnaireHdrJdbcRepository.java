01d746eadf126fb7497486ab7ee0522da10244b8
flukeTP <fook_1589@hotmail.com>
2019-02-18 16:36:15 +0700

    converting date to string to return and change type return data

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaQuestionnaireHdrJdbcRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaQuestionnaireHdrJdbcRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import th.co.baiwa.buckwaframework.common.persistence.jdbc.CommonJdbcTemplate;
 import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
-import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireHdr;
+import th.go.excise.ims.ia.vo.Int02FormVo;
 import th.go.excise.ims.ia.vo.Int02Vo;
 
 @Repository
@@ -19,7 +19,7 @@ public class IaQuestionnaireHdrJdbcRepository {
 	@Autowired
 	private CommonJdbcTemplate commonJdbcTemplate;
 	
-	public List<IaQuestionnaireHdr> getDataFilter(Int02Vo request) {
+	public List<Int02Vo> getDataFilter(Int02FormVo request) {
 		StringBuilder sql = new StringBuilder();
 		List<Object> params = new ArrayList<Object>();
 		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_HDR WHERE IS_DELETED='N' ");
@@ -35,16 +35,17 @@ public class IaQuestionnaireHdrJdbcRepository {
 		}
 		
 		if(StringUtils.isNotBlank(request.getStartDate()) && StringUtils.isNotBlank(request.getEndDate())) {
-			sql.append(" AND CREATED_DATE >= ? ");
-			sql.append(" AND CREATED_DATE <= ? ");
+			sql.append(" AND TRUNC(CREATED_DATE) >= ? ");
+			sql.append(" AND TRUNC(CREATED_DATE) <= ? ");
 			/* convert string to date */
 			params.add(ConvertDateUtils.parseStringToDate(request.getStartDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
 			params.add(ConvertDateUtils.parseStringToDate(request.getEndDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
 		}
+		sql.append(" ORDER BY CREATED_DATE ASC");
 
 		@SuppressWarnings({ "rawtypes", "unchecked" })
-		List<IaQuestionnaireHdr> datas = this.commonJdbcTemplate.query(sql.toString(), params.toArray(), new BeanPropertyRowMapper(IaQuestionnaireHdr.class));
+		List<Int02Vo> datas = this.commonJdbcTemplate.query(sql.toString(), params.toArray(), new BeanPropertyRowMapper(Int02Vo.class));
 
-		return datas;
+		return datas; 
 	}
 }
