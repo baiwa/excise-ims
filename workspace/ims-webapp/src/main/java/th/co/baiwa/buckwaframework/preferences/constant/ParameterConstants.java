da11495ee1db92e932c85ccd153a909f6d3e0544
taechapon <taechapon@gmail.com>
2019-02-22 19:51:27 +0700

    Update Constants

--- workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/preferences/constant/ParameterConstants.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/preferences/constant/ParameterConstants.java
@@ -5,6 +5,11 @@ public class ParameterConstants {
 	// All Parameter Group
 	public static final class PARAMETER_GROUP {
 		public static final String SYSTEM_CONFIG = "SYSTEM_CONFIG";
+		public static final String EXCISE_TAX_TYPE = "EXCISE_TAX_TYPE";
+		public static final String EXCISE_PRODUCT_TYPE = "EXCISE_PRODUCT_TYPE";
+		public static final String EXCISE_SERVICE_TYPE = "EXCISE_SERVICE_TYPE";
+		public static final String EXCISE_FACTORY_TYPE = "EXCISE_FACTORY_TYPE";
+		public static final String TA_MAS_COND_MAIN_DESC = "TA_MAS_COND_MAIN_DESC";
 	}
 	
 	// Parameter Group: SYSTEM_CONFIG
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/common/constant/ProjectConstants.java
@@ -0,0 +1,10 @@
+package th.go.excise.ims.common.constant;
+
+public class ProjectConstants {
+	
+	public class TA_MAS_COND_MAIN_TYPE {
+		public static final String TAX = "T";
+		public static final String OTHER = "O";
+	}
+	
+}
