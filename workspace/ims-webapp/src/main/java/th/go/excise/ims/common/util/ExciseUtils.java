9c4f881cbf232e974ddcefb7800e7a7567618308
taechapon <taechapon@gmail.com>
2019-02-25 19:01:35 +0700

    Initial ExciseUtils

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/common/util/ExciseUtils.java
@@ -0,0 +1,21 @@
+package th.go.excise.ims.common.util;
+
+public class ExciseUtils {
+	
+	public static boolean isCentral(String officeCode) {
+		return false;
+	}
+	
+	public static boolean isSector(String officeCode) {
+		return false;
+	}
+	
+	public static boolean isArea(String officeCode) {
+		return false;
+	}
+	
+	public static boolean isBranch(String officeCode) {
+		return false;
+	}
+	
+}
