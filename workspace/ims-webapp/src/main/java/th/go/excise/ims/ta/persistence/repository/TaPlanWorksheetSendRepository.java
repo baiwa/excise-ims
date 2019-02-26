12f929e222535f9914a09b7806f5fcf033ad3532
taechapon <taechapon@gmail.com>
2019-02-26 17:00:42 +0700

    Initial WorksheetSendService

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaPlanWorksheetSendRepository.java
@@ -0,0 +1,8 @@
+package th.go.excise.ims.ta.persistence.repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.ta.persistence.entity.TaPlanWorksheetSend;
+
+public interface TaPlanWorksheetSendRepository extends CommonJpaCrudRepository<TaPlanWorksheetSend, Long> {
+
+}
