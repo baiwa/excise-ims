8cc144a4a14f749ad767749f1a81b1832a3514cd
flukeTP <fook_1589@hotmail.com>
2019-02-26 17:19:31 +0700

    filter by divide each sector after send questionnaire

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaQuestionnaireMadeHdrJdbcRepository.java
@@ -0,0 +1,89 @@
+package th.go.excise.ims.ia.persistence.repository.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.stereotype.Repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.jdbc.CommonJdbcTemplate;
+import th.co.baiwa.buckwaframework.common.persistence.util.OracleUtils;
+import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
+import th.go.excise.ims.ia.vo.Int0202FormVo;
+import th.go.excise.ims.ia.vo.Int0202Vo;
+
+@Repository
+public class IaQuestionnaireMadeHdrJdbcRepository {
+	
+	@Autowired
+	private CommonJdbcTemplate commonJdbcTemplate;
+	
+	public List<Int0202Vo> getDataFilter(Int0202FormVo request) {
+		StringBuilder sql = new StringBuilder();
+		List<Object> params = new ArrayList<Object>();
+		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_MADE_HDR WHERE 1=1 AND IS_DELETED='N' ");
+		sql.append(" AND OFFICE_CODE LIKE ? ");
+		params.add(request.getOfficeCode().substring(0, 2) + "%");
+		
+		if(StringUtils.isNotBlank(request.getBudgetYear())) {
+			sql.append(" AND BUDGET_YEAR = ? ");
+			params.add(request.getBudgetYear());
+		}
+		
+		if(StringUtils.isNotBlank(request.getCreatedBy())) {
+			sql.append(" AND CREATED_BY LIKE ?");
+			params.add(request.getCreatedBy() + "%");
+		}
+		
+		if(StringUtils.isNotBlank(request.getStartDate()) && StringUtils.isNotBlank(request.getEndDate())) {
+			sql.append(" AND TRUNC(CREATED_DATE) >= ? ");
+			sql.append(" AND TRUNC(CREATED_DATE) <= ? ");
+			/* convert string to date */
+			params.add(ConvertDateUtils.parseStringToDate(request.getStartDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+			params.add(ConvertDateUtils.parseStringToDate(request.getEndDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+		}
+ 		sql.append(" ORDER BY CREATED_DATE ASC");
+
+		String limit = OracleUtils.limitForDatable(sql.toString(), request.getStart(), request.getLength());
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		List<Int0202Vo> datas = this.commonJdbcTemplate.query(limit, params.toArray(), new BeanPropertyRowMapper(Int0202Vo.class));
+
+		return datas; 
+	}
+	
+	/* count datatable */
+	public Integer countDatafilter(Int0202FormVo request) {
+		StringBuilder sql = new StringBuilder();
+		List<Object> params = new ArrayList<Object>();
+		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_MADE_HDR WHERE IS_DELETED='N' ");
+		sql.append(" AND OFFICE_CODE LIKE ? ");
+		params.add(request.getOfficeCode().substring(0, 2) + "%");
+		
+		if(StringUtils.isNotBlank(request.getBudgetYear())) {
+			sql.append(" AND BUDGET_YEAR = ? ");
+			params.add(request.getBudgetYear());
+		}
+		
+		if(StringUtils.isNotBlank(request.getCreatedBy())) {
+			sql.append(" AND CREATED_BY LIKE ? ");
+			params.add(request.getCreatedBy() + "%");
+		}
+		
+		if(StringUtils.isNotBlank(request.getStartDate()) && StringUtils.isNotBlank(request.getEndDate())) {
+			sql.append(" AND TRUNC(CREATED_DATE) >= ? ");
+			sql.append(" AND TRUNC(CREATED_DATE) <= ? ");
+			/* convert string to date */
+			params.add(ConvertDateUtils.parseStringToDate(request.getStartDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+			params.add(ConvertDateUtils.parseStringToDate(request.getEndDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+		}
+		sql.append(" ORDER BY CREATED_DATE ASC");
+
+		String sqlCount = OracleUtils.countForDataTable(sql.toString());
+		Integer count = this.commonJdbcTemplate.queryForObject(sqlCount, params.toArray(), Integer.class);
+
+		return count; 
+	}
+
+}
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0202Vo.java
@@ -0,0 +1,47 @@
+package th.go.excise.ims.ia.vo;
+
+import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireMadeHdr;
+
+public class Int0202Vo extends IaQuestionnaireMadeHdr {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3508854591072907540L;
+	private String createdDateStr;
+	private String updatedDateStr;
+	private String startDateStr;
+	private String endDateStr;
+
+	public String getCreatedDateStr() {
+		return createdDateStr;
+	}
+
+	public void setCreatedDateStr(String createdDateStr) {
+		this.createdDateStr = createdDateStr;
+	}
+
+	public String getUpdatedDateStr() {
+		return updatedDateStr;
+	}
+
+	public void setUpdatedDateStr(String updatedDateStr) {
+		this.updatedDateStr = updatedDateStr;
+	}
+
+	public String getStartDateStr() {
+		return startDateStr;
+	}
+
+	public void setStartDateStr(String startDateStr) {
+		this.startDateStr = startDateStr;
+	}
+
+	public String getEndDateStr() {
+		return endDateStr;
+	}
+
+	public void setEndDateStr(String endDateStr) {
+		this.endDateStr = endDateStr;
+	}
+
+}
