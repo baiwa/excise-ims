c7a140bf8027670138dfd20b9dd6f9631692d261
taechapon <taechapon@gmail.com>
2019-02-28 15:23:18 +0700

    Entity and Repository for MasCondSub

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaMasCondSubRiskRepository.java
@@ -0,0 +1,8 @@
+package th.go.excise.ims.ta.persistence.repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.ta.persistence.entity.TaMasCondSubRisk;
+
+public interface TaMasCondSubRiskRepository extends CommonJpaCrudRepository<TaMasCondSubRisk, Long> {
+
+}
