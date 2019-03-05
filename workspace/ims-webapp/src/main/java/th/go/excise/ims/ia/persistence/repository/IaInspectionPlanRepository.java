93ca53dcb0506bf26c619a0c1c79943c1fc2af3d
flukeTP <fook_1589@hotmail.com>
2019-03-05 12:29:48 +0700

    change name table

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/IaInspectionPlanRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/IaInspectionPlanRepository.java
@@ -3,11 +3,8 @@ package th.go.excise.ims.ia.persistence.repository;
 
 import java.math.BigDecimal;
 import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
-import th.go.excise.ims.ia.persistence.entity.IaExaminationPlan;
+import th.go.excise.ims.ia.persistence.entity.IaInspectionPlan;
 
-public interface IaExaminationPlanRepository
-    extends CommonJpaCrudRepository<IaExaminationPlan, BigDecimal>
-{
-
+public interface IaInspectionPlanRepository extends CommonJpaCrudRepository<IaInspectionPlan, BigDecimal> {
 
 }
