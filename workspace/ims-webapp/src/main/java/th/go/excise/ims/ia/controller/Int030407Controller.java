d4b580bbe1f89530d811e73052c832a549d6efc6
ryangek <artistkanjai1@gmail.com>
2019-03-05 18:55:41 +0700

    Int030407Vo

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int030407Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int030407Controller.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import th.co.baiwa.buckwaframework.common.bean.ResponseData;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_STATUS;
-import th.go.excise.ims.ia.persistence.entity.IaRiskIncomePerform;
 import th.go.excise.ims.ia.service.Int030407Service;
+import th.go.excise.ims.ia.vo.Int030407Vo;
 
 @Controller
 @RequestMapping("/api/ia/int03/04/07")
@@ -26,12 +26,12 @@ public class Int030407Controller {
 	@Autowired
 	private Int030407Service int030407Service;
 	
-	@GetMapping("/year/{budgetYear}")
+	@GetMapping("/year/{budgetYear}/{idConfig}")
 	@ResponseBody
-	public ResponseData<List<IaRiskIncomePerform>> findByYear(@PathVariable("budgetYear") String budgetYear) {
-		ResponseData<List<IaRiskIncomePerform>> response = new ResponseData<List<IaRiskIncomePerform>>();
+	public ResponseData<List<Int030407Vo>> findByYear(@PathVariable("budgetYear") String budgetYear, @PathVariable("idConfig") String idConfig) {
+		ResponseData<List<Int030407Vo>> response = new ResponseData<List<Int030407Vo>>();
 		try {
-			List<IaRiskIncomePerform> res = int030407Service.findByBudgetYear(budgetYear);
+			List<Int030407Vo> res = int030407Service.findByBudgetYear(budgetYear, idConfig);
 			response.setData(res);
 			response.setMessage(ProjectConstant.RESPONSE_MESSAGE.SUCCESS);
 			response.setStatus(RESPONSE_STATUS.SUCCESS);
