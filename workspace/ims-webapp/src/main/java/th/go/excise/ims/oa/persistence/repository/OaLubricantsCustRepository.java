c18908aaab4e2a6b8eef906e8aacedc73fc208f7
ryangek <artistkanjai1@gmail.com>
2019-03-14 14:17:45 +0700

    OaLubricants*

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/oa/persistence/repository/OaLubricantsCustRepository.java
@@ -0,0 +1,13 @@
+
+package th.go.excise.ims.oa.persistence.repository;
+
+import java.math.BigDecimal;
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.oa.persistence.entity.OaLubricantsCust;
+
+public interface OaLubricantsCustRepository
+    extends CommonJpaCrudRepository<OaLubricantsCust, BigDecimal>
+{
+
+
+}
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/oa/vo/Oa020106DtlVo.java
@@ -0,0 +1,529 @@
+package th.go.excise.ims.oa.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Oa020106DtlVo {
+	private BigDecimal oaLubricantsDtlId;
+	private BigDecimal oaLubricantsId;
+	private String officePlaceOwner;
+	private BigDecimal officeRentAmount;
+	private Date workingStartDate;
+	private Date workingEndDate;
+	private BigDecimal workdayPermonth;
+	private BigDecimal numberOfTank;
+	private String tankCapacity;
+	private BigDecimal numberUtility;
+	private String orderType;
+	private String orderPayMethod;
+	private BigDecimal employeePermanent;
+	private BigDecimal employeeTemporary;
+	private String payMethodOther;
+	private String dailyAcc;
+	private String dailyAccDoc;
+	private String dailyAuditRemark;
+	private String monthlyAcc;
+	private String monthlyAccDoc;
+	private String monthlyAuditRemark;
+	private String monthlyAcc04;
+	private String monthlyAccDoc04;
+	private String monthlyAuditRemark04;
+	private String materail;
+	private BigDecimal document;
+	private String productProcess;
+	private String productNextime;
+	private Date useStartDate;
+	private Date useEndDate;
+	private String buyOverlimit;
+	private String buyFromIndust;
+	private String buyIndustLicense;
+	private String buyFromImporter;
+	private String importerLicense;
+	private String buyFromAgent;
+	private String buyAgentLicense;
+	private String salerType;
+	private String salerLicense;
+	private String usedType;
+	private String usedRemark;
+	private BigDecimal numOfCust;
+	private String goodQuality;
+	private Date agentStartDate;
+	private Date agentEndDate;
+	private String agentOverlimit;
+	private String ABuyFromIndust;
+	private String ABuyIndustLicense;
+	private String ABuyFromImporter;
+	private String AImporterLicense;
+	private String ABuyFromAgent;
+	private String ABuyAgentLicense;
+	private String ASaleToAgent;
+	private String ASaleAgentLicense;
+	private String ASaleToUser;
+	private String ASaleUserLicense;
+	private String sentToAgent;
+	private String sentToUser;
+
+	public BigDecimal getOaLubricantsDtlId() {
+		return oaLubricantsDtlId;
+	}
+
+	public void setOaLubricantsDtlId(BigDecimal oaLubricantsDtlId) {
+		this.oaLubricantsDtlId = oaLubricantsDtlId;
+	}
+
+	public BigDecimal getOaLubricantsId() {
+		return oaLubricantsId;
+	}
+
+	public void setOaLubricantsId(BigDecimal oaLubricantsId) {
+		this.oaLubricantsId = oaLubricantsId;
+	}
+
+	public String getOfficePlaceOwner() {
+		return officePlaceOwner;
+	}
+
+	public void setOfficePlaceOwner(String officePlaceOwner) {
+		this.officePlaceOwner = officePlaceOwner;
+	}
+
+	public BigDecimal getOfficeRentAmount() {
+		return officeRentAmount;
+	}
+
+	public void setOfficeRentAmount(BigDecimal officeRentAmount) {
+		this.officeRentAmount = officeRentAmount;
+	}
+
+	public Date getWorkingStartDate() {
+		return workingStartDate;
+	}
+
+	public void setWorkingStartDate(Date workingStartDate) {
+		this.workingStartDate = workingStartDate;
+	}
+
+	public Date getWorkingEndDate() {
+		return workingEndDate;
+	}
+
+	public void setWorkingEndDate(Date workingEndDate) {
+		this.workingEndDate = workingEndDate;
+	}
+
+	public BigDecimal getWorkdayPermonth() {
+		return workdayPermonth;
+	}
+
+	public void setWorkdayPermonth(BigDecimal workdayPermonth) {
+		this.workdayPermonth = workdayPermonth;
+	}
+
+	public BigDecimal getNumberOfTank() {
+		return numberOfTank;
+	}
+
+	public void setNumberOfTank(BigDecimal numberOfTank) {
+		this.numberOfTank = numberOfTank;
+	}
+
+	public String getTankCapacity() {
+		return tankCapacity;
+	}
+
+	public void setTankCapacity(String tankCapacity) {
+		this.tankCapacity = tankCapacity;
+	}
+
+	public BigDecimal getNumberUtility() {
+		return numberUtility;
+	}
+
+	public void setNumberUtility(BigDecimal numberUtility) {
+		this.numberUtility = numberUtility;
+	}
+
+	public String getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+
+	public String getOrderPayMethod() {
+		return orderPayMethod;
+	}
+
+	public void setOrderPayMethod(String orderPayMethod) {
+		this.orderPayMethod = orderPayMethod;
+	}
+
+	public BigDecimal getEmployeePermanent() {
+		return employeePermanent;
+	}
+
+	public void setEmployeePermanent(BigDecimal employeePermanent) {
+		this.employeePermanent = employeePermanent;
+	}
+
+	public BigDecimal getEmployeeTemporary() {
+		return employeeTemporary;
+	}
+
+	public void setEmployeeTemporary(BigDecimal employeeTemporary) {
+		this.employeeTemporary = employeeTemporary;
+	}
+
+	public String getPayMethodOther() {
+		return payMethodOther;
+	}
+
+	public void setPayMethodOther(String payMethodOther) {
+		this.payMethodOther = payMethodOther;
+	}
+
+	public String getDailyAcc() {
+		return dailyAcc;
+	}
+
+	public void setDailyAcc(String dailyAcc) {
+		this.dailyAcc = dailyAcc;
+	}
+
+	public String getDailyAccDoc() {
+		return dailyAccDoc;
+	}
+
+	public void setDailyAccDoc(String dailyAccDoc) {
+		this.dailyAccDoc = dailyAccDoc;
+	}
+
+	public String getDailyAuditRemark() {
+		return dailyAuditRemark;
+	}
+
+	public void setDailyAuditRemark(String dailyAuditRemark) {
+		this.dailyAuditRemark = dailyAuditRemark;
+	}
+
+	public String getMonthlyAcc() {
+		return monthlyAcc;
+	}
+
+	public void setMonthlyAcc(String monthlyAcc) {
+		this.monthlyAcc = monthlyAcc;
+	}
+
+	public String getMonthlyAccDoc() {
+		return monthlyAccDoc;
+	}
+
+	public void setMonthlyAccDoc(String monthlyAccDoc) {
+		this.monthlyAccDoc = monthlyAccDoc;
+	}
+
+	public String getMonthlyAuditRemark() {
+		return monthlyAuditRemark;
+	}
+
+	public void setMonthlyAuditRemark(String monthlyAuditRemark) {
+		this.monthlyAuditRemark = monthlyAuditRemark;
+	}
+
+	public String getMonthlyAcc04() {
+		return monthlyAcc04;
+	}
+
+	public void setMonthlyAcc04(String monthlyAcc04) {
+		this.monthlyAcc04 = monthlyAcc04;
+	}
+
+	public String getMonthlyAccDoc04() {
+		return monthlyAccDoc04;
+	}
+
+	public void setMonthlyAccDoc04(String monthlyAccDoc04) {
+		this.monthlyAccDoc04 = monthlyAccDoc04;
+	}
+
+	public String getMonthlyAuditRemark04() {
+		return monthlyAuditRemark04;
+	}
+
+	public void setMonthlyAuditRemark04(String monthlyAuditRemark04) {
+		this.monthlyAuditRemark04 = monthlyAuditRemark04;
+	}
+
+	public String getMaterail() {
+		return materail;
+	}
+
+	public void setMaterail(String materail) {
+		this.materail = materail;
+	}
+
+	public BigDecimal getDocument() {
+		return document;
+	}
+
+	public void setDocument(BigDecimal document) {
+		this.document = document;
+	}
+
+	public String getProductProcess() {
+		return productProcess;
+	}
+
+	public void setProductProcess(String productProcess) {
+		this.productProcess = productProcess;
+	}
+
+	public String getProductNextime() {
+		return productNextime;
+	}
+
+	public void setProductNextime(String productNextime) {
+		this.productNextime = productNextime;
+	}
+
+	public Date getUseStartDate() {
+		return useStartDate;
+	}
+
+	public void setUseStartDate(Date useStartDate) {
+		this.useStartDate = useStartDate;
+	}
+
+	public Date getUseEndDate() {
+		return useEndDate;
+	}
+
+	public void setUseEndDate(Date useEndDate) {
+		this.useEndDate = useEndDate;
+	}
+
+	public String getBuyOverlimit() {
+		return buyOverlimit;
+	}
+
+	public void setBuyOverlimit(String buyOverlimit) {
+		this.buyOverlimit = buyOverlimit;
+	}
+
+	public String getBuyFromIndust() {
+		return buyFromIndust;
+	}
+
+	public void setBuyFromIndust(String buyFromIndust) {
+		this.buyFromIndust = buyFromIndust;
+	}
+
+	public String getBuyIndustLicense() {
+		return buyIndustLicense;
+	}
+
+	public void setBuyIndustLicense(String buyIndustLicense) {
+		this.buyIndustLicense = buyIndustLicense;
+	}
+
+	public String getBuyFromImporter() {
+		return buyFromImporter;
+	}
+
+	public void setBuyFromImporter(String buyFromImporter) {
+		this.buyFromImporter = buyFromImporter;
+	}
+
+	public String getImporterLicense() {
+		return importerLicense;
+	}
+
+	public void setImporterLicense(String importerLicense) {
+		this.importerLicense = importerLicense;
+	}
+
+	public String getBuyFromAgent() {
+		return buyFromAgent;
+	}
+
+	public void setBuyFromAgent(String buyFromAgent) {
+		this.buyFromAgent = buyFromAgent;
+	}
+
+	public String getBuyAgentLicense() {
+		return buyAgentLicense;
+	}
+
+	public void setBuyAgentLicense(String buyAgentLicense) {
+		this.buyAgentLicense = buyAgentLicense;
+	}
+
+	public String getSalerType() {
+		return salerType;
+	}
+
+	public void setSalerType(String salerType) {
+		this.salerType = salerType;
+	}
+
+	public String getSalerLicense() {
+		return salerLicense;
+	}
+
+	public void setSalerLicense(String salerLicense) {
+		this.salerLicense = salerLicense;
+	}
+
+	public String getUsedType() {
+		return usedType;
+	}
+
+	public void setUsedType(String usedType) {
+		this.usedType = usedType;
+	}
+
+	public String getUsedRemark() {
+		return usedRemark;
+	}
+
+	public void setUsedRemark(String usedRemark) {
+		this.usedRemark = usedRemark;
+	}
+
+	public BigDecimal getNumOfCust() {
+		return numOfCust;
+	}
+
+	public void setNumOfCust(BigDecimal numOfCust) {
+		this.numOfCust = numOfCust;
+	}
+
+	public String getGoodQuality() {
+		return goodQuality;
+	}
+
+	public void setGoodQuality(String goodQuality) {
+		this.goodQuality = goodQuality;
+	}
+
+	public Date getAgentStartDate() {
+		return agentStartDate;
+	}
+
+	public void setAgentStartDate(Date agentStartDate) {
+		this.agentStartDate = agentStartDate;
+	}
+
+	public Date getAgentEndDate() {
+		return agentEndDate;
+	}
+
+	public void setAgentEndDate(Date agentEndDate) {
+		this.agentEndDate = agentEndDate;
+	}
+
+	public String getAgentOverlimit() {
+		return agentOverlimit;
+	}
+
+	public void setAgentOverlimit(String agentOverlimit) {
+		this.agentOverlimit = agentOverlimit;
+	}
+
+	public String getABuyFromIndust() {
+		return ABuyFromIndust;
+	}
+
+	public void setABuyFromIndust(String aBuyFromIndust) {
+		ABuyFromIndust = aBuyFromIndust;
+	}
+
+	public String getABuyIndustLicense() {
+		return ABuyIndustLicense;
+	}
+
+	public void setABuyIndustLicense(String aBuyIndustLicense) {
+		ABuyIndustLicense = aBuyIndustLicense;
+	}
+
+	public String getABuyFromImporter() {
+		return ABuyFromImporter;
+	}
+
+	public void setABuyFromImporter(String aBuyFromImporter) {
+		ABuyFromImporter = aBuyFromImporter;
+	}
+
+	public String getAImporterLicense() {
+		return AImporterLicense;
+	}
+
+	public void setAImporterLicense(String aImporterLicense) {
+		AImporterLicense = aImporterLicense;
+	}
+
+	public String getABuyFromAgent() {
+		return ABuyFromAgent;
+	}
+
+	public void setABuyFromAgent(String aBuyFromAgent) {
+		ABuyFromAgent = aBuyFromAgent;
+	}
+
+	public String getABuyAgentLicense() {
+		return ABuyAgentLicense;
+	}
+
+	public void setABuyAgentLicense(String aBuyAgentLicense) {
+		ABuyAgentLicense = aBuyAgentLicense;
+	}
+
+	public String getASaleToAgent() {
+		return ASaleToAgent;
+	}
+
+	public void setASaleToAgent(String aSaleToAgent) {
+		ASaleToAgent = aSaleToAgent;
+	}
+
+	public String getASaleAgentLicense() {
+		return ASaleAgentLicense;
+	}
+
+	public void setASaleAgentLicense(String aSaleAgentLicense) {
+		ASaleAgentLicense = aSaleAgentLicense;
+	}
+
+	public String getASaleToUser() {
+		return ASaleToUser;
+	}
+
+	public void setASaleToUser(String aSaleToUser) {
+		ASaleToUser = aSaleToUser;
+	}
+
+	public String getASaleUserLicense() {
+		return ASaleUserLicense;
+	}
+
+	public void setASaleUserLicense(String aSaleUserLicense) {
+		ASaleUserLicense = aSaleUserLicense;
+	}
+
+	public String getSentToAgent() {
+		return sentToAgent;
+	}
+
+	public void setSentToAgent(String sentToAgent) {
+		this.sentToAgent = sentToAgent;
+	}
+
+	public String getSentToUser() {
+		return sentToUser;
+	}
+
+	public void setSentToUser(String sentToUser) {
+		this.sentToUser = sentToUser;
+	}
+}
