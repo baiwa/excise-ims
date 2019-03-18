19cdcd4455f71ec143cd81e1c1e5f9d4cf36a5ce
iEmDevX <watcharapong_sribun@outlook.com>
2019-03-18 11:15:29 +0700

    edit int0204

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaQuestionnaireHdrJdbcRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaQuestionnaireHdrJdbcRepository.java
@@ -11,27 +11,27 @@ import org.springframework.stereotype.Repository;
 
 import th.co.baiwa.buckwaframework.common.persistence.jdbc.CommonJdbcTemplate;
 import th.co.baiwa.buckwaframework.common.persistence.util.OracleUtils;
-import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
+import th.go.excise.ims.ia.constant.IaConstants;
 import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireHdr;
 import th.go.excise.ims.ia.vo.Int02FormVo;
 import th.go.excise.ims.ia.vo.Int02Vo;
 
 @Repository
 public class IaQuestionnaireHdrJdbcRepository {
-	
+
 	@Autowired
 	private CommonJdbcTemplate commonJdbcTemplate;
-	
+
 	public List<Int02Vo> getDataFilter(Int02FormVo request) {
 		StringBuilder sql = new StringBuilder();
 		List<Object> params = new ArrayList<Object>();
 		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_HDR WHERE 1=1 AND IS_DELETED='N' ");
-		
-		if(StringUtils.isNotBlank(request.getBudgetYear())) {
+
+		if (StringUtils.isNotBlank(request.getBudgetYear())) {
 			sql.append(" AND BUDGET_YEAR = ? ");
 			params.add(request.getBudgetYear());
 		}
-		
+
 //		if(StringUtils.isNotBlank(request.getCreatedBy())) {
 //			sql.append(" AND UPPER(CREATED_BY) LIKE ?");
 //			params.add("%" + request.getCreatedBy().toUpperCase() + "%");
@@ -41,7 +41,7 @@ public class IaQuestionnaireHdrJdbcRepository {
 //           sql.append(" AND UPPER(QTN_HEADER_NAME) LIKE ?");
 //            params.add("%" + request.getNameQtn().toUpperCase() + "%");
 //        }
-		
+
 //		if(StringUtils.isNotBlank(request.getStartDate())) {
 //			sql.append(" AND TRUNC(CREATED_DATE) >= ? ");
 //			params.add(ConvertDateUtils.parseStringToDate(request.getStartDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
@@ -51,26 +51,67 @@ public class IaQuestionnaireHdrJdbcRepository {
 //			sql.append(" AND TRUNC(CREATED_DATE) <= ? ");
 //			params.add(ConvertDateUtils.parseStringToDate(request.getEndDate(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
 //		}
- 		sql.append(" ORDER BY CREATED_DATE DESC");
+		sql.append(" ORDER BY CREATED_DATE DESC");
 
 		String limit = OracleUtils.limitForDatable(sql.toString(), request.getStart(), request.getLength());
 		@SuppressWarnings({ "rawtypes", "unchecked" })
-		List<Int02Vo> datas = this.commonJdbcTemplate.query(limit, params.toArray(), new BeanPropertyRowMapper(Int02Vo.class));
+		List<Int02Vo> datas = this.commonJdbcTemplate.query(limit, params.toArray(),
+				new BeanPropertyRowMapper(Int02Vo.class));
 
-		return datas; 
+		return datas;
 	}
-	
+
+	public List<Int02Vo> getDataFilterStatusTreeFour(Int02FormVo request) {
+		StringBuilder sql = new StringBuilder();
+		List<Object> params = new ArrayList<Object>();
+		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_HDR WHERE 1=1 AND IS_DELETED='N' ");
+		sql.append(" AND (STATUS = " + IaConstants.IA_STATUS.STATUS_3_CODE);
+		sql.append(" OR STATUS = " + IaConstants.IA_STATUS.STATUS_4_CODE + ") ");
+		if (StringUtils.isNotBlank(request.getBudgetYear())) {
+			sql.append(" AND BUDGET_YEAR = ? ");
+			params.add(request.getBudgetYear());
+		}
+
+		sql.append(" ORDER BY CREATED_DATE DESC");
+
+		String limit = OracleUtils.limitForDatable(sql.toString(), request.getStart(), request.getLength());
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		List<Int02Vo> datas = this.commonJdbcTemplate.query(limit, params.toArray(),
+				new BeanPropertyRowMapper(Int02Vo.class));
+
+		return datas;
+	}
+
+	public Integer countDatafilterStatusTreeFour(Int02FormVo request) {
+		StringBuilder sql = new StringBuilder();
+		List<Object> params = new ArrayList<Object>();
+		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_HDR WHERE IS_DELETED='N' ");
+		sql.append(" AND (STATUS = " + IaConstants.IA_STATUS.STATUS_3_CODE);
+		sql.append(" OR STATUS = " + IaConstants.IA_STATUS.STATUS_4_CODE + ") ");
+		if (StringUtils.isNotBlank(request.getBudgetYear())) {
+			sql.append(" AND BUDGET_YEAR = ? ");
+			params.add(request.getBudgetYear());
+		}
+
+		sql.append(" ORDER BY CREATED_DATE DESC");
+
+		String sqlCount = OracleUtils.countForDataTable(sql.toString());
+		Integer count = this.commonJdbcTemplate.queryForObject(sqlCount, params.toArray(), Integer.class);
+
+		return count;
+	}
+
 	/* count datatable */
 	public Integer countDatafilter(Int02FormVo request) {
 		StringBuilder sql = new StringBuilder();
 		List<Object> params = new ArrayList<Object>();
 		sql.append(" SELECT * FROM IA_QUESTIONNAIRE_HDR WHERE IS_DELETED='N' ");
-		
-		if(StringUtils.isNotBlank(request.getBudgetYear())) {
+
+		if (StringUtils.isNotBlank(request.getBudgetYear())) {
 			sql.append(" AND BUDGET_YEAR = ? ");
 			params.add(request.getBudgetYear());
 		}
-		
+
 //		if(StringUtils.isNotBlank(request.getCreatedBy())) {
 //			sql.append(" AND UPPER(CREATED_BY) LIKE ?");
 //			params.add("%" + request.getCreatedBy().toUpperCase() + "%");
@@ -93,9 +134,9 @@ public class IaQuestionnaireHdrJdbcRepository {
 		String sqlCount = OracleUtils.countForDataTable(sql.toString());
 		Integer count = this.commonJdbcTemplate.queryForObject(sqlCount, params.toArray(), Integer.class);
 
-		return count; 
+		return count;
 	}
-	
+
 	public IaQuestionnaireHdr findOne(BigDecimal id) {
 		StringBuilder sql = new StringBuilder();
 		List<Object> params = new ArrayList<Object>();
@@ -103,18 +144,19 @@ public class IaQuestionnaireHdrJdbcRepository {
 		sql.append(" AND ID=? ");
 		params.add(id);
 		@SuppressWarnings({ "rawtypes", "unchecked" })
-		IaQuestionnaireHdr data = (IaQuestionnaireHdr) commonJdbcTemplate.queryForObject(sql.toString(), params.toArray(), new BeanPropertyRowMapper(IaQuestionnaireHdr.class));
+		IaQuestionnaireHdr data = (IaQuestionnaireHdr) commonJdbcTemplate.queryForObject(sql.toString(),
+				params.toArray(), new BeanPropertyRowMapper(IaQuestionnaireHdr.class));
 
 		return data;
 	}
-	
-	public BigDecimal updateStatus(BigDecimal idHdr,String status) {
+
+	public BigDecimal updateStatus(BigDecimal idHdr, String status) {
 		StringBuilder sql = new StringBuilder(" UPDATE IA_QUESTIONNAIRE_HDR SET STATUS = ? WHERE ID = ? ");
 		List<Object> params = new ArrayList<Object>();
-		
+
 		params.add(status);
 		params.add(idHdr);
-		
+
 		commonJdbcTemplate.update(sql.toString(), params.toArray());
 
 		return idHdr;
--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0204Service.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0204Service.java
@@ -8,53 +8,39 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import th.co.baiwa.buckwaframework.common.bean.DataTableAjax;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant;
 import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
-import th.co.baiwa.buckwaframework.security.util.UserLoginUtils;
 import th.co.baiwa.buckwaframework.support.ApplicationCache;
 import th.go.excise.ims.ia.constant.IaConstants;
 import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireHdr;
 import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireMade;
 import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireMadeHdr;
-import th.go.excise.ims.ia.persistence.entity.IaQuestionnaireSide;
 import th.go.excise.ims.ia.persistence.repository.IaQuestionnaireHdrRepository;
 import th.go.excise.ims.ia.persistence.repository.IaQuestionnaireMadeHdrRepository;
 import th.go.excise.ims.ia.persistence.repository.IaQuestionnaireMadeRepository;
-import th.go.excise.ims.ia.persistence.repository.IaQuestionnaireSideRepository;
 import th.go.excise.ims.ia.persistence.repository.jdbc.IaQuestionnaireHdrJdbcRepository;
-import th.go.excise.ims.ia.persistence.repository.jdbc.IaQuestionnaireMadeHdrJdbcRepository;
-import th.go.excise.ims.ia.vo.Int020101SideVo;
-import th.go.excise.ims.ia.vo.Int020101UpdateVo;
 import th.go.excise.ims.ia.vo.Int02FormVo;
 import th.go.excise.ims.ia.vo.Int02Vo;
 
 @Service
-public class Int02Service {
-
-	@Autowired
-	private IaQuestionnaireHdrJdbcRepository iaQuestionnaireHdrJdbcRepository;
-
-	@Autowired
-	private IaQuestionnaireHdrRepository iaQuestionnaireHdrRepository;
-
+public class Int0204Service {
 	@Autowired
 	private IaQuestionnaireMadeHdrRepository iaQuestionnaireMadeHdrRepository;
-
+	
 	@Autowired
-	private IaQuestionnaireMadeHdrJdbcRepository iaQuestionnaireMadeHdrJdbcRepository;
-
+	private IaQuestionnaireHdrRepository iaQuestionnaireHdrRepository;
+	
 	@Autowired
 	private IaQuestionnaireMadeRepository iaQuestionnaireMadeRepository;
 	
 	@Autowired
-	private IaQuestionnaireSideRepository iaQuestionnaireSideRepository;
+	private IaQuestionnaireHdrJdbcRepository iaQuestionnaireHdrJdbcRepository;
 
 	public DataTableAjax<Int02Vo> filterQtnHdr(Int02FormVo request) {
 
-		List<Int02Vo> data = iaQuestionnaireHdrJdbcRepository.getDataFilter(request);
+		List<Int02Vo> data = iaQuestionnaireHdrJdbcRepository.getDataFilterStatusTreeFour(request);
 		/* convert date to string */
 		for (Int02Vo obj : data) {
 			/* to string statusStr */
@@ -90,71 +76,10 @@ public class Int02Service {
 		DataTableAjax<Int02Vo> dataTableAjax = new DataTableAjax<Int02Vo>();
 		dataTableAjax.setDraw(request.getDraw() + 1);
 		dataTableAjax.setData(data);
-		dataTableAjax.setRecordsTotal(iaQuestionnaireHdrJdbcRepository.countDatafilterStatusTreeFour(request));
-		dataTableAjax.setRecordsFiltered(iaQuestionnaireHdrJdbcRepository.countDatafilterStatusTreeFour(request));
+		dataTableAjax.setRecordsTotal(iaQuestionnaireHdrJdbcRepository.countDatafilter(request));
+		dataTableAjax.setRecordsFiltered(iaQuestionnaireHdrJdbcRepository.countDatafilter(request));
 
 		return dataTableAjax;
-	}
-
-	public IaQuestionnaireHdr findOne(String idStr) {
-		BigDecimal id = new BigDecimal(idStr);
-		IaQuestionnaireHdr hdr = iaQuestionnaireHdrJdbcRepository.findOne(id);
-//		hdr.setStatus(ApplicationCache.getParamInfoByCode("IA_STATUS", hdr.getStatus()).getValue1());
-		return hdr;
-	}
-
-	public IaQuestionnaireHdr save(IaQuestionnaireHdr request) {
-		return iaQuestionnaireHdrRepository.save(request);
-	}
-
-	public IaQuestionnaireHdr update(String idStr, Int020101UpdateVo res) {
-		updateSeq(idStr, res);
-		IaQuestionnaireHdr request = new IaQuestionnaireHdr();
-
-		request.setBudgetYear(res.getBudgetYear());
-		request.setId(res.getId());
-		request.setQtnHeaderName(res.getQtnHeaderName());
-		request.setNote(res.getNote());
-		request.setStartDate(res.getStartDate());
-		request.setEndDate(res.getEndDate());
-		request.setStatus(res.getStatus());
-
-		BigDecimal id = new BigDecimal(idStr);
-		IaQuestionnaireHdr data = iaQuestionnaireHdrJdbcRepository.findOne(id);
-		data.setBudgetYear(request.getBudgetYear());
-		data.setQtnHeaderName(request.getQtnHeaderName());
-		data.setNote(request.getNote());
-
-		/* update table Questionnaire-Made-Hdr */
-//		List<IaQuestionnaireMadeHdr> dataMadeHdr = iaQuestionnaireMadeHdrRepository.findByIdHdr(request.getId());
-//		for (IaQuestionnaireMadeHdr objMadeHdr : dataMadeHdr) {
-//			if(!"FINISH".equals(objMadeHdr.getStatus())) {
-//				objMadeHdr.setQtnHeaderName(request.getQtnHeaderName());
-//				objMadeHdr.setNote(request.getNote());
-//				iaQuestionnaireMadeHdrRepository.save(objMadeHdr);
-//			}
-//		}
-		List<IaQuestionnaireMadeHdr> madeHdrList = iaQuestionnaireMadeHdrJdbcRepository.findMadeHdrByIdHdr(request,
-				UserLoginUtils.getCurrentUserBean().getOfficeCode());
-		for (IaQuestionnaireMadeHdr madeHdr : madeHdrList) {
-			madeHdr.setQtnHeaderName(request.getQtnHeaderName());
-			madeHdr.setNote(request.getNote());
-			iaQuestionnaireMadeHdrRepository.save(madeHdr);
-		}
-
-		return iaQuestionnaireHdrRepository.save(data);
-	}
-
-	@Transactional
-	public void updateSeq(String idStr, Int020101UpdateVo res) {
-		List<Int020101SideVo> side = res.getSide();
-		IaQuestionnaireSide sideData = null;
-		for (Int020101SideVo sideVo : side) {
-			sideData = new IaQuestionnaireSide();
-			sideData = iaQuestionnaireSideRepository.findById(sideVo.getId()).get();
-			sideData.setSeq(new BigDecimal(sideVo.getSeq()));
-			iaQuestionnaireSideRepository.save(sideData);
-		}
 	}
 
 	public void updateStatus(String idStr) {
@@ -167,12 +92,14 @@ public class Int02Service {
 //				daraHdr.setStatus("FAIL_SEND_QTN");
 				iaQuestionnaireHdrRepository.save(daraHdr);
 
-				List<IaQuestionnaireMadeHdr> dataMadeHdr = iaQuestionnaireMadeHdrRepository.findByIdHdrAndIsDeleted(daraHdr.getId(), "N");
+				List<IaQuestionnaireMadeHdr> dataMadeHdr = iaQuestionnaireMadeHdrRepository
+						.findByIdHdrAndIsDeleted(daraHdr.getId(), "N");
 				for (IaQuestionnaireMadeHdr madeHdr : dataMadeHdr) {
 //					madeHdr.setStatus("FAIL_SEND_QTN");
 					iaQuestionnaireMadeHdrRepository.save(madeHdr);
 					/* update status questionnaire made dtl */
-					List<IaQuestionnaireMade> madeDtlList = iaQuestionnaireMadeRepository.findByIdMadeHdrAndIsDeleted(madeHdr.getId(), "N");
+					List<IaQuestionnaireMade> madeDtlList = iaQuestionnaireMadeRepository
+							.findByIdMadeHdrAndIsDeleted(madeHdr.getId(), "N");
 					for (IaQuestionnaireMade madeDtl : madeDtlList) {
 //						madeDtl.setStatus("FAIL_SEND_QTN");
 						iaQuestionnaireMadeRepository.save(madeDtl);
