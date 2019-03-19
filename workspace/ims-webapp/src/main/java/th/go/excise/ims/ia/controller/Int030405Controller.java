670c2160e7ceccd0bc5412f9599c6d3861827582
phattarapong58021725 <phattarapong_i@hotmail.com>
2019-03-19 17:55:59 +0700

    edit export

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int030405Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int030405Controller.java
@@ -1,12 +1,20 @@
 package th.go.excise.ims.ia.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +35,8 @@ public class Int030405Controller {
 	private Logger logger = LoggerFactory.getLogger(Int030102Controller.class);
 	
 	@Autowired
-	private Int030405Service Int030405Service;
-
+	private Int030405Service int030405Service;
+	
 	@PostMapping("/systemUnworkingList")
 	@ResponseBody
 	public DataTableAjax<Int030405Vo> systemUnworkingList(@RequestBody Int030405FormVo form) {
@@ -36,7 +44,7 @@ public class Int030405Controller {
 		List<Int030405Vo> systemUnworkingList = new ArrayList<Int030405Vo>();
 
 		try {
-			systemUnworkingList = Int030405Service.systemUnworkingList(form);
+			systemUnworkingList = int030405Service.systemUnworkingList(form);
 			response.setData(systemUnworkingList);
 
 		} catch (Exception e) {
@@ -50,10 +58,28 @@ public class Int030405Controller {
 	public Int0301Vo getForm0304(@RequestBody Int0301FormVo form) {
 		Int0301Vo response = new Int0301Vo();
 		try {	
-			response = Int030405Service.getForm0304(form);
+			response = int030405Service.getForm0304(form);
 		} catch (Exception e) {
 			logger.error("Int0304Controller : " , e);
 		}
 		return response;
+	}
+	
+	@GetMapping("/year/export/{budgetYear}/{inspectionWork}/{idConfig}")
+	public void exportByYear(@PathVariable("budgetYear") String budgetYear,@PathVariable("inspectionWork") BigDecimal inspectionWork,@PathVariable("idConfig") BigDecimal idConfig, HttpServletResponse response) throws Exception {
+		// set fileName
+		String fileName = URLEncoder.encode("สรุปผลปัจจัยเสี่ยงจำนวนครั้งที่ใช้งานไม่ได้ของระบบ", "UTF-8");
+
+		// write it as an excel attachment
+		ByteArrayOutputStream outByteStream = int030405Service.exportInt030405(budgetYear,inspectionWork,idConfig);
+		byte[] outArray = outByteStream.toByteArray();
+		response.setContentType("application/octet-stream");
+		response.setContentLength(outArray.length);
+		response.setHeader("Expires:", "0"); // eliminates browser caching
+		response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".xlsx");
+		OutputStream outStream = response.getOutputStream();
+		outStream.write(outArray);
+		outStream.flush();
+		outStream.close();
 	}
 }
--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int030405Service.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int030405Service.java
@@ -1,21 +1,34 @@
 package th.go.excise.ims.ia.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import th.go.excise.ims.common.util.ExcelUtils;
 import th.go.excise.ims.ia.persistence.entity.IaRiskSystemUnworking;
 import th.go.excise.ims.ia.persistence.repository.IaRiskSystemUnworkingRepository;
 import th.go.excise.ims.ia.persistence.repository.jdbc.Int030405JdbcRepository;
 import th.go.excise.ims.ia.util.IntCalculateCriteriaUtil;
 import th.go.excise.ims.ia.vo.Int0301FormVo;
 import th.go.excise.ims.ia.vo.Int0301Vo;
+import th.go.excise.ims.ia.vo.Int030404FormVo;
+import th.go.excise.ims.ia.vo.Int030404Vo;
 import th.go.excise.ims.ia.vo.Int030405FormVo;
 import th.go.excise.ims.ia.vo.Int030405Vo;
 import th.go.excise.ims.ia.vo.IntCalculateCriteriaVo;
@@ -96,4 +109,164 @@ public class Int030405Service {
 		}
 		return int0301Vo;
 	}
+
+	
+	public ByteArrayOutputStream exportInt030405(String budgetYear, BigDecimal inspectionWork, BigDecimal idConfig)
+			throws IOException {
+		/* create spreadsheet */
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		CellStyle thStyle = ExcelUtils.createThCellStyle(workbook);
+		CellStyle tdStyle = ExcelUtils.createTdCellStyle(workbook);
+		CellStyle tdLeft = ExcelUtils.createLeftCellStyle(workbook);
+		CellStyle tdRight = ExcelUtils.createRightCellStyle(workbook);
+		Sheet sheet = workbook.createSheet();
+		int rowNum = 0;
+		int cellNum = 0;
+
+		// Row [0]
+		Row row = sheet.createRow(rowNum);
+		Cell cell = row.createCell(cellNum);
+		String[] tbTH1 = { "ลำดับ", "ปีแผนงาน", "ชื่อแผนงานโครงการ", "ประเภทแผนงานโครงการ", "รหัสหน่วยงานที่รับผิดชอบ",
+				"ชื่อหน่วยงานที่รับผิดชอบ", "ค่าประสิทธิภาพ", "ประเมินความเสี่ยง", "" };
+		for (int i = 0; i < tbTH1.length; i++) {
+			cell = row.createCell(cellNum);
+			cell.setCellValue(tbTH1[i]);
+			cell.setCellStyle(thStyle);
+			cellNum++;
+		}
+		rowNum++;
+
+		// Row [1]
+		row = sheet.createRow(rowNum);
+		cellNum = 0;
+		cell = row.createCell(cellNum);
+
+		String[] tbTH2 = { "", "", "", "", "", "", "", "อัตราความเสี่ยง", "แปลค่าความเสี่ยง" };
+		for (int i = 0; i < tbTH2.length; i++) {
+			cell = row.createCell(cellNum);
+			cell.setCellValue(tbTH2[i]);
+			cell.setCellStyle(thStyle);
+			cellNum++;
+		}
+		rowNum++;
+
+		/* set sheet */
+		// merge(firstRow, lastRow, firstCol, lastCol)
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 0, 0));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 1, 1));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 2, 2));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 3, 3));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 4, 4));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 5, 5));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 1, 6, 6));
+		sheet.addMergedRegion(new CellRangeAddress(rowNum - 2, rowNum - 2, 7, 8));
+		/* set sheet */
+
+		// setColumnWidth
+		int width = 76;
+		sheet.setColumnWidth(0, width * 30);
+		for (int i = 1; i <= 9; i++) {
+			if (i >= 2 && i <= 3) {
+				sheet.setColumnWidth(i, width * 180);
+			} else {
+				sheet.setColumnWidth(i, width * 76);
+			}
+		}
+
+		/* start details */
+		int count = 1;
+		rowNum = 2;
+		cellNum = 0;
+
+		List<Int030405Vo> datas = new ArrayList<Int030405Vo>();
+		Int030405FormVo form = new Int030405FormVo();
+		form.setBudgetYear(budgetYear);
+		form.setInspectionWork(inspectionWork);
+		form.setIdConfig(idConfig);
+		datas = systemUnworkingList(form);
+
+		DecimalFormat df2 = new DecimalFormat(".##");
+		for (Int030405Vo data : datas) {
+			// Re Initial
+			cellNum = 0;
+			row = sheet.createRow(rowNum);
+			// Column 1
+			CellStyle styleCustom = tdStyle;
+			styleCustom.setAlignment(HorizontalAlignment.CENTER);
+			cell = row.createCell(cellNum);
+			cell.setCellValue(count++);
+			cell.setCellStyle(styleCustom);
+			cellNum++;
+			// Column 2
+			cell = row.createCell(cellNum);
+//			cell.setCellValue(data.getIaRiskProEfVo().getProjectYear());
+			cell.setCellStyle(tdStyle);
+			cellNum++;
+
+			// Column 3
+			cell = row.createCell(cellNum);
+//			cell.setCellValue(data.getIaRiskProEfVo().getProjectName());
+			cell.setCellStyle(tdLeft);
+			cellNum++;
+
+			// Column 4
+			cell = row.createCell(cellNum);
+//			cell.setCellValue(data.getIaRiskProEfVo().getProjectTypeName());
+			cell.setCellStyle(tdLeft);
+			cellNum++;
+
+			// Column 5
+			cell = row.createCell(cellNum);
+//			cell.setCellValue(data.getIaRiskProEfVo().getOwnerOfficeId());
+			cell.setCellStyle(tdStyle);
+			cellNum++;
+
+			// Column 6
+			cell = row.createCell(cellNum);
+//			cell.setCellValue(data.getIaRiskProEfVo().getOwnerOfficeName());
+			cell.setCellStyle(tdLeft);
+			cellNum++;
+
+			// Column 7
+			cell = row.createCell(cellNum);
+//			if (data.getIaRiskProEfVo().getAverageData() != null) {
+//				cell.setCellValue(data.getIaRiskProEfVo().getAverageData().toString());
+//			}else {
+//				cell.setCellValue("-");
+//			}
+			cell.setCellStyle(tdStyle);
+			cellNum++;
+
+			// Column 8
+			cell = row.createCell(cellNum);
+//			if(data.getIntCalculateCriteriaVo().getRiskRate() != null) {
+//				cell.setCellValue(data.getIntCalculateCriteriaVo().getRiskRate().doubleValue());
+//			}else {
+//				cell.setCellValue("-");
+//			}
+			cell.setCellStyle(tdStyle);
+			cellNum++;
+			
+			// Column 9
+			cell = row.createCell(cellNum);
+//			if(data.getIntCalculateCriteriaVo().getTranslatingRisk() != null) {
+//				cell.setCellValue(data.getIntCalculateCriteriaVo().getTranslatingRisk());
+//			}else {
+//				cell.setCellValue("-");
+//			}
+			cell.setCellStyle(tdStyle);
+			cellNum++;
+
+			// Next Row
+			rowNum++;
+		}
+		/* end details */
+
+		/* set write */
+		ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+		workbook.write(outByteStream);
+
+		return outByteStream;
+	}
+	
 }
