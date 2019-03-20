0620fdf7766b656a34abcc9a21f906f54259b94f
toffeeDev <saknarin.top21@gmail.com>
2019-03-20 14:14:19 +0700

    edit name jasper Ts 01_07

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/TaFormTS0107Service.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/TaFormTS0107Service.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -14,15 +16,53 @@ import th.go.excise.ims.ta.vo.TaFormTS0107Vo;
 
 @Service
 public class TaFormTS0107Service {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(TaFormTS0107Service.class);
+
 	public byte[] exportTaFormTS0107(TaFormTS0107Vo request) throws Exception, IOException {
-		String reportName = "TA_FORM_TS0107.jasper";
+		logger.info("exportTaFormTS0107");
+
+		String reportName = "TA_FORM_TS01_07.jasper";
 		Map<String, Object> params = new HashMap<>();
 
 		// get data to report
 		params.put("logo", ReportUtils.getResourceFile(PATH.IMAGE_PATH, "logo-garuda.jpg"));
-		params.put("office", request.getBookNumber1());
+		params.put("bookNumber1", request.getBookNumber1());
+		params.put("bookNumber2", request.getBookNumber2());
+		params.put("officeName1", request.getOfficeName1());
 
+		params.put("officeName2", request.getOfficeName2());
+		params.put("headOfficerFullName", request.getHeadOfficerFullName());
+		params.put("headOfficerPosition", request.getHeadOfficerPosition());
+		params.put("officerFullName1", request.getOfficerFullName1());
+		params.put("officerPosition1", request.getOfficerPosition1());
+		params.put("officerFullName2", request.getOfficerFullName2());
+		params.put("officerPosition2", request.getOfficerPosition2());
+		params.put("officerFullName3", request.getOfficerFullName3());
+		params.put("officerPosition3", request.getOfficerPosition3());
+		params.put("officerFullName4", request.getOfficerFullName4());
+		params.put("officerPosition4", request.getOfficerPosition4());
+		params.put("officerFullName5", request.getOfficerFullName5());
+		params.put("officerPosition5", request.getOfficerPosition5());
+
+		params.put("factoryType", request.getFactoryType());
+		params.put("factoryName", request.getFactoryName());
+
+		params.put("newRegId", request.getNewRegId());
+		params.put("facAddrNo", request.getFacAddrNo());
+		params.put("facMooNo", request.getFacMooNo());
+		params.put("facSoiName", request.getFacSoiName());
+		params.put("facThnName", request.getFacThnName());
+		params.put("facTambolName", request.getFacTambolName());
+		params.put("facAmphurName", request.getFacAmphurName());
+		params.put("facProvinceName", request.getFacProvinceName());
+		params.put("facZipCode", request.getFacZipCode());
+
+		params.put("lawSection", request.getLawSection());
+		params.put("signFullName", request.getSignFullName());
+		params.put("signPosition", request.getSignPosition());
+		params.put("otherText", request.getOtherText());
+		params.put("otherPhone", request.getOtherPhone());
 		// set output
 		JasperPrint jasperPrint = ReportUtils.getJasperPrint(reportName, params);
 		byte[] content = JasperExportManager.exportReportToPdf(jasperPrint);
--- workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS0107ServiceTest.java
+++ workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS0107ServiceTest.java
@@ -13,15 +13,54 @@ public class TaFormTS0107ServiceTest {
 
 	private TaFormTS0107Service taFormTS0107Service = new TaFormTS0107Service();
 	private static final String PATH = "/tmp/";
-	private static final String NAME = "TaFormTS0107.pdf";
+	private static final String NAME = "TaFormTS01_07.pdf";
 	@Test
 	public void test_exportTaFormTS0107() throws Throwable, IOException {
 
 		TaFormTS0107Vo data = new TaFormTS0107Vo();
 		//set  data
-		data.setBookNumber1("TEST");
-		data.setBookNumber2("TEST2");
-
+		data.setBookNumber1("ทดสอบ");
+		data.setBookNumber2("ทดสอบ");
+		
+		data.setOfficeName1("ทดสอบ");
+		
+		data.setOfficeName2("ทดสอบ");
+		data.setHeadOfficerFullName("ทดสอบ");
+		data.setHeadOfficerPosition("ทดสอบ");
+		data.setOfficerFullName1("ทดสอบ");
+		data.setOfficerPosition1("ทดสอบ");
+		data.setOfficerFullName2("ทดสอบ");
+		data.setOfficerPosition2("ทดสอบ");
+		data.setOfficerFullName3("ทดสอบ");
+		data.setOfficerPosition3("ทดสอบ");
+		data.setOfficerFullName4("ทดสอบ");
+		data.setOfficerPosition4("ทดสอบ");
+		data.setOfficerFullName5("ทดสอบ");
+		data.setOfficerPosition5("ทดสอบ");
+		
+		data.setCompanyName("ทดสอบ");
+		//type 1 =โรงอุตสาหกรรม || 2=สถานบริการ || 3 =สถานประกอบการผู้นำเข้า
+		data.setFactoryType("1");
+		
+		data.setFactoryName("ทดสอบ");
+		data.setNewRegId("ทดสอบ");
+		data.setFacAddrNo("ทดสอย");
+		data.setFacMooNo("ทดสอบ");
+		data.setFacSoiName("ทดสอบ");
+		data.setFacThnName("ทดสอบ");
+		data.setFacTambolName("ทดสอบ");
+		data.setFacAmphurName("ทดสอบ");
+		data.setFacProvinceName("ทดสอบ");
+		data.setFacZipCode("ทกสอบ");
+		
+		data.setLawSection("ทดสอบ");
+		data.setHeadOfficerPhone("ทดสอบ");
+		data.setSignFullName("ทดสอบ");
+		data.setSignPosition("ทดสอบ");
+		data.setOtherText("ทดสอบ");
+		data.setOtherPhone("ทดสอบ");
+		
+		
 		byte[] reportFile = taFormTS0107Service.exportTaFormTS0107(data);
 		IOUtils.write(reportFile, new FileOutputStream(new File(PATH + NAME)));
 
