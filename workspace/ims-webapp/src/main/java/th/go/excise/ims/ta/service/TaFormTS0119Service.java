528216d459367850b64b137625876dd3d362fcb3
flame123flame <flame.tana@gmail.com>
2019-03-22 13:23:06 +0700

    add jasper report

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/TaFormTS0119Service.java
@@ -0,0 +1,92 @@
+package th.go.excise.ims.ta.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.ExporterInputItem;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleExporterInputItem;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.FILE_EXTENSION;
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.IMG_NAME;
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.PATH;
+import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
+import th.co.baiwa.buckwaframework.common.util.ReportUtils;
+
+import th.go.excise.ims.ta.vo.TaFormTS0119Vo;
+
+@Service
+public class TaFormTS0119Service {
+	@SuppressWarnings("unchecked")
+	public byte[] exportTaFormTS019(TaFormTS0119Vo request) throws Exception, IOException {
+	
+
+		String reportName = "TA_FORM_TS01_19.jasper";
+		Gson gson = new Gson();
+		Map<String, Object> params = new HashMap<String, Object>();
+		params = (Map<String, Object>) gson.fromJson(request.getJson(), params.getClass());
+		
+
+		// get data to report
+		params.put("logo", ReportUtils.getResourceFile(PATH.IMAGE_PATH, IMG_NAME.LOGO_GARUDA + "." + FILE_EXTENSION.JPG));
+		
+		params.put("bookNumber1", request.getBookNumber1());
+		params.put("bookNumber2", request.getBookNumber2());
+		params.put("docText1", request.getDocText1());
+		params.put("docText2", request.getDocText2());
+		params.put("docText3", request.getDocText3());
+		params.put("docDear", request.getDocDear());
+		params.put("companyName", request.getCompanyName());
+		params.put("factoryType", request.getFactoryType());
+		params.put("factoryName", request.getFactoryName());
+		params.put("newRegId", request.getNewRegId());
+		params.put("facAddrNo", request.getFacAddrNo());
+		params.put("facMooNo", request.getFacMooNo());
+		params.put("facSoiName", request.getFacSoiName());
+		params.put("facThnName", request.getFacThnName());
+		params.put("facTambolName", request.getFacTambolName());
+		params.put("facAmphurName", request.getFacAmphurName());
+		params.put("facProvinceName", request.getFacProvinceName());
+		params.put("facZipCode", request.getFacZipCode());
+		params.put("signOfficerFullName", request.getSignOfficerFullName());
+		params.put("followTypeFlag1", request.getFollowTypeFlag1());
+		params.put("followTypeFlag2", request.getFollowTypeFlag2());
+//		params.put("docText3", request.getDocText3());
+//		params.put("docText3", request.getDocText3());
+//		params.put("docText3", request.getDocText3());
+//		params.put("docText3", request.getDocText3());
+		request.setDocDate(ConvertDateUtils.parseStringToDate(request.getDocDateStr(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+		params.put("docDate", request.getDocDate());
+		
+		request.setRefBookDate(ConvertDateUtils.parseStringToDate(request.getRefBookDateStr(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH));
+		params.put("refBookDate", request.getRefBookDate());
+		// set output
+		JasperPrint jasperPrint = ReportUtils.getJasperPrint(reportName, params);
+		List<ExporterInputItem> items = new ArrayList<ExporterInputItem>();
+		items.add(new SimpleExporterInputItem(jasperPrint));
+
+		JRPdfExporter exporter = new JRPdfExporter();
+		exporter.setExporterInput(new SimpleExporterInput(items));
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+		exporter.exportReport();
+		byte[] content = outputStream.toByteArray();
+
+		ReportUtils.closeResourceFileInputStream(params);
+
+		return content;
+	}
+
+}
---
+++ workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS0119ServiceTest.java
@@ -0,0 +1,50 @@
+package th.go.excise.ims.ta.service;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import th.go.excise.ims.ta.vo.TaFormTS0119Vo;
+
+public class TaFormTS0119ServiceTest {
+	private TaFormTS0119Service taFormTS0119Service = new TaFormTS0119Service();
+	private static final String PATH = "/tmp/";
+	private static final String NAME = "TaFormTS01_19.pdf";
+
+	@Test
+	public void test_exportTaFormTS0107() throws Throwable, IOException {
+
+		TaFormTS0119Vo data = new TaFormTS0119Vo();
+		data.setBookNumber1("25632");
+		data.setBookNumber2("632522");
+		data.setDocText1("อํานาจในการอนุมัติให้ออกตรวจ");
+		data.setDocText2("อํานาจในการอนุมัติให้ออกตรวจ");
+		data.setDocText3("อํานาจในการอนุมัติให้ออกตรวจ");
+		data.setDocDateStr("15/03/2562");;
+		data.setDocDear("ผบตร ธนพล ชัยภูมิ");
+		data.setCompanyName("โรงเหล้ายโสธร");
+		data.setFactoryType("3");
+		data.setFacThnName("โรงผลิตเหล้ายโสธร");
+		data.setNewRegId("25564541585");
+		data.setFacAddrNo("หนองนาคำ");
+		data.setFacMooNo("20");
+		data.setFacSoiName("หนองหินห่าว");
+		data.setFacThnName("โคกนาแก");
+		data.setFacTambolName("โคกอิโด่ย");
+		data.setFacAmphurName("นาบักเหลี่ยม");
+		data.setFacProvinceName("หนองบัวลำภู");
+		data.setFacZipCode("39000");
+		data.setFollowTypeFlag1("1");
+		data.setFollowTypeFlag2("2");
+		data.setRefBookDateStr("15/06/2561");
+		data.setOfficeName1("");
+		// Convert String to LocalDate
+		
+
+	byte[] reportFile = taFormTS0119Service.exportTaFormTS019(data);
+	IOUtils.write(reportFile, new FileOutputStream(new File(PATH + NAME)));
+
+	}
+}
