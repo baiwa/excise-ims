2574ad082cd34d2c20dd945d5581ce60de17ef91
taechapon <taechapon@gmail.com>
2019-03-24 23:10:21 +0700

    Initial Entity and Repository of TaFormTs

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/entity/TaFormTs0115Dtl.java
@@ -0,0 +1,160 @@
+package th.go.excise.ims.ta.persistence.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import th.co.baiwa.buckwaframework.common.persistence.entity.BaseEntity;
+
+@Entity
+@Table(name = "TA_FORM_TS0115_DTL")
+public class TaFormTs0115Dtl extends BaseEntity {
+
+	private static final long serialVersionUID = -6966818938917536700L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TA_FORM_TS0115_DTL_GEN")
+	@SequenceGenerator(name = "TA_FORM_TS0115_DTL_GEN", sequenceName = "TA_FORM_TS0115_DTL_SEQ", allocationSize = 1)
+	@Column(name = "FORM_TS0115_DTL_ID")
+	private Long formTs0115DtlId;
+	@Column(name = "OFFICE_CODE")
+	private String officeCode;
+	@Column(name = "BUDGET_YEAR")
+	private String budgetYear;
+	@Column(name = "FORM_TS_NUMBER")
+	private String formTsNumber;
+	@Column(name = "NEW_REG_ID")
+	private String newRegId;
+	@Column(name = "REC_NO")
+	private String recNo;
+	@Column(name = "REC_DATE")
+	private Date recDate;
+	@Column(name = "DUTY_TYPE_TEXT")
+	private String dutyTypeText;
+	@Column(name = "TAX_AMT")
+	private BigDecimal taxAmt;
+	@Column(name = "FINE_AMT")
+	private BigDecimal fineAmt;
+	@Column(name = "EXTRA_AMT")
+	private BigDecimal extraAmt;
+	@Column(name = "MOI_AMT")
+	private BigDecimal moiAmt;
+	@Column(name = "SUM_TAX_AMT")
+	private BigDecimal sumTaxAmt;
+
+	public Long getFormTs0115DtlId() {
+		return formTs0115DtlId;
+	}
+
+	public void setFormTs0115DtlId(Long formTs0115DtlId) {
+		this.formTs0115DtlId = formTs0115DtlId;
+	}
+
+	public String getOfficeCode() {
+		return officeCode;
+	}
+
+	public void setOfficeCode(String officeCode) {
+		this.officeCode = officeCode;
+	}
+
+	public String getBudgetYear() {
+		return budgetYear;
+	}
+
+	public void setBudgetYear(String budgetYear) {
+		this.budgetYear = budgetYear;
+	}
+
+	public String getFormTsNumber() {
+		return formTsNumber;
+	}
+
+	public void setFormTsNumber(String formTsNumber) {
+		this.formTsNumber = formTsNumber;
+	}
+
+	public String getNewRegId() {
+		return newRegId;
+	}
+
+	public void setNewRegId(String newRegId) {
+		this.newRegId = newRegId;
+	}
+
+	public String getRecNo() {
+		return recNo;
+	}
+
+	public void setRecNo(String recNo) {
+		this.recNo = recNo;
+	}
+
+	public Date getRecDate() {
+		return recDate;
+	}
+
+	public void setRecDate(Date recDate) {
+		this.recDate = recDate;
+	}
+
+	public String getDutyTypeText() {
+		return dutyTypeText;
+	}
+
+	public void setDutyTypeText(String dutyTypeText) {
+		this.dutyTypeText = dutyTypeText;
+	}
+
+	public BigDecimal getTaxAmt() {
+		return taxAmt;
+	}
+
+	public void setTaxAmt(BigDecimal taxAmt) {
+		this.taxAmt = taxAmt;
+	}
+
+	public BigDecimal getFineAmt() {
+		return fineAmt;
+	}
+
+	public void setFineAmt(BigDecimal fineAmt) {
+		this.fineAmt = fineAmt;
+	}
+
+	public BigDecimal getExtraAmt() {
+		return extraAmt;
+	}
+
+	public void setExtraAmt(BigDecimal extraAmt) {
+		this.extraAmt = extraAmt;
+	}
+
+	public BigDecimal getMoiAmt() {
+		return moiAmt;
+	}
+
+	public void setMoiAmt(BigDecimal moiAmt) {
+		this.moiAmt = moiAmt;
+	}
+
+	public BigDecimal getSumTaxAmt() {
+		return sumTaxAmt;
+	}
+
+	public void setSumTaxAmt(BigDecimal sumTaxAmt) {
+		this.sumTaxAmt = sumTaxAmt;
+	}
+
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+	}
+
+}
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/entity/TaFormTs0116.java
@@ -0,0 +1,450 @@
+package th.go.excise.ims.ta.persistence.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import th.co.baiwa.buckwaframework.common.persistence.entity.BaseEntity;
+
+@Entity
+@Table(name = "TA_FORM_TS0116")
+public class TaFormTs0116 extends BaseEntity {
+
+	private static final long serialVersionUID = -39668813174972492L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TA_FORM_TS0116_GEN")
+	@SequenceGenerator(name = "TA_FORM_TS0116_GEN", sequenceName = "TA_FORM_TS0116_SEQ", allocationSize = 1)
+	@Column(name = "FORM_TS0116_ID")
+	private Long formTs0116Id;
+	@Column(name = "OFFICE_CODE")
+	private String officeCode;
+	@Column(name = "BUDGET_YEAR")
+	private String budgetYear;
+	@Column(name = "FORM_TS_NUMBER")
+	private String formTsNumber;
+	@Column(name = "NEW_REG_ID")
+	private String newRegId;
+	@Column(name = "DOC_TEXT")
+	private String docText;
+	@Column(name = "DOC_DEAR")
+	private String docDear;
+	@Column(name = "FACTORY_NAME")
+	private String factoryName;
+	@Column(name = "FACTORY_TYPE")
+	private String factoryType;
+	@Column(name = "REQUEST_DATE")
+	private Date requestDate;
+	@Column(name = "REQUEST_TYPE")
+	private String requestType;
+	@Column(name = "REQUEST_REASON")
+	private String requestReason;
+	@Column(name = "REQUEST_DESC")
+	private String requestDesc;
+	@Column(name = "FINE_NO_FLAG")
+	private String fineNoFlag;
+	@Column(name = "FINE_REFRAIN_FLAG")
+	private String fineRefrainFlag;
+	@Column(name = "FIND_REDUCE_FLAG")
+	private String findReduceFlag;
+	@Column(name = "FINE_PERCENT")
+	private BigDecimal finePercent;
+	@Column(name = "EXTRA_NO_FLAG")
+	private String extraNoFlag;
+	@Column(name = "EXTRA_REDUCE_FLAG")
+	private String extraReduceFlag;
+	@Column(name = "EXTRA_PERCENT")
+	private BigDecimal extraPercent;
+	@Column(name = "BEFORE_TAX_AMT")
+	private BigDecimal beforeTaxAmt;
+	@Column(name = "BEFORE_FINE_PERCENT")
+	private BigDecimal beforeFinePercent;
+	@Column(name = "BEFORE_FINE_AMT")
+	private BigDecimal beforeFineAmt;
+	@Column(name = "BEFORE_EXTRA_AMT")
+	private BigDecimal beforeExtraAmt;
+	@Column(name = "BEFORE_MOI_AMT")
+	private BigDecimal beforeMoiAmt;
+	@Column(name = "BEFORE_SUM_AMT")
+	private BigDecimal beforeSumAmt;
+	@Column(name = "AFTER_TAX_AMT")
+	private BigDecimal afterTaxAmt;
+	@Column(name = "AFTER_FINE_PERCENT")
+	private BigDecimal afterFinePercent;
+	@Column(name = "AFTER_FINE_AMT")
+	private BigDecimal afterFineAmt;
+	@Column(name = "AFTER_EXTRA_AMT")
+	private BigDecimal afterExtraAmt;
+	@Column(name = "AFTER_MOI_AMT")
+	private BigDecimal afterMoiAmt;
+	@Column(name = "AFTER_SUM_AMT")
+	private BigDecimal afterSumAmt;
+	@Column(name = "SIGN_OFFICER_FULL_NAME")
+	private String signOfficerFullName;
+	@Column(name = "SIGN_OFFICER_POSITION")
+	private String signOfficerPosition;
+	@Column(name = "HEAD_OFFICER_COMMENT")
+	private String headOfficerComment;
+	@Column(name = "SIGN_HEAD_OFFICER_FULL_NAME")
+	private String signHeadOfficerFullName;
+	@Column(name = "SIGN_HEAD_OFFICER_POSITION")
+	private String signHeadOfficerPosition;
+	@Column(name = "SIGN_HEAD_OFFICER_DATE")
+	private Date signHeadOfficerDate;
+	@Column(name = "APPROVER_COMMENT")
+	private String approverComment;
+	@Column(name = "SIGN_APPROVER_FULL_NAME")
+	private String signApproverFullName;
+	@Column(name = "SIGN_APPROVER_POSITION")
+	private String signApproverPosition;
+	@Column(name = "SIGN_APPROVER_DATE")
+	private Date signApproverDate;
+
+	public Long getFormTs0116Id() {
+		return formTs0116Id;
+	}
+
+	public void setFormTs0116Id(Long formTs0116Id) {
+		this.formTs0116Id = formTs0116Id;
+	}
+
+	public String getOfficeCode() {
+		return officeCode;
+	}
+
+	public void setOfficeCode(String officeCode) {
+		this.officeCode = officeCode;
+	}
+
+	public String getBudgetYear() {
+		return budgetYear;
+	}
+
+	public void setBudgetYear(String budgetYear) {
+		this.budgetYear = budgetYear;
+	}
+
+	public String getFormTsNumber() {
+		return formTsNumber;
+	}
+
+	public void setFormTsNumber(String formTsNumber) {
+		this.formTsNumber = formTsNumber;
+	}
+
+	public String getNewRegId() {
+		return newRegId;
+	}
+
+	public void setNewRegId(String newRegId) {
+		this.newRegId = newRegId;
+	}
+
+	public String getDocText() {
+		return docText;
+	}
+
+	public void setDocText(String docText) {
+		this.docText = docText;
+	}
+
+	public String getDocDear() {
+		return docDear;
+	}
+
+	public void setDocDear(String docDear) {
+		this.docDear = docDear;
+	}
+
+	public String getFactoryName() {
+		return factoryName;
+	}
+
+	public void setFactoryName(String factoryName) {
+		this.factoryName = factoryName;
+	}
+
+	public String getFactoryType() {
+		return factoryType;
+	}
+
+	public void setFactoryType(String factoryType) {
+		this.factoryType = factoryType;
+	}
+
+	public Date getRequestDate() {
+		return requestDate;
+	}
+
+	public void setRequestDate(Date requestDate) {
+		this.requestDate = requestDate;
+	}
+
+	public String getRequestType() {
+		return requestType;
+	}
+
+	public void setRequestType(String requestType) {
+		this.requestType = requestType;
+	}
+
+	public String getRequestReason() {
+		return requestReason;
+	}
+
+	public void setRequestReason(String requestReason) {
+		this.requestReason = requestReason;
+	}
+
+	public String getRequestDesc() {
+		return requestDesc;
+	}
+
+	public void setRequestDesc(String requestDesc) {
+		this.requestDesc = requestDesc;
+	}
+
+	public String getFineNoFlag() {
+		return fineNoFlag;
+	}
+
+	public void setFineNoFlag(String fineNoFlag) {
+		this.fineNoFlag = fineNoFlag;
+	}
+
+	public String getFineRefrainFlag() {
+		return fineRefrainFlag;
+	}
+
+	public void setFineRefrainFlag(String fineRefrainFlag) {
+		this.fineRefrainFlag = fineRefrainFlag;
+	}
+
+	public String getFindReduceFlag() {
+		return findReduceFlag;
+	}
+
+	public void setFindReduceFlag(String findReduceFlag) {
+		this.findReduceFlag = findReduceFlag;
+	}
+
+	public BigDecimal getFinePercent() {
+		return finePercent;
+	}
+
+	public void setFinePercent(BigDecimal finePercent) {
+		this.finePercent = finePercent;
+	}
+
+	public String getExtraNoFlag() {
+		return extraNoFlag;
+	}
+
+	public void setExtraNoFlag(String extraNoFlag) {
+		this.extraNoFlag = extraNoFlag;
+	}
+
+	public String getExtraReduceFlag() {
+		return extraReduceFlag;
+	}
+
+	public void setExtraReduceFlag(String extraReduceFlag) {
+		this.extraReduceFlag = extraReduceFlag;
+	}
+
+	public BigDecimal getExtraPercent() {
+		return extraPercent;
+	}
+
+	public void setExtraPercent(BigDecimal extraPercent) {
+		this.extraPercent = extraPercent;
+	}
+
+	public BigDecimal getBeforeTaxAmt() {
+		return beforeTaxAmt;
+	}
+
+	public void setBeforeTaxAmt(BigDecimal beforeTaxAmt) {
+		this.beforeTaxAmt = beforeTaxAmt;
+	}
+
+	public BigDecimal getBeforeFinePercent() {
+		return beforeFinePercent;
+	}
+
+	public void setBeforeFinePercent(BigDecimal beforeFinePercent) {
+		this.beforeFinePercent = beforeFinePercent;
+	}
+
+	public BigDecimal getBeforeFineAmt() {
+		return beforeFineAmt;
+	}
+
+	public void setBeforeFineAmt(BigDecimal beforeFineAmt) {
+		this.beforeFineAmt = beforeFineAmt;
+	}
+
+	public BigDecimal getBeforeExtraAmt() {
+		return beforeExtraAmt;
+	}
+
+	public void setBeforeExtraAmt(BigDecimal beforeExtraAmt) {
+		this.beforeExtraAmt = beforeExtraAmt;
+	}
+
+	public BigDecimal getBeforeMoiAmt() {
+		return beforeMoiAmt;
+	}
+
+	public void setBeforeMoiAmt(BigDecimal beforeMoiAmt) {
+		this.beforeMoiAmt = beforeMoiAmt;
+	}
+
+	public BigDecimal getBeforeSumAmt() {
+		return beforeSumAmt;
+	}
+
+	public void setBeforeSumAmt(BigDecimal beforeSumAmt) {
+		this.beforeSumAmt = beforeSumAmt;
+	}
+
+	public BigDecimal getAfterTaxAmt() {
+		return afterTaxAmt;
+	}
+
+	public void setAfterTaxAmt(BigDecimal afterTaxAmt) {
+		this.afterTaxAmt = afterTaxAmt;
+	}
+
+	public BigDecimal getAfterFinePercent() {
+		return afterFinePercent;
+	}
+
+	public void setAfterFinePercent(BigDecimal afterFinePercent) {
+		this.afterFinePercent = afterFinePercent;
+	}
+
+	public BigDecimal getAfterFineAmt() {
+		return afterFineAmt;
+	}
+
+	public void setAfterFineAmt(BigDecimal afterFineAmt) {
+		this.afterFineAmt = afterFineAmt;
+	}
+
+	public BigDecimal getAfterExtraAmt() {
+		return afterExtraAmt;
+	}
+
+	public void setAfterExtraAmt(BigDecimal afterExtraAmt) {
+		this.afterExtraAmt = afterExtraAmt;
+	}
+
+	public BigDecimal getAfterMoiAmt() {
+		return afterMoiAmt;
+	}
+
+	public void setAfterMoiAmt(BigDecimal afterMoiAmt) {
+		this.afterMoiAmt = afterMoiAmt;
+	}
+
+	public BigDecimal getAfterSumAmt() {
+		return afterSumAmt;
+	}
+
+	public void setAfterSumAmt(BigDecimal afterSumAmt) {
+		this.afterSumAmt = afterSumAmt;
+	}
+
+	public String getSignOfficerFullName() {
+		return signOfficerFullName;
+	}
+
+	public void setSignOfficerFullName(String signOfficerFullName) {
+		this.signOfficerFullName = signOfficerFullName;
+	}
+
+	public String getSignOfficerPosition() {
+		return signOfficerPosition;
+	}
+
+	public void setSignOfficerPosition(String signOfficerPosition) {
+		this.signOfficerPosition = signOfficerPosition;
+	}
+
+	public String getHeadOfficerComment() {
+		return headOfficerComment;
+	}
+
+	public void setHeadOfficerComment(String headOfficerComment) {
+		this.headOfficerComment = headOfficerComment;
+	}
+
+	public String getSignHeadOfficerFullName() {
+		return signHeadOfficerFullName;
+	}
+
+	public void setSignHeadOfficerFullName(String signHeadOfficerFullName) {
+		this.signHeadOfficerFullName = signHeadOfficerFullName;
+	}
+
+	public String getSignHeadOfficerPosition() {
+		return signHeadOfficerPosition;
+	}
+
+	public void setSignHeadOfficerPosition(String signHeadOfficerPosition) {
+		this.signHeadOfficerPosition = signHeadOfficerPosition;
+	}
+
+	public Date getSignHeadOfficerDate() {
+		return signHeadOfficerDate;
+	}
+
+	public void setSignHeadOfficerDate(Date signHeadOfficerDate) {
+		this.signHeadOfficerDate = signHeadOfficerDate;
+	}
+
+	public String getApproverComment() {
+		return approverComment;
+	}
+
+	public void setApproverComment(String approverComment) {
+		this.approverComment = approverComment;
+	}
+
+	public String getSignApproverFullName() {
+		return signApproverFullName;
+	}
+
+	public void setSignApproverFullName(String signApproverFullName) {
+		this.signApproverFullName = signApproverFullName;
+	}
+
+	public String getSignApproverPosition() {
+		return signApproverPosition;
+	}
+
+	public void setSignApproverPosition(String signApproverPosition) {
+		this.signApproverPosition = signApproverPosition;
+	}
+
+	public Date getSignApproverDate() {
+		return signApproverDate;
+	}
+
+	public void setSignApproverDate(Date signApproverDate) {
+		this.signApproverDate = signApproverDate;
+	}
+
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+	}
+
+}
