e0ae4c1dfe0f2742eee380aa81eb60a9aa60ff03
taechapon <taechapon@gmail.com>
2019-04-01 01:31:49 +0700

    Fix bug

--- workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS01171ServiceTest.java
+++ workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS01171ServiceTest.java
@@ -3,6 +3,8 @@ package th.go.excise.ims.ta.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.chrono.ThaiBuddhistDate;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
@@ -10,65 +12,61 @@ import org.junit.Test;
 import th.co.baiwa.buckwaframework.common.constant.ReportConstants.FILE_EXTENSION;
 import th.co.baiwa.buckwaframework.common.constant.ReportConstants.PATH;
 import th.co.baiwa.buckwaframework.common.constant.ReportConstants.REPORT_NAME;
-import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
-import th.go.excise.ims.ta.vo.TaFormTS0117Vo;
+import th.go.excise.ims.ta.vo.TaFormTS01171Vo;
 
 public class TaFormTS01171ServiceTest {
-	private static final String REPORT_FILE = PATH.TEST_PATH + REPORT_NAME.TA_FORM_TS01_17 + "." + FILE_EXTENSION.PDF;
+	
+	private static final String REPORT_FILE = PATH.TEST_PATH + "%s" + "." + FILE_EXTENSION.PDF;
 
 	@Test
 	public void test_generateReport() throws Exception {
+		TaFormTS01171Service taFormTS01171Service = new TaFormTS01171Service();
 
-		TaFormTS0117Service taFormTS0117Service = new TaFormTS0117Service();
-		TaFormTS0117Vo formTS0117Vo = new TaFormTS0117Vo();
-
-		formTS0117Vo.setBookNumber1("23");
-		formTS0117Vo.setBookNumber2("30");
-		formTS0117Vo.setDocTopic("ตรวจสอบภาษี");
-		formTS0117Vo.setDocDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setDocDear("ธนพล ชัยภูมิ");
-		formTS0117Vo.setRefBookNumber1("525225");
-		formTS0117Vo.setRefBookNumber2("554566");
-		formTS0117Vo.setRefDocDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setAuditDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setCallBookNumber1("525225");
-		formTS0117Vo.setCallBookNumber2("525225");
-		formTS0117Vo.setCallBookDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setFactoryName("");
-		formTS0117Vo.setNewRegId("2156651515485115");
-		formTS0117Vo.setFacAddrNo("25/25");
-		formTS0117Vo.setFacMooNo("3");
-		formTS0117Vo.setFacSoiName("อุดมเกียรติ");
-		formTS0117Vo.setFacThnName("สุจธิสานวินิจฉัย");
-		formTS0117Vo.setFacTambolName("หนองนาคำ");
-		formTS0117Vo.setFacAmphurName("ปริมณฑล");
-		formTS0117Vo.setFacProvinceName("หนองบัวลำภู");
-		formTS0117Vo.setFacZipCode("390000");
-		formTS0117Vo.setOfficerFullName("ธนพล ชัยูมิ");
-		formTS0117Vo.setOfficerPosition("ครวจวอบภาษี");
-		formTS0117Vo.setTaxFormDateStart(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setTaxFormDateEnd(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setTestimonyDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setFactDesc("อธิบดีกรมสรรพสามิต เป็นประธานในการประชุม คณะกรรมการพิจารณาเกี่ยวกับการพัฒนามาตรฐานในการตรวจวิเคราะห์สินค้าสรรพสามิต");
-		formTS0117Vo.setLawDesc(" รักษาการในตำแหน่งที่ปรึกษาด้านการพัฒนาและบริหารการจัดเก็บภาษี เป็นประธานการประชุมโครงการเพิ่มประสิทธิภาพการจัดเก็บภาษีสถานบริการไนต์คลับ");
-		formTS0117Vo.setFactoryName2(" ");
-		formTS0117Vo.setTaxAmt(new BigDecimal(253));
-		formTS0117Vo.setFineAmt(new BigDecimal(253));
-		formTS0117Vo.setExtraAmt(new BigDecimal(253));
-		formTS0117Vo.setExciseTaxAmt(new BigDecimal(253));
-		formTS0117Vo.setMoiAmt(new BigDecimal(253));
-		formTS0117Vo.setSumAllTaxAmt(new BigDecimal(253));
-		formTS0117Vo.setExtraDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setPaymentDest("");
-		formTS0117Vo.setPaymentExciseTaxAmt(new BigDecimal(253525426));
-		formTS0117Vo.setPaymentDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setOfficeDest("โรงผลิตเหล้ายโสธร");
-		formTS0117Vo.setOfficeDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
-		formTS0117Vo.setOfficeTime("13.30");
-		formTS0117Vo.setSignOfficerFullName("");
-		formTS0117Vo.setSignOfficerPosition("");
-		byte[] reportFile = taFormTS0117Service.generateReport(formTS0117Vo);
-		IOUtils.write(reportFile, new FileOutputStream(new File(REPORT_FILE)));
-
+		TaFormTS01171Vo formTS01171Vo = new TaFormTS01171Vo();
+		formTS01171Vo.setFormTsNumber("001403");
+		formTS01171Vo.setBookNumber1("กด 3434");
+		formTS01171Vo.setBookNumber2("25623");
+		formTS01171Vo.setDocTopic("หัวข้อของเอกสาร ดูหน่อยนะ");
+		formTS01171Vo.setDocDate(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 4))));
+		formTS01171Vo.setDocDear("");
+		formTS01171Vo.setFactoryName("");
+		formTS01171Vo.setNewRegId("");
+		formTS01171Vo.setFactoryType("");
+		formTS01171Vo.setFacAddrNo("");
+		formTS01171Vo.setFacMooNo("");
+		formTS01171Vo.setFacSoiName("");
+		formTS01171Vo.setFacThnName("");
+		formTS01171Vo.setFacTambolName("");
+		formTS01171Vo.setFacAmphurName("");
+		formTS01171Vo.setFacProvinceName("");
+		formTS01171Vo.setFacZipCode("");
+		formTS01171Vo.setBookType("");
+		formTS01171Vo.setRefBookNumber1("");
+		formTS01171Vo.setRefBookNumber2("");
+		formTS01171Vo.setRefDocDate(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 5))));
+		formTS01171Vo.setAuditDateStart(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 6))));
+		formTS01171Vo.setAuditDateEnd(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 7))));
+		formTS01171Vo.setFactDesc("");
+		formTS01171Vo.setLawDesc("");
+		formTS01171Vo.setFactoryName2("");
+		formTS01171Vo.setTaxAmt(new BigDecimal("987654"));
+		formTS01171Vo.setFineAmt(new BigDecimal("0"));
+		formTS01171Vo.setExtraAmt(null);
+		formTS01171Vo.setExciseTaxAmt(new BigDecimal("12121"));
+		formTS01171Vo.setMoiAmt(new BigDecimal("23232"));
+		formTS01171Vo.setSumAllTaxAmt(new BigDecimal("454545"));
+		formTS01171Vo.setExtraDate(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 8))));
+		formTS01171Vo.setOfficeDest("");
+		formTS01171Vo.setOfficeDate(java.sql.Date.valueOf(LocalDate.from(ThaiBuddhistDate.of(2562, 3, 9))));
+		formTS01171Vo.setOfficeTime("");
+		formTS01171Vo.setSignOfficerFullName("");
+		formTS01171Vo.setSignOfficerPosition("");
+		formTS01171Vo.setOfficeName("");
+		formTS01171Vo.setOffficePhone("");
+		formTS01171Vo.setHeadOfficerFullName("");
+		
+		byte[] reportFile = taFormTS01171Service.generateReport(formTS01171Vo);
+		IOUtils.write(reportFile, new FileOutputStream(new File(String.format(REPORT_FILE, REPORT_NAME.TA_FORM_TS01_17_1))));
 	}
+	
 }
