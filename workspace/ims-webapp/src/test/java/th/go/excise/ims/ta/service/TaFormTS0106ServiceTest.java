f117635379941e9875dcb571b5365d388aa34985
flame123flame <flame.tana@gmail.com>
2019-03-31 11:21:45 +0700

    add file jasper

---
+++ workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/TaFormTS0106ServiceTest.java
@@ -0,0 +1,63 @@
+package th.go.excise.ims.ta.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.FILE_EXTENSION;
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.PATH;
+import th.co.baiwa.buckwaframework.common.constant.ReportConstants.REPORT_NAME;
+import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
+import th.go.excise.ims.ta.vo.TaFormTS0106Vo;
+
+public class TaFormTS0106ServiceTest {
+	private static final String REPORT_FILE = PATH.TEST_PATH + REPORT_NAME.TA_FORM_TS01_06 + "." + FILE_EXTENSION.PDF;
+
+	@Test
+	public void test_generateReport() throws Exception {
+
+		TaFormTS0106Service taFormTS0106Service = new TaFormTS0106Service();
+		TaFormTS0106Vo formTS0106Vo = new TaFormTS0106Vo();
+		formTS0106Vo.setDocPlace("");
+		formTS0106Vo.setDocDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
+		formTS0106Vo.setWriterFullName("");
+		formTS0106Vo.setWriterPositionFlag("");
+		formTS0106Vo.setFactoryName("");
+		formTS0106Vo.setNewRegId("");
+		formTS0106Vo.setFacAddrNo("");
+		formTS0106Vo.setFacMooNo("");
+		formTS0106Vo.setFacSoiName("");
+		formTS0106Vo.setFacThnName("");
+		formTS0106Vo.setFacTambolName("");
+		formTS0106Vo.setFacAmphurName("");
+		formTS0106Vo.setFacProvinceName("");
+		formTS0106Vo.setFacZipCode("");
+		formTS0106Vo.setFacTelNo("");
+		formTS0106Vo.setRefBookNumber1("");
+		formTS0106Vo.setRefBookNumber2("");
+		formTS0106Vo.setRefDocDate(ConvertDateUtils.parseStringToDate("01/03/2562", ConvertDateUtils.DD_MM_YYYY));
+		formTS0106Vo.setAuthFullName("");
+		formTS0106Vo.setAuthAge("");
+		formTS0106Vo.setAuthAddrNo("");
+		formTS0106Vo.setAuthSoiName("");
+		formTS0106Vo.setAuthThnName("");
+		formTS0106Vo.setAuthTambolName("");
+		formTS0106Vo.setAuthAmphurName("");
+		formTS0106Vo.setAuthProvinceName("");
+		formTS0106Vo.setAuthZipCode("");
+		formTS0106Vo.setAuthTelNo("");
+		formTS0106Vo.setAuthCardId("");
+		formTS0106Vo.setAuthCardPlace("");
+		formTS0106Vo.setDocText("");
+		formTS0106Vo.setSignAuthFullName1("");
+		formTS0106Vo.setSignAuthFullName2("");
+		formTS0106Vo.setSignAuthFullName3("");
+		formTS0106Vo.setSignWitnessFullName1("");
+		formTS0106Vo.setSignWitnessFullName2("");
+		byte[] reportFile = taFormTS0106Service.generateReport(formTS0106Vo);
+		IOUtils.write(reportFile, new FileOutputStream(new File(REPORT_FILE)));
+
+	}
+}
