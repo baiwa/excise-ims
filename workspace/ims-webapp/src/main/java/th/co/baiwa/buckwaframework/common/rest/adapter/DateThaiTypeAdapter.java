2fde063e0e022bb25f2e52f660366237a66aa1f6
toffeeDev <saknarin.top21@gmail.com>
2019-04-01 16:38:00 +0700

    updateCode  DateThaiTypeAdapter

--- workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/common/rest/adapter/DateThaiTypeAdapter.java
+++ workspace/ims-webapp/src/main/java/th/co/baiwa/buckwaframework/common/rest/adapter/DateThaiTypeAdapter.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
@@ -29,6 +30,10 @@ public class DateThaiTypeAdapter extends TypeAdapter<Date> {
 
 	@Override
 	public Date read(JsonReader reader) throws IOException {
+		if (reader.peek() == JsonToken.NULL) {
+			reader.nextNull();
+			return null;
+		}
 		return ConvertDateUtils.parseStringToDate(reader.nextString(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH);
 	}
 
