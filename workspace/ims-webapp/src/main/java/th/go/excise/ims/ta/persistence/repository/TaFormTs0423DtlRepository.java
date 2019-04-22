e622967de9bb9adb19382917a113ba1a5c656a18
taechapon <taechapon@gmail.com>
2019-04-22 11:23:34 +0700

    Add Entity and Repository of New TsForm

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaFormTs0423DtlRepository.java
@@ -0,0 +1,8 @@
+package th.go.excise.ims.ta.persistence.repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.ta.persistence.entity.TaFormTs0423Dtl;
+
+public interface TaFormTs0423DtlRepository extends CommonJpaCrudRepository<TaFormTs0423Dtl, Long> {
+
+}
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaFormTs0424HdrRepository.java
@@ -0,0 +1,8 @@
+package th.go.excise.ims.ta.persistence.repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.ta.persistence.entity.TaFormTs0424Hdr;
+
+public interface TaFormTs0424HdrRepository extends CommonJpaCrudRepository<TaFormTs0424Hdr, Long> {
+
+}
