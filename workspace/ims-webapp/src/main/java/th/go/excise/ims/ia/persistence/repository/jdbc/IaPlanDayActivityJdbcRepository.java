4079204233cbf2bbbc5d064c7cd474f6b8b4b67c
flukeTP <fook_1589@hotmail.com>
2019-04-29 15:17:31 +0700

    update response header

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaPlanDayActivityJdbcRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/IaPlanDayActivityJdbcRepository.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
@@ -25,6 +26,26 @@ public class IaPlanDayActivityJdbcRepository {
 		if (idDtl != null) {
 			sql.append(" AND PLAN_DTL_ID = ? ");
 			params.add(idDtl);
+		}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		List<IaPlanDayActivity> planDayAct = commonJdbcTemplate.query(sql.toString(), params.toArray(),
+				new BeanPropertyRowMapper(IaPlanDayActivity.class));
+		return planDayAct;
+	}
+	
+	public List<IaPlanDayActivity> findActivity(BigDecimal idDtl, String activity) {
+		StringBuilder sql = new StringBuilder();
+		List<Object> params = new ArrayList<Object>();
+		sql.append(" SELECT * FROM IA_PLAN_DAY_ACTIVITY WHERE 1=1 AND IS_DELETED='N' ");
+		if (idDtl != null) {
+			sql.append(" AND PLAN_DTL_ID = ? ");
+			params.add(idDtl);
+		}
+		
+		if (StringUtils.isNotBlank(activity)) {
+			sql.append(" AND ACTIVITY = ? ");
+			params.add(activity);
 		}
 
 		@SuppressWarnings({ "rawtypes", "unchecked" })
