78d43c72b140eb39630cfc590366fc08dab2fad1
flukeTP <fook_1589@hotmail.com>
2019-05-02 14:32:44 +0700

    add method departmentVo

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int0913Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int0913Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import th.co.baiwa.buckwaframework.common.bean.ResponseData;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_MESSAGE;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_STATUS;
+import th.co.baiwa.buckwaframework.security.util.UserLoginUtils;
 import th.go.excise.ims.ia.persistence.entity.IaUtilityBill;
 import th.go.excise.ims.ia.service.Int0913Service;
+import th.go.excise.ims.ia.util.ExciseDepartmentUtil;
+import th.go.excise.ims.ia.vo.ExciseDepartmentVo;
 import th.go.excise.ims.ia.vo.Int091301ResultSearchVo;
 import th.go.excise.ims.ia.vo.Int091301SearchVo;
 
@@ -62,6 +66,22 @@ public class Int0913Controller {
 		ResponseData response = new ResponseData();
 		try {
 			int0913Service.deleteIaUtilityBillById(id);
+			response.setMessage(RESPONSE_MESSAGE.SUCCESS);
+			response.setStatus(RESPONSE_STATUS.SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setMessage(RESPONSE_MESSAGE.ERROR500);
+			response.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return response;
+	}
+	
+	@GetMapping("/get-091301-department")
+	@ResponseBody
+	public ResponseData<ExciseDepartmentVo> getDepartment() {
+		ResponseData<ExciseDepartmentVo> response = new ResponseData<ExciseDepartmentVo>();
+		try {
+			response.setData(ExciseDepartmentUtil.getExciseDepartment(UserLoginUtils.getCurrentUserBean().getOfficeCode()));
 			response.setMessage(RESPONSE_MESSAGE.SUCCESS);
 			response.setStatus(RESPONSE_STATUS.SUCCESS);
 		} catch (Exception e) {
--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int091301SearchVo.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int091301SearchVo.java
@@ -7,37 +7,54 @@ public class Int091301SearchVo {
 	private String monthWdPayFrom;
 	private String monthWdPayTo;
 	private Long utilityBillSeq;
-	
+	private String budgetYear;
+
 	public String getOfficeCode() {
 		return officeCode;
 	}
+
 	public void setOfficeCode(String officeCode) {
 		this.officeCode = officeCode;
 	}
+
 	public String getMonthWdPayFrom() {
 		return monthWdPayFrom;
 	}
+
 	public void setMonthWdPayFrom(String monthWdPayFrom) {
 		this.monthWdPayFrom = monthWdPayFrom;
 	}
+
 	public String getMonthWdPayTo() {
 		return monthWdPayTo;
 	}
+
 	public void setMonthWdPayTo(String monthWdPayTo) {
 		this.monthWdPayTo = monthWdPayTo;
 	}
+
 	public String getUbillType() {
 		return ubillType;
 	}
+
 	public void setUbillType(String ubillType) {
 		this.ubillType = ubillType;
 	}
+
 	public Long getUtilityBillSeq() {
 		return utilityBillSeq;
 	}
+
 	public void setUtilityBillSeq(Long utilityBillSeq) {
 		this.utilityBillSeq = utilityBillSeq;
 	}
-	
-	
+
+	public String getBudgetYear() {
+		return budgetYear;
+	}
+
+	public void setBudgetYear(String budgetYear) {
+		this.budgetYear = budgetYear;
+	}
+
 }
