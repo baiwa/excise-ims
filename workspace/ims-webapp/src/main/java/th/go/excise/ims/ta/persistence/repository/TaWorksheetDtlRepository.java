2a4741fb885f2fbd88ab27cbefca9d0995a8ced8
taechapon <taechapon@gmail.com>
2019-05-13 01:06:26 +0700

    Fix for wsRegfri4000

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaWorksheetDtlRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/persistence/repository/TaWorksheetDtlRepository.java
@@ -14,7 +14,7 @@ public interface TaWorksheetDtlRepository extends CommonJpaCrudRepository<TaWork
 	@Query("select e from #{#entityName} e where e.isDeleted = '" + FLAG.N_FLAG + "' and e.analysisNumber = :analysisNumber and e.newRegId = :newRegId")
 	public TaWorksheetDtl findByAnalysisNumberAndNewRegId(@Param("analysisNumber") String analysisNumber, @Param("newRegId")  String newRegId);
 	
-	@Query("select new java.lang.String(e.newRegId) from #{#entityName} e where e.isDeleted = '" + FLAG.N_FLAG + "' and e.analysisNumber = :analysisNumber")
+	@Query("select distinct e.newRegId from #{#entityName} e where e.isDeleted = '" + FLAG.N_FLAG + "' and e.analysisNumber = :analysisNumber")
 	public List<String> findNewRegIdByAnalysisNumber(@Param("analysisNumber") String analysisNumber);
 
 }
