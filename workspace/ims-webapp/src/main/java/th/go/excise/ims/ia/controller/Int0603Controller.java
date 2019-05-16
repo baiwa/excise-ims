5d9d615ab0f616658fb6c0ac58bd7f6f09aff8a9
toffeeDev <toffeeDev>
2019-05-16 15:39:53 +0700

    edit int0603

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int0603Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int0603Controller.java
@@ -2,6 +2,7 @@ package th.go.excise.ims.ia.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,26 +12,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import th.co.baiwa.buckwaframework.common.bean.ResponseData;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_MESSAGE;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_STATUS;
-import th.go.excise.ims.ia.vo.Int0601RequestVo;
-import th.go.excise.ims.ia.vo.Ws_Incfri8020IncVo;
+import th.go.excise.ims.ia.service.Int0603Service;
+import th.go.excise.ims.ia.vo.Int0602FormVo;
+import th.go.excise.ims.ia.vo.Int0602ResultTab1Vo;
 
 @Controller
 @RequestMapping("/api/ia/int06/03")
 public class Int0603Controller {
 
-//	@PostMapping("/find-tab1")
-//	@ResponseBody
-//	public ResponseData<List<Ws_Incfri8020IncVo>> findTab1(@RequestBody Int0601RequestVo request) {
-//		ResponseData<List<Ws_Incfri8020IncVo>> response = new ResponseData<List<Ws_Incfri8020IncVo>>();
-//		try {
-//			response.setData(int0601Service.findTab1ByCriteria(request));
-//			response.setMessage(RESPONSE_MESSAGE.SUCCESS);
-//			response.setStatus(RESPONSE_STATUS.SUCCESS);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			response.setMessage(RESPONSE_MESSAGE.ERROR500);
-//			response.setStatus(RESPONSE_STATUS.FAILED);
-//		}
-//		return response;
-//	}
+	@Autowired
+	private Int0603Service int0603Service;
+
+	@PostMapping("/find-by-criteria")
+	@ResponseBody
+	public ResponseData<List<Int0602ResultTab1Vo>> findByCriteria(@RequestBody Int0602FormVo request) {
+		ResponseData<List<Int0602ResultTab1Vo>> response = new ResponseData<List<Int0602ResultTab1Vo>>();
+		try {
+			response.setData(int0603Service.findByCriteria(request));
+			response.setMessage(RESPONSE_MESSAGE.SUCCESS);
+			response.setStatus(RESPONSE_STATUS.SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setMessage(RESPONSE_MESSAGE.ERROR500);
+			response.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return response;
+	}
 }
