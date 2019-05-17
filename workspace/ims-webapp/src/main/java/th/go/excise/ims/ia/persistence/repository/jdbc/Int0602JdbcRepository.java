8cbbeb6917b8d8db8446590c7290a45adfaf707e
BaiwaDev <pavit@baiwa.co.th>
2019-05-17 18:13:01 +0700

    Change type licDate

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/Int0602JdbcRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/Int0602JdbcRepository.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import th.co.baiwa.buckwaframework.common.constant.CommonConstants.FLAG;
 import th.co.baiwa.buckwaframework.common.persistence.jdbc.CommonJdbcTemplate;
 import th.co.baiwa.buckwaframework.common.util.ConvertDateUtils;
 import th.co.baiwa.buckwaframework.common.util.LocalDateConverter;
 import th.co.baiwa.buckwaframework.common.util.LocalDateTimeConverter;
 import th.co.baiwa.buckwaframework.common.util.NumberUtils;
 import th.go.excise.ims.ia.vo.Int0602FormVo;
+import th.go.excise.ims.ia.vo.Int0602ResultTab2Vo;
 import th.go.excise.ims.ws.persistence.entity.WsLicfri6010;
 
 @Repository
@@ -37,14 +39,14 @@ public class Int0602JdbcRepository {
 			paramList.add(vo.getOfficeCode());
 		}
 		if (StringUtils.isNotBlank(vo.getLicDateFrom())) {
-			sql.append(" AND WS.LIC_DATE > ? ");
+			sql.append(" AND WS.LIC_DATE >= ? ");
 			Date date = ConvertDateUtils.parseStringToDate(vo.getLicDateFrom(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH);
-			paramList.add(ConvertDateUtils.formatDateToString(date, ConvertDateUtils.YYYYMMDD, ConvertDateUtils.LOCAL_EN));
+			paramList.add(date);
 		}
 		if (StringUtils.isNotBlank(vo.getLicDateTo())) {
-			sql.append(" AND WS.LIC_DATE < ? ");
+			sql.append(" AND WS.LIC_DATE <= ? ");
 			Date date = ConvertDateUtils.parseStringToDate(vo.getLicDateTo(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH);
-			paramList.add(ConvertDateUtils.formatDateToString(date, ConvertDateUtils.YYYYMMDD, ConvertDateUtils.LOCAL_EN));
+			paramList.add(date);
 		}
 		sql.append(" ORDER BY ").append(strOrder);
 		return commonJdbcTemplate.query(sql.toString(), paramList.toArray(), tab1RowMapper);
@@ -79,6 +81,45 @@ public class Int0602JdbcRepository {
 			vo.setCreatedDate(LocalDateTimeConverter.convertToEntityAttribute(rs.getTimestamp("CREATED_DATE")));
 			vo.setUpdatedBy(rs.getString("UPDATED_BY"));
 			vo.setUpdatedDate(LocalDateTimeConverter.convertToEntityAttribute(rs.getTimestamp("UPDATED_DATE")));
+			return vo;
+		}
+	};
+	
+	
+	public List<Int0602ResultTab2Vo> findTab2Criteria(Int0602FormVo vo) {
+		List<Object> paramList = new ArrayList<>();
+		StringBuilder sql = new StringBuilder();
+		sql.append(" SELECT WS.INC_CODE , WS.LIC_NAME , SUM(WS.LIC_PRICE) LIC_PRICE, COUNT(1) LIC_COUNT FROM WS_LICFRI6010 WS ");
+		sql.append(" WHERE WS.IS_DELETED = '").append(FLAG.N_FLAG).append("'  ");
+		if (StringUtils.isNotBlank(vo.getOfficeCode())) {
+			sql.append(" AND WS.OFFCODE LIKE ? ");
+			paramList.add(vo.getOfficeCode());
+		}
+		if (StringUtils.isNotBlank(vo.getLicDateFrom())) {
+			sql.append(" AND WS.LIC_DATE >= ? ");
+			Date date = ConvertDateUtils.parseStringToDate(vo.getLicDateFrom(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH);
+			paramList.add(date);
+		}
+		if (StringUtils.isNotBlank(vo.getLicDateTo())) {
+			sql.append(" AND WS.LIC_DATE <= ? ");
+			Date date = ConvertDateUtils.parseStringToDate(vo.getLicDateTo(), ConvertDateUtils.DD_MM_YYYY, ConvertDateUtils.LOCAL_TH);
+			paramList.add(date);
+		}
+		sql.append(" GROUP BY WS.INC_CODE , WS.LIC_NAME ");
+		sql.append(" ORDER BY COUNT(1) DESC ");
+		return commonJdbcTemplate.query(sql.toString(), paramList.toArray(), tab2RowMapper);
+
+	}
+	
+	private RowMapper<Int0602ResultTab2Vo> tab2RowMapper = new RowMapper<Int0602ResultTab2Vo>() {
+		@Override
+		public Int0602ResultTab2Vo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Int0602ResultTab2Vo vo = new Int0602ResultTab2Vo();
+			vo.setTaxCode(rs.getString("INC_CODE"));
+			vo.setLicName(rs.getString("LIC_NAME"));
+			vo.setLicCount(rs.getLong("LIC_COUNT"));
+			vo.setLicPrice(rs.getBigDecimal("LIC_PRICE"));
+			
 			return vo;
 		}
 	};
