d63d34c7211f2dd990c96b8a13418dc7948d4d25
flukeTP <fook_1589@hotmail.com>
2019-05-24 10:21:47 +0700

    add Vo

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int1301Vo.java
@@ -0,0 +1,19 @@
+package th.go.excise.ims.ia.vo;
+
+import java.util.List;
+
+import th.go.excise.ims.ws.persistence.entity.WsPmAssessD;
+import th.go.excise.ims.ws.persistence.entity.WsPmAssessH;
+
+public class Int1301Vo {
+	private List<WsPmAssessHVo> header;
+
+	public List<WsPmAssessHVo> getHeader() {
+		return header;
+	}
+
+	public void setHeader(List<WsPmAssessHVo> header) {
+		this.header = header;
+	}
+
+}
