fa7a4e0219c9f91ea12383bbdd19407c7ff1838b
toffeeDev <toffeeDev>
2019-05-28 13:50:51 +0700

    int1305

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/IaAuditPmcommitHVo.java
@@ -0,0 +1,78 @@
+package th.go.excise.ims.ia.vo;
+
+public class IaAuditPmcommitHVo {
+
+	private Long auditPmcommitId;
+	private String auditPmcommitNo;
+	private String officeCode;
+	private String budgetYear;
+	private String urlLink;
+	private String auditFlag;
+	private String conditionText;
+	private String criteriaText;
+
+	public Long getAuditPmcommitId() {
+		return auditPmcommitId;
+	}
+
+	public void setAuditPmcommitId(Long auditPmcommitId) {
+		this.auditPmcommitId = auditPmcommitId;
+	}
+
+	public String getAuditPmcommitNo() {
+		return auditPmcommitNo;
+	}
+
+	public void setAuditPmcommitNo(String auditPmcommitNo) {
+		this.auditPmcommitNo = auditPmcommitNo;
+	}
+
+	public String getOfficeCode() {
+		return officeCode;
+	}
+
+	public void setOfficeCode(String officeCode) {
+		this.officeCode = officeCode;
+	}
+
+	public String getBudgetYear() {
+		return budgetYear;
+	}
+
+	public void setBudgetYear(String budgetYear) {
+		this.budgetYear = budgetYear;
+	}
+
+	public String getUrlLink() {
+		return urlLink;
+	}
+
+	public void setUrlLink(String urlLink) {
+		this.urlLink = urlLink;
+	}
+
+	public String getAuditFlag() {
+		return auditFlag;
+	}
+
+	public void setAuditFlag(String auditFlag) {
+		this.auditFlag = auditFlag;
+	}
+
+	public String getConditionText() {
+		return conditionText;
+	}
+
+	public void setConditionText(String conditionText) {
+		this.conditionText = conditionText;
+	}
+
+	public String getCriteriaText() {
+		return criteriaText;
+	}
+
+	public void setCriteriaText(String criteriaText) {
+		this.criteriaText = criteriaText;
+	}
+
+}
