0b5a6562929a1ba510b110b7494a59073c27ee26
flukeTP <fook_1589@hotmail.com>
2019-05-31 10:53:07 +0700

    edit field order by

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/Int0913JdbcRepository.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/jdbc/Int0913JdbcRepository.java
@@ -56,7 +56,7 @@ public class Int0913JdbcRepository {
 			paramList.add((year)+ProjectConstants.QUARTER.Q4[2]);
 		}
 		
-		sql.append(" ORDER BY MONTH_WD_PAY DESC ");
+		sql.append(" ORDER BY MONTH_WD_PAY DESC, RECEIVE_INV_DATE DESC ");
 		return commonJdbcTemplate.query(sql.toString(), paramList.toArray(), int091301Mapping);
 	}
 
