062a8161ab3a16ee949ba4592f41841e6ba85065
phattarapong58021725 <32901174+phattarapong58021725@users.noreply.github.com>
2019-06-04 16:57:04 +0700

    edit 2 4

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxRateService.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxRateService.java
@@ -1,5 +1,11 @@
 package th.go.excise.ims.ta.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.chrono.ThaiBuddhistDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,6 +16,8 @@ import org.springframework.stereotype.Service;
 import th.go.excise.ims.ta.persistence.repository.TaPaperBaD4Repository;
 import th.go.excise.ims.ta.vo.BasicAnalysisFormVo;
 import th.go.excise.ims.ta.vo.BasicAnalysisTaxRateVo;
+import th.go.excise.ims.ws.persistence.entity.WsAnafri0001D;
+import th.go.excise.ims.ws.persistence.repository.WsAnafri0001DRepository;
 
 @Service
 public class BasicAnalysisTaxRateService extends AbstractBasicAnalysisService<BasicAnalysisTaxRateVo> {
@@ -18,11 +26,41 @@ public class BasicAnalysisTaxRateService extends AbstractBasicAnalysisService<Ba
 
 	@Autowired
 	private TaPaperBaD4Repository taPaperBaD4Repository;
+	
+	@Autowired
+	private WsAnafri0001DRepository wsAnafri0001DRepository;
 
 	@Override
 	protected List<BasicAnalysisTaxRateVo> inquiryByWs(BasicAnalysisFormVo formVo) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("inquiryByWs");
+
+		LocalDate localDateStart = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getStartDate().split("/")[1]),Integer.parseInt(formVo.getStartDate().split("/")[0]), 1));
+		LocalDate localDateEnd = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getEndDate().split("/")[1]),Integer.parseInt(formVo.getEndDate().split("/")[0]), 1));
+
+		String dateStart = localDateStart.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+		String dateEnd = localDateEnd.with(TemporalAdjusters.lastDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+
+		List<WsAnafri0001D> wsAna0001DList = wsAnafri0001DRepository.findProductListByBasicAnalysisFormVo(formVo.getNewRegId(), formVo.getDutyGroupId(), dateStart, dateEnd);
+	
+		List<BasicAnalysisTaxRateVo> voList = new ArrayList<>();
+		BasicAnalysisTaxRateVo vo = null;
+		
+		for (WsAnafri0001D wsAnafri0001D : wsAna0001DList) {
+			vo = new BasicAnalysisTaxRateVo();
+			vo.setGoodsDesc(wsAnafri0001D.getProductName());
+			vo.setTaxRateByPrice(wsAnafri0001D.getValueRate());
+			vo.setTaxRateByQty(wsAnafri0001D.getQtyRate());
+			BigDecimal anaTaxRateByPrice = null;
+			BigDecimal anaTaxRateByQty = null;
+			BigDecimal diffTaxRateByPrice = null;
+			BigDecimal diffTaxRateByQty = null;
+			vo.setAnaTaxRateByPrice(anaTaxRateByPrice);
+			vo.setAnaTaxRateByQty(anaTaxRateByQty);
+			vo.setDiffTaxRateByPrice(diffTaxRateByPrice);
+			vo.setDiffTaxRateByQty(diffTaxRateByQty);	
+			voList.add(vo);
+		}
+		return voList;
 	}
 
 	@Override
--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxRetailPriceService.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxRetailPriceService.java
@@ -1,5 +1,12 @@
 package th.go.excise.ims.ta.service;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.chrono.ThaiBuddhistDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,20 +16,48 @@ import org.springframework.stereotype.Service;
 
 import th.go.excise.ims.ta.persistence.repository.TaPaperBaD2Repository;
 import th.go.excise.ims.ta.vo.BasicAnalysisFormVo;
+import th.go.excise.ims.ta.vo.BasicAnalysisIncomeCompareLastMonthVo;
 import th.go.excise.ims.ta.vo.BasicAnalysisTaxRetailPriceVo;
+import th.go.excise.ims.ws.persistence.entity.WsAnafri0001D;
+import th.go.excise.ims.ws.persistence.repository.WsAnafri0001DRepository;
 
 @Service
 public class BasicAnalysisTaxRetailPriceService extends AbstractBasicAnalysisService<BasicAnalysisTaxRetailPriceVo> {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(BasicAnalysisTaxRetailPriceService.class);
-	
+
 	@Autowired
 	private TaPaperBaD2Repository taPaperBaD2Repository;
 
+	@Autowired
+	private WsAnafri0001DRepository wsAnafri0001DRepository;
+
 	@Override
 	protected List<BasicAnalysisTaxRetailPriceVo> inquiryByWs(BasicAnalysisFormVo formVo) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("inquiryByWs");
+
+		LocalDate localDateStart = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getStartDate().split("/")[1]),Integer.parseInt(formVo.getStartDate().split("/")[0]), 1));
+		LocalDate localDateEnd = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getEndDate().split("/")[1]),Integer.parseInt(formVo.getEndDate().split("/")[0]), 1));
+
+		String dateStart = localDateStart.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+		String dateEnd = localDateEnd.with(TemporalAdjusters.lastDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+
+		List<WsAnafri0001D> wsAna0001DList = wsAnafri0001DRepository.findProductListByBasicAnalysisFormVo(formVo.getNewRegId(), formVo.getDutyGroupId(), dateStart, dateEnd);
+	
+		List<BasicAnalysisTaxRetailPriceVo> voList = new ArrayList<>();
+		BasicAnalysisTaxRetailPriceVo vo = null;
+		
+		for (WsAnafri0001D wsAnafri0001D : wsAna0001DList) {
+			vo = new BasicAnalysisTaxRetailPriceVo();
+			vo.setGoodsDesc(wsAnafri0001D.getProductName());
+			vo.setTaxInformPrice(wsAnafri0001D.getProductPrice());
+			BigDecimal informPrice = null;
+			vo.setInformPrice(informPrice);
+			BigDecimal diffTaxInformPrice = null;
+			vo.setDiffTaxInformPrice(diffTaxInformPrice);
+			voList.add(vo);
+		}
+		return voList;
 	}
 
 	@Override
@@ -34,7 +69,7 @@ public class BasicAnalysisTaxRetailPriceService extends AbstractBasicAnalysisSer
 	@Override
 	protected void save(BasicAnalysisFormVo formVo) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
