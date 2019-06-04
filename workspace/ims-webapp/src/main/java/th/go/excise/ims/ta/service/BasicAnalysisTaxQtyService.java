5eb0155732a1b4e299e7dc596f3a90cef2508a91
taechapon <taechapon@gmail.com>
2019-06-04 14:11:47 +0700

    Initial Data from WsAnafri0001

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxQtyService.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ta/service/BasicAnalysisTaxQtyService.java
@@ -1,5 +1,9 @@
 package th.go.excise.ims.ta.service;
 
+import java.time.LocalDate;
+import java.time.chrono.ThaiBuddhistDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,6 +14,8 @@ import org.springframework.stereotype.Service;
 import th.go.excise.ims.ta.persistence.repository.TaPaperBaD1Repository;
 import th.go.excise.ims.ta.vo.BasicAnalysisFormVo;
 import th.go.excise.ims.ta.vo.BasicAnalysisTaxQtyVo;
+import th.go.excise.ims.ws.persistence.entity.WsAnafri0001D;
+import th.go.excise.ims.ws.persistence.repository.WsAnafri0001DRepository;
 
 @Service
 public class BasicAnalysisTaxQtyService extends AbstractBasicAnalysisService<BasicAnalysisTaxQtyVo> {
@@ -18,10 +24,21 @@ public class BasicAnalysisTaxQtyService extends AbstractBasicAnalysisService<Bas
 	
 	@Autowired
 	private TaPaperBaD1Repository taPaperBaD1Repository;
+	@Autowired
+	private WsAnafri0001DRepository wsAnafri0001DRepository;
 	
 	@Override
 	protected List<BasicAnalysisTaxQtyVo> inquiryByWs(BasicAnalysisFormVo formVo) {
-		// TODO Auto-generated method stub
+		logger.info("inquiryByWs");
+		
+		LocalDate localDateStart = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getStartDate().split("/")[1]), Integer.parseInt(formVo.getStartDate().split("/")[0]), 1));
+		LocalDate localDateEnd = LocalDate.from(ThaiBuddhistDate.of(Integer.parseInt(formVo.getEndDate().split("/")[1]), Integer.parseInt(formVo.getEndDate().split("/")[0]), 1));
+		
+		String dateStart = localDateStart.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+		String dateEnd = localDateEnd.with(TemporalAdjusters.lastDayOfMonth()).format(DateTimeFormatter.BASIC_ISO_DATE);
+		
+		List<WsAnafri0001D> wsAna0001DList = wsAnafri0001DRepository.findProductListByBasicAnalysisFormVo(formVo.getNewRegId(), formVo.getDutyGroupId(), dateStart, dateEnd);
+		
 		return null;
 	}
 
