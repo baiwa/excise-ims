f822293ccec8c97fe45caab921f55bba433ba237
BaiwaDev <pavit@baiwa.co.th>
2019-06-07 11:29:59 +0700

    commit function

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int15Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int15Controller.java
@@ -19,21 +19,78 @@ import th.co.baiwa.buckwaframework.common.constant.ProjectConstant;
 import th.co.baiwa.buckwaframework.common.constant.ProjectConstant.RESPONSE_STATUS;
 import th.co.baiwa.buckwaframework.support.ApplicationCache;
 import th.go.excise.ims.ia.service.IaGfdrawAccountService;
-import th.go.excise.ims.ia.vo.Int030101FormVo;
+import th.go.excise.ims.ia.vo.Int15UploadVo;
 
 @Controller
 @RequestMapping("/api/ia/int15/01")
 public class Int15Controller {
 	private Logger logger = LoggerFactory.getLogger(Int15Controller.class);
-	
+
 	@Autowired
 	private IaGfdrawAccountService iaGfdrawAccountService;
+
 	
-	@PostMapping("/upload")
+
+	@PostMapping("/upload-t1")
 	@ResponseBody
-	public ResponseData<String> upload(@ModelAttribute Int030101FormVo form)
-			throws EncryptedDocumentException, InvalidFormatException, IOException {
-		
+	public ResponseData<String> uploadT1(@ModelAttribute Int15UploadVo form) throws EncryptedDocumentException, InvalidFormatException, IOException {
+
+		ResponseData<String> responseData = new ResponseData<String>();
+		try {
+			MultipartFile file = form.getFile();
+			iaGfdrawAccountService.addDataByExcel(file);
+			responseData.setMessage(ApplicationCache.getMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.SUCCESS_CODE).getMessageTh());
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+
+		} catch (Exception e) {
+			logger.error("Int030102Controller upload : ", e);
+			responseData.setMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return responseData;
+	}
+
+	@PostMapping("/upload-t2")
+	@ResponseBody
+	public ResponseData<String> uploadT2(@ModelAttribute Int15UploadVo form) throws EncryptedDocumentException, InvalidFormatException, IOException {
+
+		ResponseData<String> responseData = new ResponseData<String>();
+		try {
+			MultipartFile file = form.getFile();
+			iaGfdrawAccountService.addDataByExcel(file);
+			responseData.setMessage(ApplicationCache.getMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.SUCCESS_CODE).getMessageTh());
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+
+		} catch (Exception e) {
+			logger.error("Int030102Controller upload : ", e);
+			responseData.setMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return responseData;
+	}
+
+	@PostMapping("/upload-t3")
+	@ResponseBody
+	public ResponseData<String> uploadT3(@ModelAttribute Int15UploadVo form) throws EncryptedDocumentException, InvalidFormatException, IOException {
+
+		ResponseData<String> responseData = new ResponseData<String>();
+		try {
+			MultipartFile file = form.getFile();
+			iaGfdrawAccountService.addDataByExcel(file);
+			responseData.setMessage(ApplicationCache.getMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.SUCCESS_CODE).getMessageTh());
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+
+		} catch (Exception e) {
+			logger.error("Int030102Controller upload : ", e);
+			responseData.setMessage(ProjectConstant.RESPONSE_MESSAGE.SAVE.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return responseData;
+	}
+	@PostMapping("/upload-t4")
+	@ResponseBody
+	public ResponseData<String> uploadT4(@ModelAttribute Int15UploadVo form) throws EncryptedDocumentException, InvalidFormatException, IOException {
+
 		ResponseData<String> responseData = new ResponseData<String>();
 		try {
 			MultipartFile file = form.getFile();
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int15Service.java
@@ -0,0 +1,12 @@
+package th.go.excise.ims.ia.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Int15Service {
+	
+	public void uoloadService() {
+		
+	}
+
+}
