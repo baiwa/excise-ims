962bcb94cb75c01483283df67c1286fee65cfe39
FlukeTP <fook_1589@hotmail.com>
2019-06-12 19:17:41 +0700

    update Int0802

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0802Service.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0802Service.java
@@ -1,5 +1,6 @@
 package th.go.excise.ims.ia.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +25,34 @@ public class Int0802Service {
 	public List<Int0802Vo> getResultByCondition(Int0802SearchVo reqeust) {
 		List<Int0802Vo> response = new ArrayList<Int0802Vo>();
 		if ("Y".equals(reqeust.getFlagSearch())) {
+			BigDecimal sumDebit2 = BigDecimal.ZERO;
+			BigDecimal sumCredit2 = BigDecimal.ZERO;
+			BigDecimal sumDiffDebit = BigDecimal.ZERO;
+			BigDecimal sumDiffCredit = BigDecimal.ZERO;
+			
 			response = iaGftrialBalanceRepositoryl.findDiferrenceByCondition(reqeust);
+			for (Int0802Vo int0802Vo : response) {
+				if(int0802Vo.getCurrAmt() != null) {
+					if ("40".equals(int0802Vo.getPkCode())) {
+						int0802Vo.setDebit2(int0802Vo.getCurrAmt());
+						int0802Vo.setSumDebit2(sumDebit2.add(int0802Vo.getCurrAmt()));
+					} else if ("50".equals(int0802Vo.getPkCode())) {
+						int0802Vo.setCredit2(int0802Vo.getCurrAmt());
+						int0802Vo.setSumCredit2(sumCredit2.add(int0802Vo.getCurrAmt()));
+					}
+				}
+				
+				int0802Vo.setDiffCredit(checkNullBigdecimal(int0802Vo.getCredit()).subtract(checkNullBigdecimal(int0802Vo.getCredit2())));
+				int0802Vo.setDiffDebit(checkNullBigdecimal(int0802Vo.getDebit()).subtract(checkNullBigdecimal(int0802Vo.getDebit2())));
+				int0802Vo.setSumDiffCredit(sumDiffCredit.add(int0802Vo.getDiffCredit()));
+				int0802Vo.setSumDiffDebit(sumDiffDebit.add(int0802Vo.getDiffDebit()));
+			}
 		}
 		return response;
+	}
+	
+	private BigDecimal checkNullBigdecimal(BigDecimal param) {
+		return param != null ? param: BigDecimal.ZERO;
 	}
 
 }
--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0802Vo.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0802Vo.java
@@ -15,6 +15,26 @@ public class Int0802Vo {
 	private String pkCode;
 	private BigDecimal currAmt;
 
+	/* summary */
+	private BigDecimal sumCarryForward;
+	private BigDecimal sumBringForward;
+	private BigDecimal sumDebit;
+	private BigDecimal sumCredit;
+	private BigDecimal sumDebit2;
+	private BigDecimal sumCredit2;
+	private BigDecimal sumDebit3;
+	private BigDecimal sumCredit3;
+	private BigDecimal sumDiffDebit;
+	private BigDecimal sumDiffCredit;
+
+	/* custom */
+	private BigDecimal debit2;
+	private BigDecimal credit2;
+	private BigDecimal debit3;
+	private BigDecimal credit3;
+	private BigDecimal diffDebit;
+	private BigDecimal diffCredit;
+
 	public String getAccNo() {
 		return accNo;
 	}
@@ -77,6 +97,134 @@ public class Int0802Vo {
 
 	public void setCurrAmt(BigDecimal currAmt) {
 		this.currAmt = currAmt;
+	}
+
+	public BigDecimal getSumCarryForward() {
+		return sumCarryForward;
+	}
+
+	public void setSumCarryForward(BigDecimal sumCarryForward) {
+		this.sumCarryForward = sumCarryForward;
+	}
+
+	public BigDecimal getSumBringForward() {
+		return sumBringForward;
+	}
+
+	public void setSumBringForward(BigDecimal sumBringForward) {
+		this.sumBringForward = sumBringForward;
+	}
+
+	public BigDecimal getSumDebit() {
+		return sumDebit;
+	}
+
+	public void setSumDebit(BigDecimal sumDebit) {
+		this.sumDebit = sumDebit;
+	}
+
+	public BigDecimal getSumCredit() {
+		return sumCredit;
+	}
+
+	public void setSumCredit(BigDecimal sumCredit) {
+		this.sumCredit = sumCredit;
+	}
+
+	public BigDecimal getSumDebit2() {
+		return sumDebit2;
+	}
+
+	public void setSumDebit2(BigDecimal sumDebit2) {
+		this.sumDebit2 = sumDebit2;
+	}
+
+	public BigDecimal getSumCredit2() {
+		return sumCredit2;
+	}
+
+	public void setSumCredit2(BigDecimal sumCredit2) {
+		this.sumCredit2 = sumCredit2;
+	}
+
+	public BigDecimal getSumDebit3() {
+		return sumDebit3;
+	}
+
+	public void setSumDebit3(BigDecimal sumDebit3) {
+		this.sumDebit3 = sumDebit3;
+	}
+
+	public BigDecimal getSumCredit3() {
+		return sumCredit3;
+	}
+
+	public void setSumCredit3(BigDecimal sumCredit3) {
+		this.sumCredit3 = sumCredit3;
+	}
+
+	public BigDecimal getSumDiffDebit() {
+		return sumDiffDebit;
+	}
+
+	public void setSumDiffDebit(BigDecimal sumDiffDebit) {
+		this.sumDiffDebit = sumDiffDebit;
+	}
+
+	public BigDecimal getSumDiffCredit() {
+		return sumDiffCredit;
+	}
+
+	public void setSumDiffCredit(BigDecimal sumDiffCredit) {
+		this.sumDiffCredit = sumDiffCredit;
+	}
+
+	public BigDecimal getDebit2() {
+		return debit2;
+	}
+
+	public void setDebit2(BigDecimal debit2) {
+		this.debit2 = debit2;
+	}
+
+	public BigDecimal getCredit2() {
+		return credit2;
+	}
+
+	public void setCredit2(BigDecimal credit2) {
+		this.credit2 = credit2;
+	}
+
+	public BigDecimal getDebit3() {
+		return debit3;
+	}
+
+	public void setDebit3(BigDecimal debit3) {
+		this.debit3 = debit3;
+	}
+
+	public BigDecimal getCredit3() {
+		return credit3;
+	}
+
+	public void setCredit3(BigDecimal credit3) {
+		this.credit3 = credit3;
+	}
+
+	public BigDecimal getDiffDebit() {
+		return diffDebit;
+	}
+
+	public void setDiffDebit(BigDecimal diffDebit) {
+		this.diffDebit = diffDebit;
+	}
+
+	public BigDecimal getDiffCredit() {
+		return diffCredit;
+	}
+
+	public void setDiffCredit(BigDecimal diffCredit) {
+		this.diffCredit = diffCredit;
 	}
 
 }
