3897660b73e637b1a2db6378768d82abc54b8487
phattarapong58021725 <32901174+phattarapong58021725@users.noreply.github.com>
2019-06-12 15:47:24 +0700

    edit int0501

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/persistence/repository/IaEstimateExpD1Repository.java
@@ -0,0 +1,12 @@
+
+package th.go.excise.ims.ia.persistence.repository;
+
+import th.co.baiwa.buckwaframework.common.persistence.repository.CommonJpaCrudRepository;
+import th.go.excise.ims.ia.persistence.entity.IaEstimateExpD1;
+
+public interface IaEstimateExpD1Repository
+    extends CommonJpaCrudRepository<IaEstimateExpD1, Long>
+{
+
+
+}
