848139a2e6eefba77795951fac07acad19dc91e7
FlukeTP <fook_1589@hotmail.com>
2019-06-13 17:46:42 +0700

    add footer

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0802Service.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/service/Int0802Service.java
@@ -34,6 +34,8 @@ public class Int0802Service {
 			BigDecimal sumCredit2 = BigDecimal.ZERO;
 			BigDecimal sumDiffDebit = BigDecimal.ZERO;
 			BigDecimal sumDiffCredit = BigDecimal.ZERO;
+			BigDecimal sumBringForward = BigDecimal.ZERO;
+			BigDecimal sumCarryForward = BigDecimal.ZERO;
 
 			/* _______________ year concat period _______________ */
 			reqeust.setPeriodFromYear((ConvertDateUtils.formatDateToString(
@@ -50,10 +52,10 @@ public class Int0802Service {
 				if (int0802Vo.getCurrAmt() != null) {
 					if ("40".equals(int0802Vo.getPkCode())) {
 						int0802Vo.setDebit2(int0802Vo.getCurrAmt());
-						int0802Vo.setSumDebit2(sumDebit2.add(int0802Vo.getCurrAmt()));
+						sumDebit2 = sumDebit2.add(int0802Vo.getCurrAmt());
 					} else if ("50".equals(int0802Vo.getPkCode())) {
 						int0802Vo.setCredit2(int0802Vo.getCurrAmt());
-						int0802Vo.setSumCredit2(sumCredit2.add(int0802Vo.getCurrAmt()));
+						sumCredit2 = sumCredit2.add(int0802Vo.getCurrAmt());
 					}
 				}
 
@@ -64,15 +66,30 @@ public class Int0802Service {
 						.subtract(NumberUtils.nullToZero(int0802Vo.getDebit2())));
 
 				/* _______________ summary credit and debit _______________ */
-				int0802Vo.setSumDebit(
-						NumberUtils.nullToZero(sumDebit.add(NumberUtils.nullToZero(int0802Vo.getDebit()))));
-				int0802Vo.setSumCredit(
-						NumberUtils.nullToZero(sumCredit.add(NumberUtils.nullToZero(int0802Vo.getCredit()))));
+				sumDebit = NumberUtils.nullToZero(sumDebit.add(NumberUtils.nullToZero(int0802Vo.getDebit())));
+				sumCredit = NumberUtils.nullToZero(sumCredit.add(NumberUtils.nullToZero(int0802Vo.getCredit())));
 
 				/* _______________ summary difference credit and debit _______________ */
-				int0802Vo.setSumDiffCredit(sumDiffCredit.add(int0802Vo.getDiffCredit()));
-				int0802Vo.setSumDiffDebit(sumDiffDebit.add(int0802Vo.getDiffDebit()));
+				sumDiffCredit = sumDiffCredit.add(int0802Vo.getDiffCredit());
+				sumDiffDebit = sumDiffDebit.add(int0802Vo.getDiffDebit());
+
+				/* ___________ summary sumBringForward and sumCarryForward ___________ */
+				sumBringForward = sumBringForward.add(NumberUtils.nullToZero(int0802Vo.getBringForward()));
+				sumCarryForward = sumCarryForward.add(NumberUtils.nullToZero(int0802Vo.getCarryForward()));
 			}
+
+			/* _______________ footer _______________ */
+			Int0802Vo footer = new Int0802Vo();
+			footer.setAccName("รวม");
+			footer.setDebit(sumDebit);
+			footer.setCredit(sumCredit);
+			footer.setCredit2(sumCredit2);
+			footer.setDebit2(sumDebit2);
+			footer.setBringForward(sumBringForward);
+			footer.setCarryForward(sumCarryForward);
+			footer.setDiffCredit(sumDiffCredit);
+			footer.setDiffDebit(sumDiffDebit);
+			response.add(footer);
 		}
 		return response;
 	}
@@ -89,6 +106,8 @@ public class Int0802Service {
 			BigDecimal sumCredit3 = BigDecimal.ZERO;
 			BigDecimal sumDiffDebit = BigDecimal.ZERO;
 			BigDecimal sumDiffCredit = BigDecimal.ZERO;
+			BigDecimal sumBringForward = BigDecimal.ZERO;
+			BigDecimal sumCarryForward = BigDecimal.ZERO;
 
 			/* _______________ year concat period _______________ */
 			reqeust.setPeriodFromYear((ConvertDateUtils.formatDateToString(
@@ -105,10 +124,10 @@ public class Int0802Service {
 				if (int0802Vo.getCurrAmt() != null) {
 					if ("40".equals(int0802Vo.getPkCode())) {
 						int0802Vo.setDebit2(int0802Vo.getCurrAmt());
-						int0802Vo.setSumDebit2(sumDebit2.add(int0802Vo.getCurrAmt()));
+						sumDebit2 = sumDebit2.add(int0802Vo.getCurrAmt());
 					} else if ("50".equals(int0802Vo.getPkCode())) {
 						int0802Vo.setCredit2(int0802Vo.getCurrAmt());
-						int0802Vo.setSumCredit2(sumCredit2.add(int0802Vo.getCurrAmt()));
+						sumCredit2 = sumCredit2.add(int0802Vo.getCurrAmt());
 					}
 				}
 
@@ -119,19 +138,34 @@ public class Int0802Service {
 						.subtract(NumberUtils.nullToZero(int0802Vo.getDebit3())));
 
 				/* _______________ summary credit, debit, credit3, debit3 _______________ */
-				int0802Vo.setSumDebit(
-						NumberUtils.nullToZero(sumDebit.add(NumberUtils.nullToZero(int0802Vo.getDebit()))));
-				int0802Vo.setSumCredit(
-						NumberUtils.nullToZero(sumCredit.add(NumberUtils.nullToZero(int0802Vo.getCredit()))));
-				int0802Vo.setSumDebit3(
-						NumberUtils.nullToZero(sumDebit3.add(NumberUtils.nullToZero(int0802Vo.getDebit3()))));
-				int0802Vo.setSumCredit3(
-						NumberUtils.nullToZero(sumCredit3.add(NumberUtils.nullToZero(int0802Vo.getCredit3()))));
+				sumDebit = NumberUtils.nullToZero(sumDebit.add(NumberUtils.nullToZero(int0802Vo.getDebit())));
+				sumCredit = NumberUtils.nullToZero(sumCredit.add(NumberUtils.nullToZero(int0802Vo.getCredit())));
+				sumDebit3 = NumberUtils.nullToZero(sumDebit3.add(NumberUtils.nullToZero(int0802Vo.getDebit3())));
+				sumDebit3 = NumberUtils.nullToZero(sumCredit3.add(NumberUtils.nullToZero(int0802Vo.getCredit3())));
 
-				/* ___________ summary difference credit2, debit2 ___________ */
-				int0802Vo.setSumDiffCredit(sumDiffCredit.add(int0802Vo.getDiffCredit()));
-				int0802Vo.setSumDiffDebit(sumDiffDebit.add(int0802Vo.getDiffDebit()));
+				/* _______________ summary difference credit2 and debit2 _______________ */
+				sumDiffCredit = sumDiffCredit.add(int0802Vo.getDiffCredit());
+				sumDiffDebit = sumDiffDebit.add(int0802Vo.getDiffDebit());
+
+				/* ___________ summary sumBringForward and sumCarryForward ___________ */
+				sumBringForward = sumBringForward.add(NumberUtils.nullToZero(int0802Vo.getBringForward()));
+				sumCarryForward = sumCarryForward.add(NumberUtils.nullToZero(int0802Vo.getCarryForward()));
 			}
+
+			/* _______________ footer _______________ */
+			Int0802Vo footer = new Int0802Vo();
+			footer.setAccName("รวม");
+			footer.setDebit(sumDebit);
+			footer.setCredit(sumCredit);
+			footer.setCredit2(sumCredit2);
+			footer.setDebit2(sumDebit2);
+			footer.setCredit3(sumCredit3);
+			footer.setDebit3(sumDebit3);
+			footer.setBringForward(sumBringForward);
+			footer.setCarryForward(sumCarryForward);
+			footer.setDiffCredit(sumDiffCredit);
+			footer.setDiffDebit(sumDiffDebit);
+			response.add(footer);
 		}
 		return response;
 	}
