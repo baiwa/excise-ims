624a9ef9eb765e502f51a08db794785ade6c5ba2
taechapon <taechapon@gmail.com>
2019-06-17 14:30:20 +0700

    Update Service Paper UnitTest

--- workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/ServicePaperQtyServiceTest.java
+++ workspace/ims-webapp/src/test/java/th/go/excise/ims/ta/service/ServicePaperQtyServiceTest.java
@@ -1,36 +1,96 @@
 package th.go.excise.ims.ta.service;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import th.co.baiwa.buckwaframework.common.constant.CommonConstants.PROFILE;
+import th.go.excise.ims.Application;
+import th.go.excise.ims.ta.vo.ServicePaperFormVo;
+import th.go.excise.ims.ta.vo.ServicePaperQtyVo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WithUserDetails(value = "ta001402", userDetailsServiceBeanName = "userDetailService")
+@ActiveProfiles(value = PROFILE.UNITTEST)
 public class ServicePaperQtyServiceTest {
-
-	private static final String PATH = "/tmp/";
-	private static final String TYPE = ".xlsx";
-	private static final String MATERIAL_RECEIVE = "service";
-
+	
+	private static final String OUTPUT_PATH = "/tmp/excise/ims/report";
+	
+	@Autowired
+	private ServicePaperQtyService servicePaperQtyService;
+	
 	@Test
-	public void test_materialReceive() throws IOException {
-
-		String fileName = URLEncoder.encode(MATERIAL_RECEIVE, "UTF-8");
-		ServicePaperQtyService servicePaperQty = new ServicePaperQtyService();
-
-//		try {
-//			byte[] outArray = servicePaperQty.exportFileQuantityServiceVo();
-//			FileOutputStream Output = new FileOutputStream(PATH + fileName + TYPE);
-//			Output.write(outArray);
-//			Output.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-
-		System.out.println("Done");
-
+	public void test_inquiry() {
+		ServicePaperFormVo formVo = new ServicePaperFormVo();
+		formVo.setAuditPlanCode("");
+		formVo.setNewRegId("01055491068592001");
+		formVo.setDutyGroupId("1702");
+		formVo.setStartDate("01/2561");
+		formVo.setEndDate("12/2561");
+		//formVo.setPaperSvNumber("");
+		
+		List<ServicePaperQtyVo> voList = servicePaperQtyService.inquiry(formVo);
+		voList.forEach(e -> {
+			System.out.println(ToStringBuilder.reflectionToString(e, ToStringStyle.SHORT_PREFIX_STYLE));
+		});
 	}
+	
+	//@Test
+	public void test_export() {
+		ServicePaperFormVo formVo = new ServicePaperFormVo();
+		formVo.setAuditPlanCode("");
+		formVo.setNewRegId("01055491068592001");
+		formVo.setDutyGroupId("1702");
+		formVo.setStartDate("01/2561");
+		formVo.setEndDate("12/2561");
+		//formVo.setPaperSvNumber("");
+		
+		String fileName = "servicePaper01_" + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".xlsx";
+		
+		try (FileOutputStream os = new FileOutputStream(OUTPUT_PATH + "/" + fileName)) {
+			byte[] bytes = servicePaperQtyService.export(formVo);
+			os.write(bytes);
+			System.out.println("Creating excel " + fileName + " Done");
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+	
+	//@Test
+	public void test_upload() {
+		ServicePaperFormVo formVo = new ServicePaperFormVo();
+		formVo.setAuditPlanCode("");
+		formVo.setNewRegId("01055491068592001");
+		formVo.setDutyGroupId("1702");
+		formVo.setStartDate("01/2561");
+		formVo.setEndDate("12/2561");
+		//formVo.setPaperSvNumber("");
+		
+	}
+	
+	//@Test
+	public void test_save() {
+		ServicePaperFormVo formVo = new ServicePaperFormVo();
+		formVo.setAuditPlanCode("");
+		formVo.setNewRegId("01055491068592001");
+		formVo.setDutyGroupId("1702");
+		formVo.setStartDate("01/2561");
+		formVo.setEndDate("12/2561");
+		//formVo.setPaperSvNumber("");
+		
+	}
+	
 }
