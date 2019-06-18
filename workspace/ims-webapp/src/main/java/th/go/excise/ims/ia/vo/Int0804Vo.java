c10a76c149b2270eb911d53cf4d23740d1acecc3
FlukeTP <fook_1589@hotmail.com>
2019-06-18 17:44:00 +0700

    update vo

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0804Vo.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0804Vo.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 public class Int0804Vo {
 	private List<Int0804DateVo> dateList;
-	private Long maxColumn1;
-	private Long maxColumn2;
+	private List<Int0804HeaderTable> th;
 
 	public List<Int0804DateVo> getDateList() {
 		return dateList;
@@ -15,20 +14,12 @@ public class Int0804Vo {
 		this.dateList = dateList;
 	}
 
-	public Long getMaxColumn1() {
-		return maxColumn1;
+	public List<Int0804HeaderTable> getTh() {
+		return th;
 	}
 
-	public void setMaxColumn1(Long maxColumn1) {
-		this.maxColumn1 = maxColumn1;
-	}
-
-	public Long getMaxColumn2() {
-		return maxColumn2;
-	}
-
-	public void setMaxColumn2(Long maxColumn2) {
-		this.maxColumn2 = maxColumn2;
+	public void setTh(List<Int0804HeaderTable> th) {
+		this.th = th;
 	}
 
 }
