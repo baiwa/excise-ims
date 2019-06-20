88b5b2533dd4191fbaecc7bbefa6c154038c72c8
FlukeTP <fook_1589@hotmail.com>
2019-06-20 10:55:13 +0700

    update Vo

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0804SummaryVo.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0804SummaryVo.java
@@ -1,9 +1,13 @@
 package th.go.excise.ims.ia.vo;
 
+import java.math.BigDecimal;
+
 public class Int0804SummaryVo {
 	private String dateDefault;
 	private String gfExciseCode;
 	private String sumCarryForward;
+	private BigDecimal sumOfMonth;
+	private BigDecimal totalMonths;
 
 	public String getDateDefault() {
 		return dateDefault;
@@ -27,6 +31,22 @@ public class Int0804SummaryVo {
 
 	public void setSumCarryForward(String sumCarryForward) {
 		this.sumCarryForward = sumCarryForward;
+	}
+
+	public BigDecimal getSumOfMonth() {
+		return sumOfMonth;
+	}
+
+	public void setSumOfMonth(BigDecimal sumOfMonth) {
+		this.sumOfMonth = sumOfMonth;
+	}
+
+	public BigDecimal getTotalMonths() {
+		return totalMonths;
+	}
+
+	public void setTotalMonths(BigDecimal totalMonths) {
+		this.totalMonths = totalMonths;
 	}
 
 }
