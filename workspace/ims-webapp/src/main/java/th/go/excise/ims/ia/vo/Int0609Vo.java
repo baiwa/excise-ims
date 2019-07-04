391f73ea229d2ab4d2a2540acedf7519e4ecc443
FlukeTP <fook_1589@hotmail.com>
2019-07-04 15:31:49 +0700

    add Vo and entity

---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/Int0609Vo.java
@@ -0,0 +1,25 @@
+package th.go.excise.ims.ia.vo;
+
+import java.util.List;
+
+public class Int0609Vo {
+	private List<Int0609TableVo> table;
+	private Int0609TableVo footer;
+
+	public List<Int0609TableVo> getTable() {
+		return table;
+	}
+
+	public void setTable(List<Int0609TableVo> table) {
+		this.table = table;
+	}
+
+	public Int0609TableVo getFooter() {
+		return footer;
+	}
+
+	public void setFooter(Int0609TableVo footer) {
+		this.footer = footer;
+	}
+
+}
---
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/vo/SearchVo.java
@@ -0,0 +1,50 @@
+package th.go.excise.ims.ia.vo;
+
+public class SearchVo {
+	private String officeCode;
+	private String periodMonth;
+	private String periodFrom;
+	private String periodTo;
+	private String incomeCode;
+
+	public String getOfficeCode() {
+		return officeCode;
+	}
+
+	public void setOfficeCode(String officeCode) {
+		this.officeCode = officeCode;
+	}
+
+	public String getPeriodMonth() {
+		return periodMonth;
+	}
+
+	public void setPeriodMonth(String periodMonth) {
+		this.periodMonth = periodMonth;
+	}
+
+	public String getPeriodFrom() {
+		return periodFrom;
+	}
+
+	public void setPeriodFrom(String periodFrom) {
+		this.periodFrom = periodFrom;
+	}
+
+	public String getPeriodTo() {
+		return periodTo;
+	}
+
+	public void setPeriodTo(String periodTo) {
+		this.periodTo = periodTo;
+	}
+
+	public String getIncomeCode() {
+		return incomeCode;
+	}
+
+	public void setIncomeCode(String incomeCode) {
+		this.incomeCode = incomeCode;
+	}
+
+}
