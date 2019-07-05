26099b6c040cc7e7448443a5ef05b41c62e713cc
watcharapong sribun <mmxx155@gmail.com>
2019-07-05 17:34:58 +0700

    edit export int090101

--- workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int090101Controller.java
+++ workspace/ims-webapp/src/main/java/th/go/excise/ims/ia/controller/Int090101Controller.java
@@ -1,12 +1,19 @@
 package th.go.excise.ims.ia.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,5 +52,27 @@ public class Int090101Controller {
 			responseData.setStatus(RESPONSE_STATUS.FAILED);
 		}
 		return responseData;
+	}
+
+//	/{officeCode}/{monthStart}/{yearStart}/monthEnd}/{yearEnd}
+	@GetMapping("/year/export-{officeCode}-{yearStart}-{monthStart}-{yearEnd}-{monthEnd}")
+	public void exportByYear(@PathVariable("officeCode") String officeCode, @PathVariable("yearStart") String yearStart,
+			@PathVariable("monthStart") String monthStart, @PathVariable("yearEnd") String yearEnd,
+			@PathVariable("monthEnd") String monthEnd, HttpServletResponse response) throws Exception {
+		// set fileName
+		String fileName = URLEncoder.encode("ตรวจสอบข้อมูลค่าใช้จ่ายพื้นที่:" + officeCode + "งวดที่" + monthStart + "/"
+				+ yearStart + "ถึง" + monthEnd + "/" + yearEnd, "UTF-8");
+		// write it as an excel attachment
+		ByteArrayOutputStream outByteStream = this.int090101Service.export(officeCode, yearStart, monthStart, yearEnd,
+				monthEnd);
+		byte[] outArray = outByteStream.toByteArray();
+		response.setContentType("application/octet-stream");
+		response.setContentLength(outArray.length);
+		response.setHeader("Expires:", "0"); // eliminates browser caching
+		response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".xlsx");
+		OutputStream outStream = response.getOutputStream();
+		outStream.write(outArray);
+		outStream.flush();
+		outStream.close();
 	}
 }
